9929d9ef0cd0ff052a98b39f442baadcd855802a
Sten Wessel <s.wessel@student.tue.nl>
2016-07-01 22:01:54 +0200

    Added comment syntax highlighting

--- src/nl/rubensten/texifyidea/LatexSyntaxHighlighter.java
+++ src/nl/rubensten/texifyidea/LatexSyntaxHighlighter.java
@@ -1,15 +1,23 @@
 package nl.rubensten.texifyidea;
 
 import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.tree.IElementType;
+import nl.rubensten.texifyidea.psi.LatexTypes;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Sten Wessel
  */
 public class LatexSyntaxHighlighter extends SyntaxHighlighterBase {
+
+    public static final TextAttributesKey COMMENT = TextAttributesKey.
+            createTextAttributesKey("LATEX_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 
 
     @NotNull
@@ -21,6 +29,10 @@ public class LatexSyntaxHighlighter extends SyntaxHighlighterBase {
     @NotNull
     @Override
     public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
-        return new TextAttributesKey[0];
+        if (tokenType.equals(LatexTypes.COMMENT_TOKEN)) {
+            return COMMENT_KEYS;
+        } else {
+            return EMPTY_KEYS;
+        }
     }
 }
