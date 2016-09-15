d2c30cc86988df622a12acb162baeaaddd0f93e6
Sten Wessel <sten.wessel@gmail.com>
2016-09-15 08:10:15 +0200

    Updated icon from GreenTeaFlavouredIcons

--- src/nl/rubensten/texifyidea/TexifyIcons.java
+++ src/nl/rubensten/texifyidea/TexifyIcons.java
@@ -9,6 +9,10 @@ import javax.swing.*;
  */
 public class TexifyIcons {
 
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
     public static final Icon LATEX_FILE = IconLoader.getIcon(
             "/nl/rubensten/texifyidea/icons/latex-file.png"
     );
