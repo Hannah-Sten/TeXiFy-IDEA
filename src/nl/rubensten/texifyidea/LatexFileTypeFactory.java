f2e9bd59651ba46997c7b0dfa4c34d66d32386d7
RubenSchellekens <r.p.schellekens@gmail.com>
2017-02-08 20:12:49 +0100

    Added LaTeX style file type.

--- src/nl/rubensten/texifyidea/LatexFileTypeFactory.java
+++ src/nl/rubensten/texifyidea/LatexFileTypeFactory.java
@@ -2,6 +2,7 @@ package nl.rubensten.texifyidea;
 
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
+import nl.rubensten.texifyidea.file.StyleFileType;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -12,5 +13,6 @@ public class LatexFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer consumer) {
         consumer.consume(LatexFileType.INSTANCE, "latex");
+        consumer.consume(StyleFileType.INSTANCE, "latex-style");
     }
 }
