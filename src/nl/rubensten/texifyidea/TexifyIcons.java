85618e0697fcb04db1193d9193397f2cebd4a68d
RubenSchellekens <r.p.schellekens@gmail.com>
2017-02-08 19:56:37 +0100

    Added several TexifyIcons-icons.
    Made constants in TexifyIcons and added image source files.

--- src/nl/rubensten/texifyidea/TexifyIcons.java
+++ src/nl/rubensten/texifyidea/TexifyIcons.java
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 /**
- * @author Sten Wessel
+ * @author Ruben Schellekens, Sten Wessel
  */
 public class TexifyIcons {
 
@@ -15,6 +15,62 @@ public class TexifyIcons {
      */
     public static final Icon LATEX_FILE = IconLoader.getIcon(
             "/nl/rubensten/texifyidea/icons/latex-file.png"
+    );
+
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
+    public static final Icon PDF_FILE = IconLoader.getIcon(
+            "/nl/rubensten/texifyidea/icons/pdf-file.png"
+    );
+
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
+    public static final Icon BIBLIOGRAPHY_FILE = IconLoader.getIcon(
+            "/nl/rubensten/texifyidea/icons/bibliography-file.png"
+    );
+
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
+    public static final Icon CLASS_FILE = IconLoader.getIcon(
+            "/nl/rubensten/texifyidea/icons/class-file.png"
+    );
+
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
+    public static final Icon DOCUMENTED_LATEX_SOURCE = IconLoader.getIcon(
+            "/nl/rubensten/texifyidea/icons/doc-latex-file.png"
+    );
+
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
+    public static final Icon STYLE_FILE = IconLoader.getIcon(
+            "/nl/rubensten/texifyidea/icons/style-file.png"
+    );
+
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
+    public static final Icon FILE = IconLoader.getIcon(
+            "/nl/rubensten/texifyidea/icons/file.png"
+    );
+
+    /**
+     * Copyright (c) 2016 Ruben Schellekens
+     * https://github.com/RubenSchellekens/GreenTeaFlavouredIcons
+     */
+    public static final Icon TEXT_FILE = IconLoader.getIcon(
+            "/nl/rubensten/texifyidea/icons/text-file.png"
     );
 
     public static final Icon LATEX_MODULE = IconLoader.getIcon(
