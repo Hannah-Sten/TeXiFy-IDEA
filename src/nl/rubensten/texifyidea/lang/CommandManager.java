fd976e3934a1c578ca079976166fd749fe70faa6
RubenSchellekens <r.p.schellekens@gmail.com>
2017-02-12 09:46:24 +0100

    Extended CommandManager outline.

--- src/nl/rubensten/texifyidea/lang/CommandManager.java
+++ src/nl/rubensten/texifyidea/lang/CommandManager.java
@@ -1,15 +1,18 @@
 package nl.rubensten.texifyidea.lang;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * TODO: A whole lot.
  *
  * @author Ruben Schellekens
  */
-public class CommandManager {
+public class CommandManager implements Iterable<String>, Serializable {
+
+    private static final long serialVersionUID = 192873489129843L;
 
     /**
      * TODO: docs.
@@ -100,9 +103,81 @@ public class CommandManager {
     /**
      * TODO: docs.
      */
+    public boolean isRegistered(String command) {
+        return aliases.containsKey(command);
+    }
+
+    /**
+     * TODO: implement.
+     */
+    public boolean isRegisteredNoSlash(String commandNoSlash) {
+        return false;
+    }
+
+    /**
+     * TODO: docs.
+     */
+    public boolean isOriginal(String command) {
+        return original.containsKey(command);
+    }
+
+    /**
+     * TODO: implement.
+     */
+    public boolean isOriginalNoSlash(String commandNoSlash) {
+        return false;
+    }
+
+    /**
+     * TODO: docs.
+     */
     public void clear() {
         aliases.clear();
         original.clear();
+    }
+
+    /**
+     * TODO: docs.
+     */
+    public int size() {
+        return aliases.size();
+    }
+
+    /**
+     * TODO: docs.
+     */
+    public int originalSize() {
+        return original.size();
+    }
+
+    /**
+     * TODO: docs.
+     */
+    public Stream<String> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
+
+    /**
+     * TODO: docs.
+     */
+    public Stream<String> parallelStream() {
+        return StreamSupport.stream(spliterator(), true);
+    }
+
+    /**
+     * TODO: docs.
+     */
+    @Override
+    public Iterator<String> iterator() {
+        return aliases.keySet().iterator();
+    }
+
+    /**
+     * TODO: docs.
+     */
+    @Override
+    public Spliterator<String> spliterator() {
+        return aliases.keySet().spliterator();
     }
 
 }
