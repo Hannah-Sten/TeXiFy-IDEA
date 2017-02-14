72581cd6a3c61ba66a1135065ab0c9228ba07caf
RubenSchellekens <r.p.schellekens@gmail.com>
2017-02-14 11:35:41 +0100

    Reverted to old new file system.

--- src/nl/rubensten/texifyidea/action/NewLatexFileAction.java
+++ src/nl/rubensten/texifyidea/action/NewLatexFileAction.java
@@ -1,18 +1,17 @@
 package nl.rubensten.texifyidea.action;
 
-import com.intellij.ide.actions.CreateElementActionBase;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import nl.rubensten.texifyidea.TexifyIcons;
-import nl.rubensten.texifyidea.file.LatexFileType;
-import nl.rubensten.texifyidea.templates.LatexTemplatesFactory;
 import nl.rubensten.texifyidea.util.TexifyUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -23,7 +22,7 @@ import java.io.IOException;
 /**
  * @author Ruben Schellekens
  */
-public class NewLatexFileAction extends CreateElementActionBase implements DumbAware {
+public class NewLatexFileAction extends AnAction implements DumbAware {
 
     private static final String OPTION_TEX_FILE = "tex";
     private static final String OPTION_STY_FILE = "sty";
@@ -33,59 +32,17 @@ public class NewLatexFileAction extends CreateElementActionBase implements DumbA
         super("LaTeX File", "Create a new LaTeX file", TexifyIcons.LATEX_FILE);
     }
 
-    @NotNull
     @Override
-    protected PsiElement[] invokeDialog(Project project, PsiDirectory psiDirectory) {
-        LatexFileCreator fileCreator = new LatexFileCreator(project, psiDirectory);
+    public void actionPerformed(AnActionEvent event) {
+        final Project project = event.getData(PlatformDataKeys.PROJECT);
+        final VirtualFile directory = event.getData(PlatformDataKeys.VIRTUAL_FILE);
 
         CreateFileFromTemplateDialog.Builder builder = CreateFileFromTemplateDialog.createDialog(project);
         builder.setTitle("Create a New LaTeX File");
         builder.addKind("Sources (.tex)", TexifyIcons.LATEX_FILE, OPTION_TEX_FILE);
         builder.addKind("Package (.sty)", TexifyIcons.STYLE_FILE, OPTION_STY_FILE);
         builder.addKind("Document class (.cls)", TexifyIcons.CLASS_FILE, OPTION_CLS_FILE);
-        builder.show("", null, fileCreator);
-
-        return new PsiElement[] {
-                fileCreator.getCreatedFile()
-        };
-    }
-
-    @NotNull
-    @Override
-    protected PsiElement[] create(String newName, PsiDirectory psiDirectory) throws Exception {
-        PsiFile file = LatexTemplatesFactory.createFromTemplate(
-                psiDirectory,
-                TexifyUtil.appendExtension(newName, "tex"),
-                LatexTemplatesFactory.DEFAULT_TEMPLATE_FILENAME,
-                LatexFileType.INSTANCE);
-
-        PsiElement child = file.getLastChild();
-        if (child == null) {
-            return new PsiElement[] {
-                    file
-            };
-        }
-        else {
-            return new PsiElement[] {
-                    file,
-                    child
-            };
-        }
-    }
-
-    @Override
-    protected String getErrorTitle() {
-        return "Error";
-    }
-
-    @Override
-    protected String getCommandName() {
-        return "LaTeX File";
-    }
-
-    @Override
-    protected String getActionName(PsiDirectory psiDirectory, String s) {
-        return "LaTeX File";
+        builder.show("", null, new LatexFileCreator(project, directory));
     }
 
     private class LatexFileCreator implements CreateFileFromTemplateDialog.FileCreator<PsiElement> {
@@ -94,12 +51,15 @@ public class NewLatexFileAction extends CreateElementActionBase implements DumbA
         private final VirtualFile directory;
         private final LocalFileSystem fileSystem;
 
-        private PsiFile createdFile;
-
-        private LatexFileCreator(Project project, PsiDirectory directory) {
+        private LatexFileCreator(Project project, VirtualFile directory) {
             this.project = project;
-            this.directory = directory.getVirtualFile();
+            this.directory = directory;
             this.fileSystem = LocalFileSystem.getInstance();
+        }
+
+        private void openFile(VirtualFile virtualFile) {
+            FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+            fileEditorManager.openFile(virtualFile, true);
         }
 
         @Nullable
@@ -111,7 +71,6 @@ public class NewLatexFileAction extends CreateElementActionBase implements DumbA
 
             String dirPath = directory.getPath();
             String newFilePath = TexifyUtil.appendExtension(dirPath + "/" + fileName, option);
-
             File newFile = new File(newFilePath);
 
             try {
@@ -122,17 +81,15 @@ public class NewLatexFileAction extends CreateElementActionBase implements DumbA
             }
 
             VirtualFile virtualFile = fileSystem.refreshAndFindFileByPath(newFilePath);
-            return (createdFile = PsiManager.getInstance(project).findFile(virtualFile));
+            openFile(virtualFile);
+
+            return PsiManager.getInstance(project).findFile(virtualFile);
         }
 
         @NotNull
         @Override
         public String getActionName(@NotNull String fileName, @NotNull String option) {
             return "New LaTeX File";
-        }
-
-        public PsiFile getCreatedFile() {
-            return createdFile;
         }
     }
 }
