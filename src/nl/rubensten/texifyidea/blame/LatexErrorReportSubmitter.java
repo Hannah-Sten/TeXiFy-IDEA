5194a8a9ccd696869982a8b650e95c9a9ecbd0ec
Sten Wessel <sten.wessel@gmail.com>
2017-03-01 16:14:13 +0100

    Implemented error report link to GitHub.
    Opens browser from the bug report page, so the user can file an issue. However, passing everything in the URL is not optimal because of a character limit.

---
+++ src/nl/rubensten/texifyidea/blame/LatexErrorReportSubmitter.java
@@ -0,0 +1,59 @@
+package nl.rubensten.texifyidea.blame;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.diagnostic.ErrorReportSubmitter;
+import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
+import com.intellij.openapi.diagnostic.SubmittedReportInfo;
+import com.intellij.util.Consumer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Send error report to GitHub issue tracker.
+ *
+ * @author Sten Wessel
+ */
+public class LatexErrorReportSubmitter extends ErrorReportSubmitter {
+
+    private static final String URL = "https://github.com/Ruben-Sten/TeXiFy-IDEA/issues/new?labels[]=bug&title=Crash%20Report:%20";
+    private static final String ENCODING = "UTF-8";
+
+    @NotNull
+    @Override
+    public String getReportActionText() {
+        return "Report to TeXiFy-IDEA GitHub";
+    }
+
+    @Override
+    public boolean submit(@NotNull IdeaLoggingEvent[] events, @Nullable String additionalInfo,
+                          @NotNull Component parentComponent, @NotNull
+                                      Consumer<SubmittedReportInfo> consumer) {
+
+        IdeaLoggingEvent event = events[0];
+
+        StringBuilder builder = new StringBuilder(URL);
+        try {
+            builder.append(URLEncoder.encode(event.getMessage(), ENCODING));
+            builder.append("&body=");
+            builder.append(URLEncoder.encode("### Description\n", ENCODING));
+            builder.append(URLEncoder.encode(additionalInfo == null ? "\n" : additionalInfo, ENCODING));
+            builder.append(URLEncoder.encode("\n### Stacktrace\n```\n", ENCODING));
+            builder.append(URLEncoder.encode(event.getThrowableText(), ENCODING));
+            builder.append(URLEncoder.encode("\n```", ENCODING));
+        }
+        catch (UnsupportedEncodingException e) {
+            consumer.consume(new SubmittedReportInfo(null, null, SubmittedReportInfo
+                    .SubmissionStatus.FAILED));
+            return false;
+        }
+
+        BrowserUtil.browse(builder.toString());
+        consumer.consume(new SubmittedReportInfo(null, "GitHub issue",
+                                                 SubmittedReportInfo.SubmissionStatus.NEW_ISSUE));
+        return true;
+    }
+}
