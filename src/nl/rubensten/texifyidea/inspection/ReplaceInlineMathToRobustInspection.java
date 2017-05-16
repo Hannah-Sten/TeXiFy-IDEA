4c7e9f806a5497e9b1e6d5f952b8841f992267be
Sten Wessel <sten.wessel@gmail.com>
2017-05-16 18:33:09 +0200

    Added a sample inspection

--- src/nl/rubensten/texifyidea/inspection/ReplaceInlineMathToRobustInspection.java
+++ src/nl/rubensten/texifyidea/inspection/ReplaceInlineMathToRobustInspection.java
@@ -1,39 +1,28 @@
 package nl.rubensten.texifyidea.inspection;
 
 import com.intellij.codeInspection.InspectionManager;
-import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.psi.PsiElement;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiRecursiveElementWalkingVisitor;
-import nl.rubensten.texifyidea.psi.LatexTypes;
+import com.intellij.psi.util.PsiTreeUtil;
+import kotlin.reflect.jvm.internal.impl.utils.SmartList;
+import nl.rubensten.texifyidea.file.LatexFile;
+import nl.rubensten.texifyidea.psi.LatexInlineMath;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
+import java.util.List;
+
 /**
+ * Not yet ready for production, only for demonstration.
+ *
  * @author Sten Wessel
  */
-public class ReplaceInlineMathToRobustInspection extends LocalInspectionTool {
-
-    private static void checkFile(final PsiFile file, final ProblemsHolder problemsHolder) {
-        file.accept(new PsiRecursiveElementWalkingVisitor() {
-            @Override
-            public void visitElement(PsiElement element) {
-                if (element.getNode() == LatexTypes.INLINE_MATH_START) {
-                    // TODO
-                }
-            }
-        });
-    }
-
-    @Nls
-    @NotNull
-    @Override
-    public String getGroupDisplayName() {
-        return "LaTeX";
-    }
+public class ReplaceInlineMathToRobustInspection extends TexifyInspectionBase {
 
     @Nls
     @NotNull
@@ -52,8 +41,18 @@ public class ReplaceInlineMathToRobustInspection extends LocalInspectionTool {
     @Override
     public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager
             manager, boolean isOnTheFly) {
-        ProblemsHolder problemsHolder = new ProblemsHolder(manager, file, isOnTheFly);
-        checkFile(file, problemsHolder);
-        return problemsHolder.getResultsArray();
+        if (!(file instanceof LatexFile)) {
+            return null;
+        }
+
+        List<ProblemDescriptor> descriptors = new SmartList<>();
+
+        Collection<LatexInlineMath> envs = PsiTreeUtil.findChildrenOfType(file, LatexInlineMath.class);
+        for (LatexInlineMath env : envs) {
+            ProgressManager.checkCanceled();
+            descriptors.add(manager.createProblemDescriptor(env, "Change to robust environment", (LocalQuickFix)null, ProblemHighlightType.WEAK_WARNING, true));
+        }
+
+        return descriptors.toArray(new ProblemDescriptor[descriptors.size()]);
     }
 }
---
+++ src/nl/rubensten/texifyidea/inspection/TexifyInspectionBase.java
@@ -0,0 +1,19 @@
+package nl.rubensten.texifyidea.inspection;
+
+import com.intellij.codeInspection.LocalInspectionTool;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Sten Wessel
+ */
+public abstract class TexifyInspectionBase extends LocalInspectionTool {
+    private static final String GROUP_DISPLAY_NAME = "TeXiFy";
+
+    @Nls
+    @NotNull
+    @Override
+    public String getGroupDisplayName() {
+        return GROUP_DISPLAY_NAME;
+    }
+}
