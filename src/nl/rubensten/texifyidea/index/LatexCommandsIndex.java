8877c5910de358f6cf4133387d4d992b9c4ebf12
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-05-19 15:55:40 +0200

    Added index for latex commands.

---
+++ src/nl/rubensten/texifyidea/index/LatexCommandsIndex.java
@@ -0,0 +1,21 @@
+package nl.rubensten.texifyidea.index;
+
+import com.intellij.psi.stubs.StringStubIndexExtension;
+import com.intellij.psi.stubs.StubIndexKey;
+import nl.rubensten.texifyidea.psi.LatexCommands;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Ruben Schellekens
+ */
+public class LatexCommandsIndex extends StringStubIndexExtension<LatexCommands> {
+
+    private static final StubIndexKey<String, LatexCommands> KEY =
+            StubIndexKey.createIndexKey("nl.rubensten.texifyidea.commands");
+
+    @NotNull
+    @Override
+    public StubIndexKey<String, LatexCommands> getKey() {
+        return KEY;
+    }
+}
