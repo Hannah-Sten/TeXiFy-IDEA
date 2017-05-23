97a82877019305aa9e7ab42c9e4fd0a512b44b2b
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-05-23 21:24:58 +0200

    Added label filter to structure view.

--- src/nl/rubensten/texifyidea/structure/LatexStructureViewModel.java
+++ src/nl/rubensten/texifyidea/structure/LatexStructureViewModel.java
@@ -3,9 +3,11 @@ package nl.rubensten.texifyidea.structure;
 import com.intellij.ide.structureView.StructureViewModel.ElementInfoProvider;
 import com.intellij.ide.structureView.StructureViewModelBase;
 import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.Filter;
 import com.intellij.ide.util.treeView.smartTree.Sorter;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
+import nl.rubensten.texifyidea.structure.filter.LatexLabelFilter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,15 +17,25 @@ import org.jetbrains.annotations.Nullable;
 public class LatexStructureViewModel extends StructureViewModelBase implements ElementInfoProvider {
 
     public static final Sorter[] SORTERS = new Sorter[] { Sorter.ALPHA_SORTER };
+    public static final Filter[] FILTERS = new Filter[] {
+            new LatexLabelFilter()
+    };
 
     public LatexStructureViewModel(@NotNull PsiFile psiFile, @Nullable Editor editor) {
         super(psiFile, editor, new LatexStructureViewElement(psiFile));
+        getFilters();
     }
 
     @NotNull
     @Override
     public Sorter[] getSorters() {
         return SORTERS;
+    }
+
+    @NotNull
+    @Override
+    public Filter[] getFilters() {
+        return FILTERS;
     }
 
     @Override
