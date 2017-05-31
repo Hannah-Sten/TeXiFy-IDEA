dcab264c51e4f8727539fa2d48cbc526fd54da03
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-05-31 15:20:19 +0200

    Forgot to mention Fixed #44. Refactored the node decorator a tad.

--- src/nl/rubensten/texifyidea/editor/TeXiFyProjectViewNodeDecorator.java
+++ src/nl/rubensten/texifyidea/editor/TeXiFyProjectViewNodeDecorator.java
@@ -8,21 +8,42 @@ import com.intellij.packageDependencies.ui.PackageDependenciesNode;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import nl.rubensten.texifyidea.TexifyIcons;
 
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Ruben Schellekens
  */
 public class TeXiFyProjectViewNodeDecorator implements ProjectViewNodeDecorator {
 
-    @Override
-    public void decorate(ProjectViewNode projectViewNode, PresentationData presentationData) {
+    private static final Map<String, Icon> FILE_ICONS = new HashMap<>();
+    static {
+        FILE_ICONS.put("pdf", TexifyIcons.PDF_FILE);
+    }
+
+    private void setIcon(ProjectViewNode projectViewNode, PresentationData presentationData) {
         VirtualFile file = projectViewNode.getVirtualFile();
         if (file.isDirectory()) {
             return;
         }
 
-        if (file.getExtension().equalsIgnoreCase("pdf")) {
-            presentationData.setIcon(TexifyIcons.PDF_FILE);
+        String extension = file.getExtension();
+        if (extension == null) {
+            return;
         }
+
+        Icon icon = FILE_ICONS.get(file.getExtension().toLowerCase());
+        if (icon == null) {
+            return;
+        }
+
+        presentationData.setIcon(icon);
+    }
+
+    @Override
+    public void decorate(ProjectViewNode projectViewNode, PresentationData presentationData) {
+        setIcon(projectViewNode, presentationData);
     }
 
     @Override
