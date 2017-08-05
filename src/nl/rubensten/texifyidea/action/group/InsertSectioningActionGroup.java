ccfe3a8df6d2ef3ebcd12b8e8f1a16a6b1ad5375
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-08-05 16:40:43 +0200

    Added insert groups + insert part.

---
+++ src/nl/rubensten/texifyidea/action/group/InsertSectioningActionGroup.java
@@ -0,0 +1,15 @@
+package nl.rubensten.texifyidea.action.group;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+
+/**
+ * @author Ruben Schellekens
+ */
+public class InsertSectioningActionGroup extends DefaultActionGroup {
+
+    @Override
+    public void update(AnActionEvent e) {
+        // Do nothing.
+    }
+}
