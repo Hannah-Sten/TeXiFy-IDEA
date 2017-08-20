765f291a8bc4548fc6b9d92e5f21aef7ee786a76
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-08-20 23:40:20 +0200

    Added missing import inspection.

--- src/nl/rubensten/texifyidea/lang/LatexCommand.java
+++ src/nl/rubensten/texifyidea/lang/LatexCommand.java
@@ -1,11 +1,34 @@
 package nl.rubensten.texifyidea.lang;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Ruben Schellekens
  */
 public interface LatexCommand {
+
+    /**
+     * Looks up the given command name in all {@link LatexMathCommand}s and {@link
+     * LatexNoMathCommand}.
+     *
+     * @param commandName
+     *         The command name to look up. Can start with or without {@code \}.
+     * @return The found command, or {@code null} whe the command doesn't exist.
+     */
+    @Nullable
+    static LatexCommand lookup(String commandName) {
+        if (commandName.startsWith("\\")) {
+            commandName = commandName.substring(1);
+        }
+
+        LatexMathCommand math = LatexMathCommand.get(commandName);
+        if (math != null) {
+            return math;
+        }
+
+        return LatexNoMathCommand.get(commandName).orElse(null);
+    }
 
     /**
      * Get the name of the command without the first backslash.
