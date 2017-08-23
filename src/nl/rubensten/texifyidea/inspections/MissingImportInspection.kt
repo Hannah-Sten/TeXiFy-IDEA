e59ae17a74e30489b4ebddccdbd39e4f9d090a26
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-08-24 00:15:17 +0200

    Updated MissingImports package to account for environment imports.

--- src/nl/rubensten/texifyidea/inspections/MissingImportInspection.kt
+++ src/nl/rubensten/texifyidea/inspections/MissingImportInspection.kt
@@ -6,14 +6,17 @@ import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import nl.rubensten.texifyidea.lang.Environment
 import nl.rubensten.texifyidea.lang.LatexCommand
+import nl.rubensten.texifyidea.lang.Package
+import nl.rubensten.texifyidea.psi.LatexBeginCommand
 import nl.rubensten.texifyidea.psi.LatexCommands
-import nl.rubensten.texifyidea.util.PackageUtils
-import nl.rubensten.texifyidea.util.commandsInFileSet
+import nl.rubensten.texifyidea.psi.LatexEnvironment
+import nl.rubensten.texifyidea.util.*
 import kotlin.reflect.jvm.internal.impl.utils.SmartList
 
 /**
- * Currently only works for built-in commands.
+ * Currently works for built-in commands and environments.
  *
  * @author Ruben Schellekens
  */
@@ -32,6 +35,38 @@ open class MissingImportInspection : TexifyInspectionBase() {
         val descriptors = SmartList<ProblemDescriptor>()
 
         val includedPackages = PackageUtils.getIncludedPackages(file)
+        analyseCommands(file, includedPackages, descriptors, manager, isOntheFly)
+        analyseEnvironments(file, includedPackages, descriptors, manager, isOntheFly)
+
+        return descriptors
+    }
+
+    private fun analyseEnvironments(file: PsiFile, includedPackages: Collection<String>,
+                                descriptors: MutableList<ProblemDescriptor>, manager: InspectionManager,
+                                isOntheFly: Boolean) {
+        val environments = file.childrenOfType(LatexEnvironment::class)
+        for (env in environments) {
+            val name = env.name()?.text ?: continue
+            val environment = Environment[name] ?: continue
+            val pack = environment.getDependency()
+
+            if (pack == Package.DEFAULT || includedPackages.contains(pack.name)) {
+                continue
+            }
+
+            descriptors.add(manager.createProblemDescriptor(
+                    env.firstChildOfType(LatexBeginCommand::class) ?: env,
+                    "Environment requires ${pack.name} package",
+                    ImportEnvironmentFix(pack.name),
+                    ProblemHighlightType.ERROR,
+                    isOntheFly
+            ))
+        }
+    }
+
+    private fun analyseCommands(file: PsiFile, includedPackages: Collection<String>,
+                                descriptors: MutableList<ProblemDescriptor>, manager: InspectionManager,
+                                isOntheFly: Boolean) {
         val commands = file.commandsInFileSet()
         for (cmd in commands) {
             val latexCommand = LatexCommand.lookup(cmd.name) ?: continue
@@ -45,23 +80,21 @@ open class MissingImportInspection : TexifyInspectionBase() {
                 descriptors.add(manager.createProblemDescriptor(
                         cmd,
                         "Command requires ${pack.name} package",
-                        ImportFix(),
+                        ImportCommandFix(pack.name),
                         ProblemHighlightType.ERROR,
                         isOntheFly
                 ))
             }
         }
-
-        return descriptors
     }
 
     /**
      * @author Ruben Schellekens
      */
-    private class ImportFix : LocalQuickFix {
+    private class ImportCommandFix(val import: String) : LocalQuickFix {
 
         override fun getFamilyName(): String {
-            return "Add import"
+            return "Add import for package '$import'"
         }
 
         override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
@@ -70,6 +103,25 @@ open class MissingImportInspection : TexifyInspectionBase() {
             val file = command.containingFile
 
             PackageUtils.insertUsepackage(file, latexCommand.getDependency())
+        }
+    }
+
+    /**
+     * @author Ruben Schellekens
+     */
+    private class ImportEnvironmentFix(val import: String) : LocalQuickFix {
+
+        override fun getFamilyName(): String {
+            return "Add import for package '$import'"
+        }
+
+        override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+            val begin = descriptor.psiElement as LatexBeginCommand
+            val environment = begin.parentOfType(LatexEnvironment::class) ?: return
+            val thingy = Environment.fromPsi(environment) ?: return
+            val file = environment.containingFile
+
+            PackageUtils.insertUsepackage(file, thingy.getDependency())
         }
     }
 }
