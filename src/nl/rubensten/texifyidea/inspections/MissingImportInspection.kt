4ebbd31c853b724c80477e3f1f345b5b5527c94e
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-08-24 00:25:00 +0200

    MissingImports inspection underlines command tokens/environment name only.
    That means that not the whole command gets underlined.

--- src/nl/rubensten/texifyidea/inspections/MissingImportInspection.kt
+++ src/nl/rubensten/texifyidea/inspections/MissingImportInspection.kt
@@ -5,14 +5,17 @@ import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
 import nl.rubensten.texifyidea.lang.Environment
 import nl.rubensten.texifyidea.lang.LatexCommand
 import nl.rubensten.texifyidea.lang.Package
-import nl.rubensten.texifyidea.psi.LatexBeginCommand
 import nl.rubensten.texifyidea.psi.LatexCommands
 import nl.rubensten.texifyidea.psi.LatexEnvironment
-import nl.rubensten.texifyidea.util.*
+import nl.rubensten.texifyidea.util.PackageUtils
+import nl.rubensten.texifyidea.util.childrenOfType
+import nl.rubensten.texifyidea.util.commandsInFileSet
+import nl.rubensten.texifyidea.util.name
 import kotlin.reflect.jvm.internal.impl.utils.SmartList
 
 /**
@@ -55,11 +58,12 @@ open class MissingImportInspection : TexifyInspectionBase() {
             }
 
             descriptors.add(manager.createProblemDescriptor(
-                    env.firstChildOfType(LatexBeginCommand::class) ?: env,
+                    env,
+                    TextRange(7, 7 + name.length),
                     "Environment requires ${pack.name} package",
-                    ImportEnvironmentFix(pack.name),
                     ProblemHighlightType.ERROR,
-                    isOntheFly
+                    isOntheFly,
+                    ImportEnvironmentFix(pack.name)
             ))
         }
     }
@@ -79,10 +83,11 @@ open class MissingImportInspection : TexifyInspectionBase() {
             if (!includedPackages.contains(pack.name)) {
                 descriptors.add(manager.createProblemDescriptor(
                         cmd,
+                        TextRange(0, latexCommand.command.length + 1),
                         "Command requires ${pack.name} package",
-                        ImportCommandFix(pack.name),
                         ProblemHighlightType.ERROR,
-                        isOntheFly
+                        isOntheFly,
+                        ImportCommandFix(pack.name)
                 ))
             }
         }
@@ -116,8 +121,7 @@ open class MissingImportInspection : TexifyInspectionBase() {
         }
 
         override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
-            val begin = descriptor.psiElement as LatexBeginCommand
-            val environment = begin.parentOfType(LatexEnvironment::class) ?: return
+            val environment = descriptor.psiElement as? LatexEnvironment ?: return
             val thingy = Environment.fromPsi(environment) ?: return
             val file = environment.containingFile
 
