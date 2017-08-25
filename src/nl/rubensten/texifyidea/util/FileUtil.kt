10710a6d3271d613ea97cbbd717de609ba2f96dc
Sten Wessel <sten.wessel@gmail.com>
2017-08-25 23:26:35 +0200

    Converted CommandLineState to Kotlin to prevent NPEs

--- src/nl/rubensten/texifyidea/util/FileUtil.kt
+++ src/nl/rubensten/texifyidea/util/FileUtil.kt
@@ -1,6 +1,8 @@
 package nl.rubensten.texifyidea.util
 
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
@@ -9,3 +11,12 @@ import com.intellij.psi.PsiManager
  * Looks up the PsiFile that corresponds to the Virtual File.
  */
 fun VirtualFile.psiFile(project: Project): PsiFile? = PsiManager.getInstance(project).findFile(this)
+
+/**
+ * Creates a project directory at `path` which will be marked as excluded.
+ *
+ * @param path The path to create the directory to.
+ */
+fun Module.createExcludedDir(path: String) {
+    ModuleRootManager.getInstance(this).modifiableModel.addContentEntry(path).addExcludeFolder(path)
+}
