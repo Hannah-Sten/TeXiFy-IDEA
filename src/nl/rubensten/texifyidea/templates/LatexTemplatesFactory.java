8f02a091c4529b88d6de06086706601284474efa
Sten Wessel <sten.wessel@gmail.com>
2017-11-12 16:43:25 +0100

    Added option to enable BibTeX support in LatexModuleBuilder

--- src/nl/rubensten/texifyidea/templates/LatexTemplatesFactory.java
+++ src/nl/rubensten/texifyidea/templates/LatexTemplatesFactory.java
@@ -23,6 +23,7 @@ public class LatexTemplatesFactory implements FileTemplateGroupDescriptorFactory
     public static final String DESCRIPTOR = "LaTeX";
 
     public static final String FILE_TEMPLATE_TEX = "LaTeX Source.tex";
+    public static final String FILE_TEMPLATE_TEX_WITH_BIB = "LaTeX Source With BibTeX.tex";
     public static final String FILE_TEMPLATE_STY = "LaTeX Package.sty";
     public static final String FILE_TEMPLATE_CLS = "LaTeX Document class.cls";
     public static final String FILE_TEMPLATE_BIB = "BibTeX Bibliography.bib";
@@ -74,6 +75,7 @@ public class LatexTemplatesFactory implements FileTemplateGroupDescriptorFactory
         );
 
         descriptor.addTemplate(new FileTemplateDescriptor(FILE_TEMPLATE_TEX, TexifyIcons.LATEX_FILE));
+        descriptor.addTemplate(new FileTemplateDescriptor(FILE_TEMPLATE_TEX_WITH_BIB, TexifyIcons.LATEX_FILE));
         descriptor.addTemplate(new FileTemplateDescriptor(FILE_TEMPLATE_STY, TexifyIcons.STYLE_FILE));
         descriptor.addTemplate(new FileTemplateDescriptor(FILE_TEMPLATE_CLS, TexifyIcons.CLASS_FILE));
         descriptor.addTemplate(new FileTemplateDescriptor(FILE_TEMPLATE_BIB, TexifyIcons.BIBLIOGRAPHY_FILE));
