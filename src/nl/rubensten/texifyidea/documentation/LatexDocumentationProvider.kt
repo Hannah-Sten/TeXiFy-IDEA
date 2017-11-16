b06e764a8fca730ecf2092499ddde8032d0e1e02
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-11-16 08:40:56 +0100

    Fixed #238: Java/Kotlin nullability

--- src/nl/rubensten/texifyidea/documentation/LatexDocumentationProvider.kt
+++ src/nl/rubensten/texifyidea/documentation/LatexDocumentationProvider.kt
@@ -11,17 +11,17 @@ import nl.rubensten.texifyidea.psi.LatexCommands
  */
 class LatexDocumentationProvider : DocumentationProvider {
 
-    override fun getQuickNavigateInfo(psiElement: PsiElement, originalElement: PsiElement) = when (psiElement) {
+    override fun getQuickNavigateInfo(psiElement: PsiElement?, originalElement: PsiElement?) = when (psiElement) {
         is LatexCommands -> LabelDeclarationLabel(psiElement).makeLabel()
         is BibtexId -> IdDeclarationLabel(psiElement).makeLabel()
         else -> null
     }
 
-    override fun getUrlFor(psiElement: PsiElement, originalElement: PsiElement) = null
+    override fun getUrlFor(psiElement: PsiElement?, originalElement: PsiElement?) = null
 
-    override fun generateDoc(psiElement: PsiElement, originalElement: PsiElement?) = null
+    override fun generateDoc(psiElement: PsiElement?, originalElement: PsiElement?) = null
 
-    override fun getDocumentationElementForLookupItem(psiManager: PsiManager, o: Any, psiElement: PsiElement) = null
+    override fun getDocumentationElementForLookupItem(psiManager: PsiManager?, o: Any?, psiElement: PsiElement?) = null
 
-    override fun getDocumentationElementForLink(psiManager: PsiManager, s: String, psiElement: PsiElement) = null
+    override fun getDocumentationElementForLink(psiManager: PsiManager?, s: String?, psiElement: PsiElement?) = null
 }
