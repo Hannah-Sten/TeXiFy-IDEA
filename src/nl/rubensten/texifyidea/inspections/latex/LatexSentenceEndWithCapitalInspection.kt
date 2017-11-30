8a0bc4cb0d7dab25990e59b5b7bb5aa91e3ea851
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-11-30 23:34:26 +0100

    Disabled SentenceEndWithCapital inspection in verbatim environments.

--- src/nl/rubensten/texifyidea/inspections/latex/LatexSentenceEndWithCapitalInspection.kt
+++ src/nl/rubensten/texifyidea/inspections/latex/LatexSentenceEndWithCapitalInspection.kt
@@ -1,7 +1,10 @@
 package nl.rubensten.texifyidea.inspections.latex
 
+import com.intellij.psi.PsiElement
 import nl.rubensten.texifyidea.inspections.TexifyRegexInspection
 import nl.rubensten.texifyidea.psi.LatexCommands
+import nl.rubensten.texifyidea.util.inDirectEnvironment
+import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 /**
@@ -16,4 +19,12 @@ open class LatexSentenceEndWithCapitalInspection : TexifyRegexInspection(
         replacementRange = { it.start(1)..it.start(1) + 1 },
         quickFixName = { "Add an end-of-sentence space" },
         cancelIf = { matcher, file -> isInElement<LatexCommands>(matcher, file) }
-)
+) {
+
+    override fun checkContext(matcher: Matcher, element: PsiElement): Boolean {
+        if (element.inDirectEnvironment(setOf("verbatim", "verbatim*", "lstlisting"))) {
+            return false
+        }
+        return super.checkContext(matcher, element)
+    }
+}
