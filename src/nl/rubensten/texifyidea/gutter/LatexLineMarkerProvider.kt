ae1afbd0c9da22e38435b835e57a0c4aa7c8d9b4
Ruben Schellekens <r.p.schellekens@gmail.com>
2017-12-01 11:19:35 +0100

    Added configuration checkbox for second $ and upgraded to 2017.3

--- src/nl/rubensten/texifyidea/gutter/LatexLineMarkerProvider.kt
+++ src/nl/rubensten/texifyidea/gutter/LatexLineMarkerProvider.kt
@@ -10,7 +10,7 @@ import com.intellij.psi.PsiElement
 open class LatexLineMarkerProvider : RelatedItemLineMarkerProvider() {
 
     override fun collectNavigationMarkers(element: PsiElement,
-                                          result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>?) {
+                                          result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>) {
         LatexNavigationGutter().collectNavigationMarkers(element, result)
     }
 }
