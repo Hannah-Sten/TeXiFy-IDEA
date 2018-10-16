72539ab48a07e67f2dcfdb399093cac4e5b2110d
Ruben Schellekens <r.p.schellekens@gmail.com>
2018-10-16 10:17:49 +0200

    Accidentally removed fileTemplateTikz, now it's back!

--- src/nl/rubensten/texifyidea/templates/LatexTemplatesFactory.kt
+++ src/nl/rubensten/texifyidea/templates/LatexTemplatesFactory.kt
@@ -29,6 +29,7 @@ open class LatexTemplatesFactory : FileTemplateGroupDescriptorFactory {
         const val fileTemplateSty = "LaTeX Package.sty"
         const val fileTemplateCls = "LaTeX Document class.cls"
         const val fileTemplateBib = "BibTeX Bibliography.bib"
+        const val fileTemplateTikz = "TikZ Picture.tikz"
 
         @JvmStatic
         fun createFromTemplate(directory: PsiDirectory, fileName: String,
