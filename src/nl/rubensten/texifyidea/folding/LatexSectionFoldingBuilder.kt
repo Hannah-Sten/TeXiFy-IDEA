e112264288f82921d92e37af29a6e5c15ad6798c
tevers <tom.j.evers@gmail.com>
2018-12-19 14:22:11 +0100

    Working

--- src/nl/rubensten/texifyidea/folding/LatexSectionFoldingBuilder.kt
+++ src/nl/rubensten/texifyidea/folding/LatexSectionFoldingBuilder.kt
@@ -7,10 +7,7 @@ import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import nl.rubensten.texifyidea.index.LatexCommandsIndex
-import nl.rubensten.texifyidea.psi.LatexCommands
 import nl.rubensten.texifyidea.psi.LatexContent
-import nl.rubensten.texifyidea.psi.PsiContainer
-import nl.rubensten.texifyidea.util.endOffset
 import nl.rubensten.texifyidea.util.nextSiblingIgnoreWhitespace
 import nl.rubensten.texifyidea.util.parentOfType
 import nl.rubensten.texifyidea.util.previousSiblingIgnoreWhitespace
@@ -27,73 +24,54 @@ open class LatexSectionFoldingBuilder : FoldingBuilderEx() {
                 "\\paragraph", "\\subparagraph")
     }
 
-
     override fun isCollapsedByDefault(node: ASTNode) = false
 
     override fun getPlaceholderText(node: ASTNode) = node.text + "..."
 
-    override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+    override fun buildFoldRegions(root: PsiElement, document: Document,
+                                  quick: Boolean): Array<FoldingDescriptor> {
         val descriptors = ArrayList<FoldingDescriptor>()
-        val commands = LatexCommandsIndex.getCommandsByNames(root.containingFile, *sectionCommands)
-                .toList().sortedBy { x -> x.textOffset }
-
+        val commands = LatexCommandsIndex.getCommandsByNames(root.containingFile, *sectionCommands).toList()
+                .sortedBy { x -> x.textOffset }
         if (commands.isEmpty()) {
             return descriptors.toTypedArray()
         }
 
-        // Fold all section markers.
-        recursiveFold(descriptors, commands, 0, commands.size)
+        for (index_cur in 0 until commands.size) {
+            var found_higher = false
+            val cur_command = commands[index_cur]
+            val depth_cur = sectionCommands.indexOf(cur_command.name)
+            for (index_next in index_cur + 1 until commands.size) {
+                val next_command = commands[index_next]
+                val depth_next = sectionCommands.indexOf(next_command.name)
+                if (depth_cur >= depth_next) {
+                    val end = next_command.parentOfType(LatexContent::class)
+                            ?.previousSiblingIgnoreWhitespace() ?: break
+                    val folding_range = TextRange(cur_command.textOffset, end.textOffset + end.textLength)
+                    if (folding_range.length > 0) {
+                        descriptors.add(FoldingDescriptor(cur_command, folding_range))
+                    }
+                    found_higher = true
+                    break
+                }
+            }
+            /* If this item is the topmost level of the section structure or the last section marker,
+             * use the end of all text as the end of the range.
+             */
+            if (!found_higher) {
+                val content = commands.last().parentOfType(LatexContent::class) ?: continue // TODO: Check this
+                var next: LatexContent? = content
+                var previous: LatexContent = content
+                while (next != null) {
+                    previous = next
+                    next = next.nextSiblingIgnoreWhitespace() as? LatexContent
+                }
+                val range = TextRange(cur_command.textOffset, previous.textOffset + previous.textLength)
+                if (range.length > 0) {
+                    descriptors.add(FoldingDescriptor(cur_command, range))
+                }
+            }
+        }
         return descriptors.toTypedArray()
-    }
-
-    private fun recursiveFold(descriptors: MutableCollection<FoldingDescriptor>, commands: List<LatexCommands>,
-                              index_cur_toplevel: Int, index_last: Int) {
-        if (index_cur_toplevel >= index_last)
-            return
-
-        val toplevel = commands[index_cur_toplevel]
-        var index_next_toplevel = index_last
-        for (i in index_cur_toplevel + 1 until index_last) {
-            if (commands[i].name == toplevel.name) {
-                index_next_toplevel = i
-                break
-            }
-        }
-        val startOffset = toplevel.textOffset
-        val endOffset: Int
-        val lastFoldedCommand = commands[index_next_toplevel - 1]
-        if (index_next_toplevel == commands.size) {
-            val content = lastFoldedCommand.parentOfType(LatexContent::class) ?: return
-            var next: LatexContent? = content
-            var previous: LatexContent = content
-            while (next != null) {
-                previous = next
-                next = next.nextSiblingIgnoreWhitespace() as? LatexContent
-            }
-            endOffset = previous.textOffset + previous.textLength
-        } else {
-            endOffset = lastFoldedCommand.textOffset + lastFoldedCommand.textLength
-            System.err.println(lastFoldedCommand.name)
-            System.err.println(lastFoldedCommand.textLength.toString())
-        }
-
-
-        if (startOffset > endOffset) {
-            throw Exception("Startoffset = " + startOffset.toString() +
-                    "\nendoffset = " + endOffset.toString() +
-                    "\ncur = " + index_cur_toplevel.toString() + " (" + toplevel.name + ")" +
-                    "\nindex_next = " + index_next_toplevel.toString() + " (" + commands[index_next_toplevel].name + ")" +
-                    "\nindex_last = " + index_last.toString() +
-                    "\nCommand queue:" + commands.map { x -> x.name }.toString())
-        }
-
-        if (endOffset > startOffset) {
-            descriptors.add(FoldingDescriptor(toplevel.node, TextRange(startOffset, endOffset)))
-        }
-
-        if (index_cur_toplevel + 1 < index_next_toplevel) {
-            recursiveFold(descriptors, commands, index_cur_toplevel + 1, index_next_toplevel)
-        }
-        recursiveFold(descriptors, commands, index_next_toplevel, index_last)
     }
 }
