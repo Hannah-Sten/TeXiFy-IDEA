7aa2464c5ca50951433f92fa744c1b2d7cddd4d2
Thomas <hollandpirates@gmail.com>
2019-01-31 21:23:35 +0100

    Add to Code menu in non-idea IDEs

--- src/nl/rubensten/texifyidea/action/AnalyzeMenuRegistration.kt
+++ src/nl/rubensten/texifyidea/action/AnalyzeMenuRegistration.kt
@@ -12,32 +12,33 @@ import nl.rubensten.texifyidea.action.group.LatexAnalyzeMenuGroup
 /**
  * Register a menu item programmatically instead of in plugin.xml, so we can customize when it is shown or not.
  */
-class WordCountMenuRegistration : BaseComponent {
+class AnalyzeMenuRegistration : BaseComponent {
     override fun initComponent() {
         super.initComponent()
 
         // Documentation for registering an action: http://www.jetbrains.org/intellij/sdk/docs/basics/action_system.html?search=action#registering-actions-from-code
+
+        // Get the group which should be added to either the Analyze menu or something else
+        val latexAnalyzeMenuGroup = ActionManager.getInstance().getAction("texify.LatexMenuAnalyze") as DefaultActionGroup
 
         // The Analyze menu group is not present in PyCharm and probably also not in other IDEs, so we will only use it in IntelliJ.
         // In PyCharm (and others) we will use the Code group.
         val applicationName = ApplicationNamesInfo.getInstance().scriptName
 
         if (applicationName == "idea") {
-
             // Get an instance of the Analyze action group by ID
             val analyzeGroup: DefaultActionGroup = ActionManager.getInstance().getAction(IdeActions.GROUP_ANALYZE) as DefaultActionGroup
 
             analyzeGroup.addSeparator()
 
-            val latexAnalyzeMenuGroup = ActionManager.getInstance().getAction("texify.LatexMenuAnalyze") as DefaultActionGroup
-
             analyzeGroup.addAll(latexAnalyzeMenuGroup)
-        } else {
+        }
+        else {
             // Get an instance of the Analyze action group by ID
-//            val analyzeGroup: DefaultActionGroup = com.intellij.openapi.actionSystem.ActionManager.getInstance().getAction(IdeActions.????) as DefaultActionGroup
+            val analyzeGroup: DefaultActionGroup = com.intellij.openapi.actionSystem.ActionManager.getInstance().getAction("CodeMenu") as DefaultActionGroup
 
-//            analyzeGroup.addSeparator()
-//            analyzeGroup.add(action)
+            analyzeGroup.addSeparator()
+            analyzeGroup.addAll(latexAnalyzeMenuGroup)
         }
 
     }
