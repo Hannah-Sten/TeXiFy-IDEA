8f90f1bc96c07821758a43ce95c17c67e88e483c
Thomas <hollandpirates@gmail.com>
2019-01-31 11:30:15 +0100

    Register WordCountAction programmatically

--- src/nl/rubensten/texifyidea/action/group/LatexAnalyzeMenuGroup.kt
+++ src/nl/rubensten/texifyidea/action/group/LatexAnalyzeMenuGroup.kt
@@ -1,17 +1,18 @@
 package nl.rubensten.texifyidea.action.group
 
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.actionSystem.DefaultActionGroup
-import nl.rubensten.texifyidea.util.isLatexFile
+import com.intellij.openapi.actionSystem.*
+import nl.rubensten.texifyidea.action.analysis.WordCountAction
 
 /**
  * @author Ruben Schellekens
  */
 open class LatexAnalyzeMenuGroup : DefaultActionGroup() {
 
+
     override fun update(event: AnActionEvent) {
+
         val file = event.getData(CommonDataKeys.PSI_FILE)
-        event.presentation.isEnabledAndVisible = file?.isLatexFile() ?: false
+        event.presentation.isEnabledAndVisible = true
+//        event.presentation.isEnabledAndVisible = file?.isLatexFile() ?: false
     }
 }
