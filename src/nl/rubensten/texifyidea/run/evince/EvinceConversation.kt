9abcbf327fcf4b42947bb1f9b2fe9eb370526297
Thomas <hollandpirates@gmail.com>
2019-02-04 15:39:53 +0100

    Finish implementation of forward search by saving the process owner

--- src/nl/rubensten/texifyidea/run/evince/EvinceConversation.kt
+++ src/nl/rubensten/texifyidea/run/evince/EvinceConversation.kt
@@ -6,6 +6,8 @@ import org.gnome.evince.Daemon
 import java.io.File
 import java.io.IOException
 import java.util.concurrent.TimeUnit
+import com.github.hypfvieh.util.SystemUtil
+import nl.rubensten.texifyidea.TeXception
 
 /**
  * Indicates whether Evince is installed and available.
@@ -58,15 +60,20 @@ object EvinceConversation {
      * D-Bus object which allows us to execute the FindDocument function, which is exported on the D-Bus
      * by Evince.
      */
-    val evinceDaemonPath = "/org/gnome/evince/Daemon"
+    private const val evinceDaemonPath = "/org/gnome/evince/Daemon"
 
     /**
      * Object name of the Evince daemon.
      */
-    val evinceDaemonName = "org.gnome.evince.Daemon"
+    private const val evinceDaemonName = "org.gnome.evince.Daemon"
 
     /**
-     * Open a file in Evince, starting it if it is not running yet.
+     * This variable will hold the latest known Evince process owner. We need to know the owner of the pdf file in order to execute forward search.
+     */
+    private var processOwner: String? = null
+
+    /**
+     * Open a file in Evince, starting it if it is not running yet. This also finds the process owner of the pdf, so we can execute forward search later.
      */
     fun openFile(pdfFilePath: String, newWindow: Boolean = false, focus: Boolean = false, forceRefresh: Boolean = false) {
         // todo what happens when the command fails?
@@ -74,33 +81,48 @@ object EvinceConversation {
         // todo is filepath full path or relative? need working dir in second case
         // todo focus (always on top) or not depends on DE? https://unix.stackexchange.com/questions/36998/open-pdf-previewer-width-specific-size-and-position-and-always-on-top-from-com
         // Will do nothing if file is already open in Evince
-        "evince $pdfFilePath".runCommand()
+        findProcessOwner(pdfFilePath)
     }
 
     /**
      * Execute forward search, highlighting a certain line in Evince.
-     * Will also open Evince on the given pdf file.
+     * If a pdf file is given, it will execute FindDocument and open the pdf file again to find the latest process owner. If the pdf file is already open, this will do nothing.
      *
      * @param pdfFilePath Full path to a pdf file.
      * @param sourceFilePath Full path to the LaTeX source file.
      * @param line Line number in the source file to highlight in the pdf.
      */
     fun forwardSearch(pdfFilePath: String? = null, sourceFilePath: String, line: Int) {
+        if (pdfFilePath != null) {
+            findProcessOwner(pdfFilePath)
+        }
+
+        if (processOwner != null) {
+            // Theoretically we should use the Java D-Bus bindings as well to call SyncView, but that did
+            // not succeed with a NoReply exception, so we will execute a command via the shell
+            val command = "gdbus call --session --dest $processOwner --object-path /org/gnome/evince/Window/0 --method org.gnome.evince.Window.SyncView $sourceFilePath '($line, 1)' 0"
+            Runtime.getRuntime().exec(arrayOf("bash", "-c", command))
+        } else {
+            throw TeXception("Could not execute forward search with Evince because something went wrong when finding the pdf file.")
+        }
+    }
+
+    /**
+     * Execute FindDocument on the D-Bus in order to find the process owner of the given pdf file, i.e. the process name which we can use for
+     * forward/inverse search.
+     * The value found will be saved for later use.
+     *
+     * @param pdfFilePath Full path to the pdf file to find the owner of.
+     */
+    private fun findProcessOwner(pdfFilePath: String) {
         // Initialize a session bus
         val connection = DBusConnection.getConnection(DBusConnection.DBusBusType.SESSION)
 
         // Get the Daemon object using its bus name and object path
         val daemon = connection.getRemoteObject(evinceDaemonName, evinceDaemonPath, Daemon::class.java)
 
-        // If no pdf file is given, try to guess it
-        val pdfFile = pdfFilePath ?: sourceFilePath.dropLast(4) + ".pdf"
-
         // Call the method on the D-Bus by using the function we defined in the Daemon interface
-        val documentProcessOwner = daemon.FindDocument("file://$pdfFile", true)
-
-        // Theoretically we should use the Java D-Bus bindings as well to call SyncView, but that did
-        // not succeed with a NoReply exception, so we will execute a command via the shell.
-        val command = "gdbus call --session --dest $documentProcessOwner --object-path /org/gnome/evince/Window/0 --method org.gnome.evince.Window.SyncView $sourceFilePath '($line, 1)' 0"
-        Runtime.getRuntime().exec(arrayOf("bash", "-c", command))
+        processOwner = daemon.FindDocument("file://$pdfFilePath", true)
     }
+
 }
