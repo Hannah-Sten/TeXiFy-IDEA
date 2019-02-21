15cb615b98d89b1d9676b059618027bbb7b40d5a
Michael Schweiker <michael@gtschweiker.de>
2019-02-21 16:34:25 +0100

    added comments to the intention

--- src/nl/rubensten/texifyidea/intentions/LatexLabelDefiningNewCommand.kt
+++ src/nl/rubensten/texifyidea/intentions/LatexLabelDefiningNewCommand.kt
@@ -17,12 +17,14 @@ open class LatexLabelDefiningNewCommand : TexifyIntentionBase("Add label definin
             return false
         }
 
+        // check if selected part is \label
         val element = file.findElementAt(editor.caretModel.offset) ?: return false
         val selected = element as? LatexCommands ?: element.parentOfType(LatexCommands::class) ?: return false
         if (selected.name != "\\label") {
             return false
         }
 
+        // check if command is in \newcommand command
         val parentElement = selected.parent
         val parent = parentElement as? LatexCommands ?:
                     parentElement.parentOfType(LatexCommands::class) ?: return false
@@ -32,6 +34,7 @@ open class LatexLabelDefiningNewCommand : TexifyIntentionBase("Add label definin
         }
 
         val parameter = parent.requiredParameter(0) ?: return false
+        // check if there is already an entry wih this name
         return !settings.labelCommands.containsKey(parameter)
     }
 
@@ -40,29 +43,38 @@ open class LatexLabelDefiningNewCommand : TexifyIntentionBase("Add label definin
             return
         }
 
+        // get current label
         val element = file.findElementAt(editor.caretModel.offset) ?: return
         val selected = element as? LatexCommands ?: element.parentOfType(LatexCommands::class) ?: return
         if (selected.name != "\\label") {
             return
         }
 
+        // get parent command
         val parent = selected.parentOfType(LatexCommands::class) ?: return
         if (parent.name != "\\newcommand") {
             return
         }
 
+        // get position of label command in children list
         val childCommands = parent.childrenOfType(LatexCommands::class)
         val firstLabel = childCommands.indexOfFirst { it.name == "\\label" }
         val childCommandBeforeLabel = childCommands.take(firstLabel)
 
-
+        // get the name of the command
         val commandName = parent.requiredParameter(0) ?: return
+        // get the position of the label in new command, identified by the argument number of the first required parameter
+        // of label command
         val position = selected.requiredParameter(0)?.replace("#", "")
                 ?.toIntOrNull() ?: 1
+        // check if any command before the label command increases an counter which could be labeled to set the checkbox
+        // to a possible correct status
         val labelAnyCommand = childCommandBeforeLabel.none { it.name in Magic.Command.increasesCounter }
 
+        // initialize the dialog with computed values
         val newCommand = EditLabelDefiningCommand(commandName, position, labelAnyCommand)
         if (newCommand.showAndGet()) {
+            // save the computed values
             settings.addCommand(LabelingCommandInformation(newCommand.getCommandName(), newCommand.getCommandPosition(),
                     newCommand.getLabelAnyPrevCommand()))
         }
