7b0bea49fe31333c2e09ba60c37b36152dbecbb3
Ruben Schellekens <r.p.schellekens@gmail.com>
2019-04-16 16:02:53 +0200

    Removed unnecessary DumbAware

--- src/nl/rubensten/texifyidea/action/group/SumatraActionGroup.kt
+++ src/nl/rubensten/texifyidea/action/group/SumatraActionGroup.kt
@@ -2,13 +2,12 @@ package nl.rubensten.texifyidea.action.group
 
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.DefaultActionGroup
-import com.intellij.openapi.project.DumbAware
 import nl.rubensten.texifyidea.run.isSumatraAvailable
 
 /**
  * @author Ruben Schellekens, Sten Wessel
  */
-class SumatraActionGroup : DefaultActionGroup(), DumbAware {
+class SumatraActionGroup : DefaultActionGroup() {
 
     override fun canBePerformed(context: DataContext) = isSumatraAvailable
 
