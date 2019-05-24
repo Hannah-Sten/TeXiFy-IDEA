1c890aee8e7bfaec8c54399901e09304630b5374
Klaus Greff <klaus.greff@startmail.com>
2019-05-24 16:49:37 +0200

    Ignore cref and autoref for nbsp inspection
    The commands \cref, \cpageref and \autoref automatically produce the name of the section/figure/etc..
    Therefore there is no need for a non-breaking space before them.

--- src/nl/rubensten/texifyidea/inspections/latex/LatexNonBreakingSpaceInspection.kt
+++ src/nl/rubensten/texifyidea/inspections/latex/LatexNonBreakingSpaceInspection.kt
@@ -30,7 +30,7 @@ open class LatexNonBreakingSpaceInspection : TexifyInspectionBase() {
         /**
          * All commands that should not have a forced breaking space.
          */
-        val IGNORED_COMMANDS = setOf("\\citet", "\\citet*", "\\Citet", "\\Citet*")
+        val IGNORED_COMMANDS = setOf("\\citet", "\\citet*", "\\Citet", "\\Citet*", "\\cref", "\\cpageref", "\\autoref")
     }
 
     override fun getInspectionGroup() = InsightGroup.LATEX
