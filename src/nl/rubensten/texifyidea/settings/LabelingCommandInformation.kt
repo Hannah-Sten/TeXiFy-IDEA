ec3c2555c690ff9e0275e7ec199cc0755b8c4923
Michael Schweiker <michael@gtschweiker.de>
2019-06-06 14:51:15 +0200

    added comments for properties

--- src/nl/rubensten/texifyidea/settings/LabelingCommandInformation.kt
+++ src/nl/rubensten/texifyidea/settings/LabelingCommandInformation.kt
@@ -1,15 +1,23 @@
 package nl.rubensten.texifyidea.settings
 
-data class LabelingCommandInformation(var commandName: String, var position: Int, var labelsPreviousCommand: Boolean) {
+data class LabelingCommandInformation(
+        // name of the command which could label other commands
+        var commandName: String,
+        // position of the parameter which contains the label
+        var position: Int,
+        // information about the ability to label commands like '\section' before it
+        var labelsPreviousCommand: Boolean
+) {
 
     companion object {
-    
+
         /**
          * Needed to get back the object from the string in the xml file
          */
-        @JvmStatic fun fromString(string: CharSequence): LabelingCommandInformation {
+        @JvmStatic
+        fun fromString(string: CharSequence): LabelingCommandInformation {
             val parts = string.split(";")
-            require (parts.size == 3) {
+            require(parts.size == 3) {
                 "String must contain exactly three parts, got ${parts.size}"
             }
             val position = parts[1].toIntOrNull() ?: error("Second part must be type Int")
