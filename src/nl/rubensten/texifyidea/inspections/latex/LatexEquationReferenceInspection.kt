afae085b204cbb22bf5df4164c60c30bbe89b700
Abby Berkers <abbyberkers@gmail.com>
2019-06-07 13:13:47 +0200

    Only suggest \eqref when label is defined in math environment

--- src/nl/rubensten/texifyidea/inspections/latex/LatexEquationReferenceInspection.kt
+++ src/nl/rubensten/texifyidea/inspections/latex/LatexEquationReferenceInspection.kt
@@ -5,19 +5,25 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import nl.rubensten.texifyidea.inspections.TexifyRegexInspection
 import nl.rubensten.texifyidea.lang.Package
-import nl.rubensten.texifyidea.util.PackageUtils
 import nl.rubensten.texifyidea.util.document
+import nl.rubensten.texifyidea.util.findLabels
+import nl.rubensten.texifyidea.util.findOuterMathEnvironment
 import nl.rubensten.texifyidea.util.insertUsepackage
 import java.util.regex.Pattern
 
-class LatexEquationReferenceInspection : TexifyRegexInspection(
+open class LatexEquationReferenceInspection : TexifyRegexInspection(
         inspectionDisplayName = "Use of (\\ref{...}) instead of \\eqref{...}",
         myInspectionId = "EquationReference",
         errorMessage = { "Use \\eqref" },
         pattern = Pattern.compile("(\\(\\\\ref\\{)([\\w:]+)(}\\))"),
         quickFixName = { "Replace with \\eqref" },
-        groupFetcher = { listOf(it.group(2))}
+        groupFetcher = { listOf(it.group(2)) },
+        cancelIf = { matcher, psiFile ->
+            // Cancel if the label was defined outside a math environment.
+            psiFile.findLabels().find { it.text == "\\label{${matcher.group(2)}}" }.findOuterMathEnvironment() == null
+        }
 ) {
+
     override fun applyFix(project: Project, descriptor: ProblemDescriptor, replacementRange: IntRange, replacement: String, groups: List<String>): Int {
         val file = descriptor.psiElement as PsiFile
         val document = file.document() ?: return 0
