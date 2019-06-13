eb3d00ea1e18c5e533a47a7e2fb8395ce2ffe6a7
Ruben Schellekens <r.p.schellekens@gmail.com>
2019-06-13 12:00:03 +0200

    Fixed #929: group disappearing after autocompletion of an include.
    Problem: the 2nd group is seen as part of a LatexCommands and the
    insertion handler overwrites the whole command.
    
    Solution: Only overwrite until the end of the first required argument.

--- src/nl/rubensten/texifyidea/completion/handlers/FileNameInsertionHandler.kt
+++ src/nl/rubensten/texifyidea/completion/handlers/FileNameInsertionHandler.kt
@@ -4,10 +4,8 @@ import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElement
 import nl.rubensten.texifyidea.psi.LatexCommands
-import nl.rubensten.texifyidea.util.document
-import nl.rubensten.texifyidea.util.endOffset
-import nl.rubensten.texifyidea.util.parentOfType
-import nl.rubensten.texifyidea.util.removeFileExtension
+import nl.rubensten.texifyidea.psi.LatexRequiredParam
+import nl.rubensten.texifyidea.util.*
 
 /**
  * @author Ruben Schellekens
@@ -22,11 +20,13 @@ open class FileNameInsertionHandler : InsertHandler<LookupElement> {
         val normalTextWord = file.findElementAt(offset) ?: return
         val command = normalTextWord.parentOfType(LatexCommands::class) ?: return
 
-        if (command.name != "\\include" && command.name != "\\bibliography" && command.name != "\\addbibresource") {
-            return
-        }
+        if (command.name != "\\include" && command.name != "\\bibliography" && command.name != "\\addbibresource") return
+
+        // Only replace the first command argument.
+        val firstRequiredArgument = command.firstChildOfType(LatexRequiredParam::class) ?: return
+        val endOffset = firstRequiredArgument.endOffset()
 
         val extensionless = text.toString().removeFileExtension()
-        document.replaceString(command.textOffset, command.endOffset(), "${command.name}{$extensionless}")
+        document.replaceString(command.textOffset, endOffset, "${command.name}{$extensionless}")
     }
 }
