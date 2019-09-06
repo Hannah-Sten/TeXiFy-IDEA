f37d4d9c61dccdd9369a3f68781fb05569442d38
Thomas <hollandpirates@gmail.com>
2019-09-06 07:24:28 +0200

    Improve evince error message

--- src/nl/hannahsten/texifyidea/run/evince/EvinceConversation.kt
+++ src/nl/hannahsten/texifyidea/run/evince/EvinceConversation.kt
@@ -99,7 +99,7 @@ object EvinceConversation {
             Runtime.getRuntime().exec(arrayOf("bash", "-c", command))
         }
         else {
-            throw TeXception("Could not execute forward search with Evince because something went wrong when finding the pdf file.")
+            throw TeXception("Could not execute forward search with Evince because something went wrong when finding the pdf file at $pdfFilePath")
         }
     }
 
