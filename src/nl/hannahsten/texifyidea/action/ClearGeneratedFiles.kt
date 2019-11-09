a69775cee8bf664fbe4fef4c80ba9b565bd7ecb4
Thomas <hollandpirates@gmail.com>
2019-11-09 15:44:45 +0100

    Remove dependency on apache FileUtils

--- src/nl/hannahsten/texifyidea/action/ClearGeneratedFiles.kt
+++ src/nl/hannahsten/texifyidea/action/ClearGeneratedFiles.kt
@@ -4,7 +4,6 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.vfs.LocalFileSystem
 import nl.hannahsten.texifyidea.util.Magic
-import org.apache.commons.io.FileUtils
 import java.io.File
 
 /**
@@ -13,8 +12,9 @@ import java.io.File
 class ClearGeneratedFiles : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = getEventProject(e) ?: return
-        val files = FileUtils.listFiles(File(project.basePath!!), Magic.File.generatedFileTypes, true)
-        files.forEach { it.delete() }
+        File(project.basePath!!).walk().filter { it.isFile }
+                .filter { it.extension in Magic.File.generatedFileTypes }
+                .forEach { it.delete() }
         // todo minted files in _minted-*/*
         // todo dialog
         LocalFileSystem.getInstance().refresh(true)
