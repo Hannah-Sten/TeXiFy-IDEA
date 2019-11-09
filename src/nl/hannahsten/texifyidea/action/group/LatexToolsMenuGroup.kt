9b6963f08bcbb3d1a97d35ed53153562b6c2193a
Thomas <hollandpirates@gmail.com>
2019-11-09 17:27:17 +0100

    Remove apache dependency

--- src/nl/hannahsten/texifyidea/action/group/LatexToolsMenuGroup.kt
+++ src/nl/hannahsten/texifyidea/action/group/LatexToolsMenuGroup.kt
@@ -3,8 +3,6 @@ package nl.hannahsten.texifyidea.action.group
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
-import nl.hannahsten.texifyidea.util.files.isLatexFile
-import org.apache.commons.io.FileUtils
 import java.io.File
 
 /**
@@ -15,7 +13,6 @@ open class LatexToolsMenuGroup : DefaultActionGroup() {
     override fun update(event: AnActionEvent) {
         // Show menu when any LaTeX files are in the project
         val basePath = AnAction.getEventProject(event)?.basePath ?: return
-        val files = FileUtils.listFiles(File(basePath), arrayOf("tex"), true)
-        event.presentation.isEnabledAndVisible = !files.isEmpty()
+        event.presentation.isEnabledAndVisible = File(basePath).walk().any { it.extension == "tex" }
     }
 }
