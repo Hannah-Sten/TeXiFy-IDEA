a91260a4fde1b1462156de0358738a67cef4f2fe
Thomas <hollandpirates@gmail.com>
2019-12-31 16:30:50 +0100

    Regenerate part of the LaTeX parser. This commit should not change any behaviour.

--- gen/nl/hannahsten/texifyidea/psi/impl/LatexDisplayMathImpl.java
+++ gen/nl/hannahsten/texifyidea/psi/impl/LatexDisplayMathImpl.java
@@ -1,15 +1,19 @@
 // This is a generated file. Not intended for manual editing.
 package nl.hannahsten.texifyidea.psi.impl;
 
+import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static nl.hannahsten.texifyidea.psi.LatexTypes.*;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import nl.hannahsten.texifyidea.psi.*;
 
 public class LatexDisplayMathImpl extends ASTWrapperPsiElement implements LatexDisplayMath {
 
-  public LatexDisplayMathImpl(ASTNode node) {
+  public LatexDisplayMathImpl(@NotNull ASTNode node) {
     super(node);
   }
 
