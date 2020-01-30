8d552859f4eb2617c5d87e523fc330cb109ac36e
Thomas <hollandpirates@gmail.com>
2020-01-30 17:17:01 +0100

    Ignore \ifoot, fixes #1219

--- src/nl/hannahsten/texifyidea/util/Magic.kt
+++ src/nl/hannahsten/texifyidea/util/Magic.kt
@@ -409,7 +409,7 @@ object Magic {
          * All commands that at first glance look like \if-esque commands, but that actually aren't.
          */
         @JvmField
-        val ignoredIfs = hashSetOf("\\newif", "\\iff", "\\ifthenelse", "\\iftoggle")
+        val ignoredIfs = hashSetOf("\\newif", "\\iff", "\\ifthenelse", "\\iftoggle", "\\ifoot")
 
         /**
          * List of all TeX style primitives.
