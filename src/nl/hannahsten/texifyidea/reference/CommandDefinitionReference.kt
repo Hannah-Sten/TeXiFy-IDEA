a4ba097bc288daacf789f1871eeed42843e993e6
Thomas <hollandpirates@gmail.com>
2020-02-06 16:58:26 +0100

    Take renew commands into account

--- src/nl/hannahsten/texifyidea/reference/CommandDefinitionReference.kt
+++ src/nl/hannahsten/texifyidea/reference/CommandDefinitionReference.kt
@@ -6,6 +6,7 @@ import nl.hannahsten.texifyidea.index.LatexDefinitionIndex
 import nl.hannahsten.texifyidea.psi.LatexCommands
 import nl.hannahsten.texifyidea.util.Magic
 import nl.hannahsten.texifyidea.util.childrenOfType
+import nl.hannahsten.texifyidea.util.parentsOfType
 import nl.hannahsten.texifyidea.util.projectSearchScope
 
 /**
@@ -20,22 +21,28 @@ class CommandDefinitionReference(element: LatexCommands) : PsiReferenceBase<Late
 
     // Find all command definitions and redefinitions which define the current element
     override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
-        return LatexDefinitionIndex.getCommandsByNames(Magic.Command.commandDefinitions, element.project, element.project.projectSearchScope)
-                .filter { it.requiredParameters.firstOrNull() == element.name }
-                .mapNotNull {newcommand ->
-                    // Find the command being defined, e.g. \hi in case of \newcommand{\hi}{}
-                    // We should resolve to \hi, not to \newcommand, because otherwise the find usages will try to find references to the \hi definition and won't find anything because the references point to the \newcommand
-                    val definedCommand = newcommand.childrenOfType<LatexCommands>().firstOrNull()
+        val definitionsAndRedefinitions = Magic.Command.commandDefinitions + Magic.Command.redefinitions
 
-                    // Don't resolve to itself
-                    if (definedCommand == null || definedCommand == element) {
-                        null
+        // Don't resolve to a definition when you are in a \newcommand
+        if (element.parentsOfType<LatexCommands>().any { it.name in definitionsAndRedefinitions }) {
+            return emptyArray()
+        }
+        else {
+            return LatexDefinitionIndex.getCommandsByNames(definitionsAndRedefinitions, element.project, element.project.projectSearchScope)
+                    .filter { it.requiredParameters.firstOrNull() == element.name }
+                    .mapNotNull { newcommand ->
+                        // Find the command being defined, e.g. \hi in case of \newcommand{\hi}{}
+                        // We should resolve to \hi, not to \newcommand, because otherwise the find usages will try to find references to the \hi definition and won't find anything because the references point to the \newcommand
+                        val definedCommand = newcommand.childrenOfType<LatexCommands>().firstOrNull()
+
+                        if (definedCommand == null) {
+                            null
+                        } else {
+                            PsiElementResolveResult(definedCommand as PsiElement)
+                        }
                     }
-                    else {
-                        PsiElementResolveResult(definedCommand as PsiElement)
-                    }
-                }
-                .toArray(emptyArray())
+                    .toArray(emptyArray())
+        }
     }
 
     override fun resolve(): PsiElement? {
