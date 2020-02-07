c61c2ef7f4e0de30b98f5f8f992038c41d180a06
Abby Berkers <abbyberkers@gmail.com>
2020-02-07 21:45:12 +0100

    Move methods from BibtexPsiImplUtil to BibtexEntryUtil.kt

--- src/nl/hannahsten/texifyidea/psi/BibtexPsiImplUtil.java
+++ src/nl/hannahsten/texifyidea/psi/BibtexPsiImplUtil.java
@@ -1,28 +1,21 @@
 package nl.hannahsten.texifyidea.psi;
 
-import com.intellij.openapi.paths.WebReference;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import nl.hannahsten.texifyidea.index.stub.BibtexEntryStub;
 import nl.hannahsten.texifyidea.reference.BibtexStringReference;
 import nl.hannahsten.texifyidea.util.BibtexKt;
-import nl.hannahsten.texifyidea.util.Magic;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * This class is used for method injection in generated classes.
  * Documentation can be found at https://github.com/JetBrains/Grammar-Kit/blob/master/HOWTO.md#34-implement-interface-via-method-injection.
  */
 public class BibtexPsiImplUtil {
-    static final Set<String> URL_COMMANDS = Magic.Command.bibUrls;
 
     /**
      * Get a reference to the declaration of the string variable.
@@ -32,27 +25,7 @@ public class BibtexPsiImplUtil {
     }
 
     public static PsiReference[] getReferences(@NotNull BibtexEntry element) {
-        // Get all references from a URL to a website.
-        if (URL_COMMANDS.stream().anyMatch(urlTag -> !element.getTagContent(urlTag).isEmpty())) {
-            String contentText = element.getEntryContent().getText();
-            // Compute all the ranges that contain a url, relative to the content of the entry.
-            List<TextRange> rangesInParent = URL_COMMANDS.stream().map(urlTag ->
-                    TextRange.from(contentText.indexOf(element.getTagContent(urlTag)), element.getTagContent(urlTag).length())
-            ).collect(Collectors.toList());
-
-            return extractWebReferences(rangesInParent, element);
-        }
-        return new PsiReference[0];
-    }
-
-    private static PsiReference[] extractWebReferences(List<TextRange> rangesInParent, @NotNull BibtexEntry element) {
-        List<PsiReference> references = new ArrayList<>();
-        for (TextRange range : rangesInParent) {
-            references.add(new WebReference(
-                    element, range.shiftRight(element.getEntryContent().getTextOffset() - element.getTextOffset())
-            ));
-        }
-        return references.toArray(new PsiReference[references.size()]);
+        return BibtexEntryUtilKt.getReferences(element);
     }
 
     public static PsiElement setName(@NotNull BibtexEntry element, @NotNull @NonNls String name) {
@@ -102,23 +75,6 @@ public class BibtexPsiImplUtil {
     }
 
     public static String getTagContent(@NotNull BibtexEntry element, String tagName) {
-        BibtexEntryContent entryContent = element.getEntryContent();
-        if (entryContent == null) return "";
-
-        for (BibtexTag bibtexTag : entryContent.getTagList()) {
-            BibtexContent content = bibtexTag.getContent();
-            if (tagName.equalsIgnoreCase(bibtexTag.getKey().getText())) {
-                String text = BibtexKt.evaluate(content);
-
-                // sanitise double braced strings
-                if (text.charAt(0) == '{' && text.charAt(text.length()-1) == '}') {
-                    return text.substring(1, text.length() - 1);
-                }
-
-                return text;
-            }
-        }
-
-        return "";
+        return BibtexEntryUtilKt.getTagContent(element, tagName);
     }
 }
