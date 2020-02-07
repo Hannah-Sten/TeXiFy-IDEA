64190bde06ad6eb7a28853a955a5f5ab0d4907cf
Abby Berkers <abbyberkers@gmail.com>
2020-02-07 09:14:16 +0100

    Convert all package names to lowercase before comparing them

--- src/nl/hannahsten/texifyidea/inspections/latex/LatexPackageCouldNotBeFound.kt
+++ src/nl/hannahsten/texifyidea/inspections/latex/LatexPackageCouldNotBeFound.kt
@@ -22,15 +22,17 @@ class LatexPackageCouldNotBeFound : TexifyInspectionBase() {
 
     override fun inspectFile(file: PsiFile, manager: InspectionManager, isOntheFly: Boolean): List<ProblemDescriptor> {
         val descriptors = descriptorList()
-        val ctanPackages = PackageUtils.CTAN_PACKAGE_NAMES
-        val customPackages = LatexDefinitionIndex.getCommandsByName("\\ProvidesPackage", file.project, file.project.projectSearchScope).map { it.requiredParameter(0) }
+        val ctanPackages = PackageUtils.CTAN_PACKAGE_NAMES.map { it.toLowerCase() }
+        val customPackages = LatexDefinitionIndex.getCommandsByName("\\ProvidesPackage", file.project, file.project.projectSearchScope)
+                .map { it.requiredParameter(0) }
+                .map { it?.toLowerCase() }
         val packages = ctanPackages + customPackages
 
         val commands = file.childrenOfType(LatexCommands::class)
                 .filter { it.name == "\\usepackage" || it.name == "\\RequirePackage" }
 
         for (command in commands) {
-            val `package` = command.requiredParameters.first()
+            val `package` = command.requiredParameters.first().toLowerCase()
             if (!packages.contains(`package`)) {
                 descriptors.add(manager.createProblemDescriptor(
                         command,
