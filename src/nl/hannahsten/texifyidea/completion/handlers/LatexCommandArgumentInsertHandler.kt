c7a70288a4205862aee8857166667abb7507ffa0
Abby Berkers <abbyberkers@gmail.com>
2020-02-13 08:29:58 +0100

    Convert LatexCommandArgumentInsertHandler from Java to Kotlin

--- src/nl/hannahsten/texifyidea/completion/handlers/LatexCommandArgumentInsertHandler.kt
+++ src/nl/hannahsten/texifyidea/completion/handlers/LatexCommandArgumentInsertHandler.kt
@@ -1,108 +1,97 @@
-package nl.hannahsten.texifyidea.completion.handlers;
+package nl.hannahsten.texifyidea.completion.handlers
 
-import com.intellij.codeInsight.completion.InsertHandler;
-import com.intellij.codeInsight.completion.InsertionContext;
-import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.util.TextRange;
-import nl.hannahsten.texifyidea.lang.LatexMathCommand;
-import nl.hannahsten.texifyidea.lang.LatexRegularCommand;
-import nl.hannahsten.texifyidea.psi.LatexCommands;
-
-import java.util.LinkedList;
-import java.util.List;
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.openapi.editor.CaretModel
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.TextRange
+import nl.hannahsten.texifyidea.lang.LatexMathCommand
+import nl.hannahsten.texifyidea.lang.LatexRegularCommand
+import nl.hannahsten.texifyidea.psi.LatexCommands
+import java.util.*
 
 /**
  * @author Hannah Schellekens
  */
-public class LatexCommandArgumentInsertHandler implements InsertHandler<LookupElement> {
-
-    @Override
-    public void handleInsert(InsertionContext insertionContext, LookupElement lookupElement) {
-        Object object = lookupElement.getObject();
-
-        if (object instanceof LatexCommands) {
-            insertCommands((LatexCommands)object, insertionContext);
-        }
-        else if (object instanceof LatexMathCommand) {
-            insertMathCommand((LatexMathCommand)object, insertionContext);
-        }
-        else if (object instanceof LatexRegularCommand) {
-            insertNoMathCommand((LatexRegularCommand)object, insertionContext);
-        }
-    }
-
-    private void insertCommands(LatexCommands commands, InsertionContext context) {
-        List<String> optional = new LinkedList<>(commands.getOptionalParameters().keySet());
-        if (optional.isEmpty()) {
-            return;
-        }
-
-        int cmdParameterCount = 0;
-        try {
-            cmdParameterCount = Integer.parseInt(optional.get(0));
-        } catch (NumberFormatException ignore) {
-        }
-
-        if (cmdParameterCount > 0) {
-            insert(context, "{}");
-        }
-    }
-
-    private void insertMathCommand(LatexMathCommand mathCommand, InsertionContext context) {
-        if (mathCommand.autoInsertRequired()) {
-            insert(context, mathCommand.getCommand());
-        }
-    }
-
-    private void insertNoMathCommand(LatexRegularCommand noMathCommand, InsertionContext context) {
-        if (noMathCommand.autoInsertRequired()) {
-            insert(context, noMathCommand.getCommand());
-        }
-    }
-
-    private void insert(InsertionContext context, String text) {
-        Editor editor = context.getEditor();
-        Document document = editor.getDocument();
-        CaretModel caret = editor.getCaretModel();
-        int offset = caret.getOffset();
-
-        // When not followed by {}, insert {}.
-        if (offset >= document.getTextLength() - 1 ||
-                !document.getText(TextRange.from(offset, 1)).equals("{")) {
-            insertSquigglyBracketPair(editor, caret);
-        }
-        else {
-            skipSquigglyBrackets(editor, caret);
-        }
-    }
-
-    private void skipSquigglyBrackets(Editor editor, CaretModel caret) {
-        Document document = editor.getDocument();
-        int offset = caret.getOffset();
-
-        int depth = 0;
-        for (int i = offset; i < editor.getDocument().getTextLength(); i++) {
-            String text = document.getText(TextRange.from(i, 1));
-
-            switch (text) {
-                case "{":
-                    depth++;
-                    break;
-                case "}":
-                    if (--depth == 0) {
-                        caret.moveToOffset(i + 1);
-                        return;
-                    }
-                default:
+class LatexCommandArgumentInsertHandler : InsertHandler<LookupElement> {
+    override fun handleInsert(insertionContext: InsertionContext, lookupElement: LookupElement) {
+        when (val `object` = lookupElement.getObject()) {
+            is LatexCommands -> {
+                insertCommands(`object`, insertionContext)
+            }
+            is LatexMathCommand -> {
+                insertMathCommand(`object`, insertionContext)
+            }
+            is LatexRegularCommand -> {
+                insertNoMathCommand(`object`, insertionContext)
             }
         }
     }
 
-    private void insertSquigglyBracketPair(Editor editor, CaretModel caret) {
-        editor.getDocument().insertString(caret.getOffset(), "{}");
-        caret.moveToOffset(caret.getOffset() + 1);
+    private fun insertCommands(commands: LatexCommands, context: InsertionContext) {
+        val optional: List<String> = LinkedList(commands.optionalParameters
+                .keys)
+        if (optional.isEmpty()) return
+
+        var cmdParameterCount = 0
+        try {
+            cmdParameterCount = optional[0].toInt()
+        } catch (ignore: NumberFormatException) {
+        }
+
+        if (cmdParameterCount > 0) {
+            insert(context, "{}")
+        }
+    }
+
+    private fun insertMathCommand(mathCommand: LatexMathCommand, context: InsertionContext) {
+        if (mathCommand.autoInsertRequired()) {
+            insert(context, mathCommand.command)
+        }
+    }
+
+    private fun insertNoMathCommand(noMathCommand: LatexRegularCommand, context: InsertionContext) {
+        if (noMathCommand.autoInsertRequired()) {
+            insert(context, noMathCommand.command)
+        }
+    }
+
+    private fun insert(context: InsertionContext, text: String) {
+        val editor = context.editor
+        val document = editor.document
+        val caret = editor.caretModel
+        val offset = caret.offset
+
+        // When not followed by {}, insert {}.
+        if (offset >= document.textLength - 1 ||
+                document.getText(TextRange.from(offset, 1)) != "{") {
+            insertSquigglyBracketPair(editor, caret)
+        }
+        else {
+            skipSquigglyBrackets(editor, caret)
+        }
+    }
+
+    private fun skipSquigglyBrackets(editor: Editor, caret: CaretModel) {
+        val document = editor.document
+        val offset = caret.offset
+        var depth = 0
+        for (i in offset until editor.document.textLength) {
+            when (document.getText(TextRange.from(i, 1))) {
+                "{" -> depth++
+                "}" -> if (--depth == 0) {
+                    caret.moveToOffset(i + 1)
+                    return
+                }
+                else -> {
+                }
+            }
+        }
+    }
+
+    private fun insertSquigglyBracketPair(editor: Editor, caret: CaretModel) {
+        editor.document.insertString(caret.offset, "{}")
+        caret.moveToOffset(caret.offset + 1)
     }
 }
