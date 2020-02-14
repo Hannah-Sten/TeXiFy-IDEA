7518595e824604691d8e4423d94a108fea3c8a50
Thomas <hollandpirates@gmail.com>
2020-02-14 09:23:28 +0100

    Add namesvalidator

--- src/nl/hannahsten/texifyidea/refactoring/LatexRefactoringSupportProvider.kt
+++ src/nl/hannahsten/texifyidea/refactoring/LatexRefactoringSupportProvider.kt
@@ -4,6 +4,9 @@ import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
 import nl.hannahsten.texifyidea.psi.LatexNormalText
 
+/**
+ * This class is used to enable inline refactoring.
+ */
 class LatexRefactoringSupportProvider : RefactoringSupportProvider() {
     override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
         // Label parameters are NormalText
