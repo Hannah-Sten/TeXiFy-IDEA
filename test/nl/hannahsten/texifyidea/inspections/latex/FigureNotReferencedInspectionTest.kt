c2a89037cdd1af1195d88f5387606f0428b32d90
Thomas <hollandpirates@gmail.com>
2020-02-19 21:02:00 +0100

    Handle multiple references in one command for FigureNotReferencedInspection

--- test/nl/hannahsten/texifyidea/inspections/latex/FigureNotReferencedInspectionTest.kt
+++ test/nl/hannahsten/texifyidea/inspections/latex/FigureNotReferencedInspectionTest.kt
@@ -16,4 +16,41 @@ class FigureNotReferencedInspectionTest : TexifyInspectionTestBase(LatexFigureNo
         """.trimIndent())
         myFixture.checkHighlighting(false, false, true, false)
     }
+
+    fun testFigureReferencedNoWarning() {
+        myFixture.configureByText(LatexFileType, """
+            \usepackage{listings}
+            \begin{document}
+                \begin{figure}
+                    \label{fig:some-figure}
+                \end{figure}
+                
+                \ref{fig:some-figure}
+            \end{document}
+        """.trimIndent())
+        myFixture.checkHighlighting(false, false, true, false)
+    }
+
+    fun testFigureReferencedMultipleReferencesNoWarning() {
+        myFixture.configureByText(LatexFileType, """
+            \documentclass{article}
+            \usepackage{cleveref}
+            \begin{document}
+                \begin{figure}
+                    Figure text.
+                    \caption{Caption}
+                    \label{fig:some-figure}
+                \end{figure}
+            
+                \begin{figure}
+                    Figure text.
+                    \caption{Caption2}
+                    \label{fig:some-figure2}
+                \end{figure}
+            
+                I ref~\cref{fig:some-figure,fig:some-figure2}
+            \end{document}
+        """.trimIndent())
+        myFixture.checkHighlighting(false, false, true, false)
+    }
 }
