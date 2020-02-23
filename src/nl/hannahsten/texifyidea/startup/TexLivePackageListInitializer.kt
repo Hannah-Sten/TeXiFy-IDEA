6817e10d6d42167f019454002cf1d628fc21f9cf
Abby Berkers <abbyberkers@gmail.com>
2020-02-23 12:44:03 +0100

    Add html for not installed package inspection

--- src/nl/hannahsten/texifyidea/startup/TexLivePackageListInitializer.kt
+++ src/nl/hannahsten/texifyidea/startup/TexLivePackageListInitializer.kt
@@ -2,12 +2,16 @@ package nl.hannahsten.texifyidea.startup
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import nl.hannahsten.texifyidea.util.LatexDistribution
 import nl.hannahsten.texifyidea.util.TexLivePackages
 import nl.hannahsten.texifyidea.util.runCommand
 
 class TexLivePackageListInitializer : StartupActivity {
     override fun runActivity(project: Project) {
-        val result = "tlmgr list --only-installed".runCommand() ?: return
-        TexLivePackages.packageList = Regex("i\\s(.*):").findAll(result).map { it.groupValues.last() }.toMutableList()
+        if (LatexDistribution.isTexlive) {
+            val result = "tlmgr list --only-installed".runCommand() ?: return
+            TexLivePackages.packageList = Regex("i\\s(.*):").findAll(result)
+                    .map { it.groupValues.last() }.toMutableList()
+        }
     }
 }
