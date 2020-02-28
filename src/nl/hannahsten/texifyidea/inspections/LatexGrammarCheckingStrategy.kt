914eb4e7eede963bd39b14ea3483f43ba6f46039
Thomas <hollandpirates@gmail.com>
2020-02-28 17:52:43 +0100

    Implement new Grazie strategy

--- src/nl/hannahsten/texifyidea/inspections/LatexGrammarCheckingStrategy.kt
+++ src/nl/hannahsten/texifyidea/inspections/LatexGrammarCheckingStrategy.kt
@@ -3,11 +3,9 @@ package nl.hannahsten.texifyidea.inspections
 import com.intellij.grazie.grammar.strategy.GrammarCheckingStrategy
 import com.intellij.grazie.grammar.strategy.StrategyUtils
 import com.intellij.grazie.utils.parents
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
-import nl.hannahsten.texifyidea.psi.LatexGroup
-import nl.hannahsten.texifyidea.psi.LatexMathEnvironment
-import nl.hannahsten.texifyidea.psi.LatexNormalText
-import nl.hannahsten.texifyidea.psi.LatexOpenGroup
+import nl.hannahsten.texifyidea.psi.*
 
 class LatexGrammarCheckingStrategy : GrammarCheckingStrategy {
     private fun PsiElement.isNotInMathEnvironment() = parents().none { it is LatexMathEnvironment }
@@ -15,7 +13,19 @@ class LatexGrammarCheckingStrategy : GrammarCheckingStrategy {
     private fun PsiElement.isNotInSquareBrackets() = parents().find { it is LatexGroup || it is LatexOpenGroup }
             ?.let { it is LatexGroup } ?: true
 
-    override fun isMyContextRoot(element: PsiElement) = element is LatexNormalText && element.isNotInMathEnvironment() && element.isNotInSquareBrackets()
+    override fun isMyContextRoot(element: PsiElement) = element is LatexNormalText && element.isNotInMathEnvironment() && element.isNotInSquareBrackets() || element is PsiComment
 
     override fun getStealthyRanges(root: PsiElement, text: CharSequence) = StrategyUtils.indentIndexes(text, setOf(' '))
+
+    override fun isEnabledByDefault() = true
+
+    override fun getContextRootTextDomain(root: PsiElement): GrammarCheckingStrategy.TextDomain {
+        return when (root) {
+            is PsiComment -> GrammarCheckingStrategy.TextDomain.COMMENTS
+            is LatexNormalText -> GrammarCheckingStrategy.TextDomain.PLAIN_TEXT
+            is LatexCommands -> GrammarCheckingStrategy.TextDomain.NON_TEXT
+            is LatexParameter -> GrammarCheckingStrategy.TextDomain.LITERALS
+            else -> GrammarCheckingStrategy.TextDomain.NON_TEXT
+        }
+    }
 }
