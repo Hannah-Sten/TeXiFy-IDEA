22bd3c1ba2e783d420e71c46c6b589d1835325d5
Thomas <hollandpirates@gmail.com>
2020-03-02 07:58:50 +0100

    Disable inline refactoring

--- src/nl/hannahsten/texifyidea/refactoring/BibtexRefactoringSupportProvider.kt
+++ src/nl/hannahsten/texifyidea/refactoring/BibtexRefactoringSupportProvider.kt
@@ -2,13 +2,13 @@ package nl.hannahsten.texifyidea.refactoring
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
-import nl.hannahsten.texifyidea.psi.BibtexId
 
 /**
  * This class is used to enable inline refactoring.
  */
 class BibtexRefactoringSupportProvider : RefactoringSupportProvider() {
     override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
-        return element is BibtexId
+        // Inline refactoring is not enabled for BibtexId because it will not handle the comma separator correctly when the bibtex id is renamed directly (not from usages).
+        return false
     }
 }
