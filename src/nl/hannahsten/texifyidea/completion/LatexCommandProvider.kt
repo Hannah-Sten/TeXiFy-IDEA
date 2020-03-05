e4299ff0ddc44b81021adc1ef12cc4830e59cd7b
Thomas <hollandpirates@gmail.com>
2020-03-05 17:16:16 +0100

    Convert LatexCommandProvider.java to Kotlin

--- src/nl/hannahsten/texifyidea/completion/LatexCommandProvider.kt
+++ src/nl/hannahsten/texifyidea/completion/LatexCommandProvider.kt
@@ -1,280 +1,203 @@
-package nl.hannahsten.texifyidea.completion;
+package nl.hannahsten.texifyidea.completion
 
-import com.google.common.base.Strings;
-import com.intellij.codeInsight.completion.CompletionParameters;
-import com.intellij.codeInsight.completion.CompletionProvider;
-import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.completion.InsertHandler;
-import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.ProcessingContext;
-import com.intellij.util.containers.ContainerUtil;
-import nl.hannahsten.texifyidea.TexifyIcons;
-import nl.hannahsten.texifyidea.completion.handlers.LatexCommandArgumentInsertHandler;
-import nl.hannahsten.texifyidea.completion.handlers.LatexMathInsertHandler;
-import nl.hannahsten.texifyidea.completion.handlers.LatexNoMathInsertHandler;
-import nl.hannahsten.texifyidea.index.LatexCommandsIndex;
-import nl.hannahsten.texifyidea.index.LatexDefinitionIndex;
-import nl.hannahsten.texifyidea.lang.*;
-import nl.hannahsten.texifyidea.psi.LatexCommands;
-import nl.hannahsten.texifyidea.util.Kindness;
-import nl.hannahsten.texifyidea.util.Magic;
-import nl.hannahsten.texifyidea.util.PsiCommandsKt;
-import nl.hannahsten.texifyidea.util.files.FileSetKt;
-import nl.hannahsten.texifyidea.util.files.FilesKt;
-import nl.hannahsten.texifyidea.util.files.RootFileKt;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.*;
-import java.util.stream.Collectors;
+import com.google.common.base.Strings
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.PsiFile
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.util.ProcessingContext
+import com.intellij.util.containers.ContainerUtil
+import nl.hannahsten.texifyidea.TexifyIcons
+import nl.hannahsten.texifyidea.completion.handlers.LatexCommandArgumentInsertHandler
+import nl.hannahsten.texifyidea.completion.handlers.LatexMathInsertHandler
+import nl.hannahsten.texifyidea.completion.handlers.LatexNoMathInsertHandler
+import nl.hannahsten.texifyidea.index.LatexCommandsIndex
+import nl.hannahsten.texifyidea.index.LatexDefinitionIndex
+import nl.hannahsten.texifyidea.lang.*
+import nl.hannahsten.texifyidea.psi.LatexCommands
+import nl.hannahsten.texifyidea.util.*
+import nl.hannahsten.texifyidea.util.Kindness.getKindWords
+import nl.hannahsten.texifyidea.util.files.*
+import java.util.*
+import java.util.stream.Collectors
+import kotlin.math.min
 
 /**
  * @author Hannah Schellekens, Sten Wessel
  */
-public class LatexCommandProvider extends CompletionProvider<CompletionParameters> {
-
-    private LatexMode mode;
-
-    LatexCommandProvider(LatexMode mode) {
-        this.mode = mode;
-    }
-
-    @Override
-    protected void addCompletions(@NotNull CompletionParameters parameters,
-                                  ProcessingContext context, @NotNull CompletionResultSet result) {
-        switch (mode) {
-            case NORMAL:
-                addNormalCommands(result);
-                addCustomCommands(parameters, result);
-                break;
-            case MATH:
-                addMathCommands(result);
-                addCustomCommands(parameters, result, LatexMode.MATH);
-                break;
-            case ENVIRONMENT_NAME:
-                addEnvironments(result, parameters);
-                break;
+class LatexCommandProvider internal constructor(private val mode: LatexMode) : CompletionProvider<CompletionParameters>() {
+    override fun addCompletions(parameters: CompletionParameters,
+                                context: ProcessingContext, result: CompletionResultSet) {
+        when (mode) {
+            LatexMode.NORMAL -> {
+                addNormalCommands(result)
+                addCustomCommands(parameters, result)
+            }
+            LatexMode.MATH -> {
+                addMathCommands(result)
+                addCustomCommands(parameters, result, LatexMode.MATH)
+            }
+            LatexMode.ENVIRONMENT_NAME -> addEnvironments(result, parameters)
         }
-
-        result.addLookupAdvertisement("Don't use \\\\ outside of tabular or math mode, it's evil.");
+        result.addLookupAdvertisement("Don't use \\\\ outside of tabular or math mode, it's evil.")
     }
 
-    private void addNormalCommands(CompletionResultSet result) {
+    private fun addNormalCommands(result: CompletionResultSet) {
         result.addAllElements(ContainerUtil.map2List(
-                LatexRegularCommand.values(),
-                cmd -> LookupElementBuilder.create(cmd, cmd.getCommand())
-                        .withPresentableText(cmd.getCommandDisplay())
-                        .bold()
-                        .withTailText(cmd.getArgumentsDisplay() + " " + packageName(cmd), true)
-                        .withTypeText(cmd.getDisplay())
-                        .withInsertHandler(new LatexNoMathInsertHandler())
-                        .withIcon(TexifyIcons.DOT_COMMAND)
-        ));
-        result.addLookupAdvertisement(Kindness.getKindWords());
+                LatexRegularCommand.values()
+        ) { cmd: LatexRegularCommand ->
+            LookupElementBuilder.create(cmd, cmd.command)
+                    .withPresentableText(cmd.commandDisplay)
+                    .bold()
+                    .withTailText(cmd.getArgumentsDisplay() + " " + packageName(cmd), true)
+                    .withTypeText(cmd.display)
+                    .withInsertHandler(LatexNoMathInsertHandler())
+                    .withIcon(TexifyIcons.DOT_COMMAND)
+        })
+        result.addLookupAdvertisement(getKindWords())
     }
 
-    private void addMathCommands(CompletionResultSet result) {
+    private fun addMathCommands(result: CompletionResultSet) {
         // Find all commands.
-        List<LatexCommand> commands = new ArrayList<>();
-        Collections.addAll(commands, LatexMathCommand.values());
-        commands.add(LatexRegularCommand.BEGIN);
+        val commands: MutableList<LatexCommand> = ArrayList()
+        Collections.addAll(commands, *LatexMathCommand.values())
+        commands.add(LatexRegularCommand.BEGIN)
 
         // Create autocomplete elements.
         result.addAllElements(ContainerUtil.map2List(
-                commands,
-                cmd -> {
-                    InsertHandler<LookupElement> handler = cmd instanceof LatexRegularCommand ?
-                            new LatexNoMathInsertHandler() :
-                            new LatexMathInsertHandler();
-
-
-                    return LookupElementBuilder.create(cmd, cmd.getCommand())
-                            .withPresentableText(cmd.getCommandDisplay())
-                            .bold()
-                            .withTailText(cmd.getArgumentsDisplay() + " " + packageName(cmd), true)
-                            .withTypeText(cmd.getDisplay())
-                            .withInsertHandler(handler)
-                            .withIcon(TexifyIcons.DOT_COMMAND);
-                }
-        ));
+                commands
+        ) { cmd: LatexCommand ->
+            val handler = if (cmd is LatexRegularCommand) LatexNoMathInsertHandler() else LatexMathInsertHandler()
+            LookupElementBuilder.create(cmd, cmd.command)
+                    .withPresentableText(cmd.commandDisplay)
+                    .bold()
+                    .withTailText(cmd.getArgumentsDisplay() + " " + packageName(cmd), true)
+                    .withTypeText(cmd.display)
+                    .withInsertHandler(handler)
+                    .withIcon(TexifyIcons.DOT_COMMAND)
+        })
     }
 
-    private void addEnvironments(CompletionResultSet result, CompletionParameters parameters) {
+    private fun addEnvironments(result: CompletionResultSet, parameters: CompletionParameters) {
         // Find all environments.
-        List<Environment> environments = new ArrayList<>();
-        Collections.addAll(environments, DefaultEnvironment.values());
-
-        LatexDefinitionIndex.Companion.getItemsInFileSet(parameters.getOriginalFile()).stream()
-                .filter(cmd -> Magic.Command.environmentDefinitions.contains(cmd.getName()))
-                .map(cmd -> PsiCommandsKt.requiredParameter(cmd, 0))
-                .filter(Objects::nonNull)
-                .map(SimpleEnvironment::new)
-                .forEach(environments::add);
+        val environments: MutableList<Environment> = ArrayList()
+        Collections.addAll(environments, *DefaultEnvironment.values())
+        LatexDefinitionIndex.getItemsInFileSet(parameters.originalFile).stream()
+                .filter { cmd: LatexCommands -> Magic.Command.environmentDefinitions.contains(cmd.name) }
+                .map { cmd: LatexCommands -> cmd.requiredParameter(0) }
+                .filter { obj: String? -> Objects.nonNull(obj) }
+                .map { environmentName: String? -> SimpleEnvironment(environmentName!!) }
+                .forEach { e: SimpleEnvironment -> environments.add(e) }
 
         // Create autocomplete elements.
         result.addAllElements(ContainerUtil.map2List(
-                environments,
-                env -> LookupElementBuilder.create(env, env.getEnvironmentName())
-                        .withPresentableText(env.getEnvironmentName())
-                        .bold()
-                        .withTailText(env.getArgumentsDisplay() + " " + packageName(env), true)
-                        .withIcon(TexifyIcons.DOT_ENVIRONMENT)
-        ));
-        result.addLookupAdvertisement(Kindness.getKindWords());
+                environments
+        ) { env: Environment ->
+            LookupElementBuilder.create(env, env.environmentName)
+                    .withPresentableText(env.environmentName)
+                    .bold()
+                    .withTailText(env.getArgumentsDisplay() + " " + packageName(env), true)
+                    .withIcon(TexifyIcons.DOT_ENVIRONMENT)
+        })
+        result.addLookupAdvertisement(getKindWords())
     }
 
-    private String packageName(Dependend dependend) {
-        String name = dependend.getDependency().getName();
-        if ("".equals(name)) {
-            return "";
+    private fun packageName(dependend: Dependend): String {
+        val name = dependend.dependency.name
+        return if ("" == name) {
+            ""
         }
-
-        return " (" + name + ")";
+        else " ($name)"
     }
 
-    private void addCustomCommands(CompletionParameters parameters, CompletionResultSet result) {
-        addCustomCommands(parameters, result, null);
-    }
-
-    private void addCustomCommands(CompletionParameters parameters, CompletionResultSet result,
-                                   @Nullable LatexMode mode) {
-        Project project = parameters.getEditor().getProject();
-        if (project == null) {
-            return;
-        }
-
-        PsiFile file = parameters.getOriginalFile();
-        Set<PsiFile> files = new HashSet<>(FileSetKt.referencedFileSet(file));
-        PsiFile root = RootFileKt.findRootFile(file);
-        PsiFile documentClass = FilesKt.documentClassFileInProject(root);
+    private fun addCustomCommands(parameters: CompletionParameters, result: CompletionResultSet,
+                                  mode: LatexMode? = null) {
+        val project = parameters.editor.project ?: return
+        val file = parameters.originalFile
+        val files: MutableSet<PsiFile> = HashSet(file.referencedFileSet())
+        val root = file.findRootFile()
+        val documentClass = root.documentClassFileInProject()
         if (documentClass != null) {
-            files.add(documentClass);
+            files.add(documentClass)
         }
-
-        Set<VirtualFile> searchFiles = files.stream()
-                .map(PsiFile::getVirtualFile)
-                .collect(Collectors.toSet());
-        searchFiles.add(file.getVirtualFile());
-        GlobalSearchScope scope = GlobalSearchScope.filesScope(project, searchFiles);
-
-        Collection<LatexCommands> cmds = LatexCommandsIndex.Companion.getItems(project, scope);
-
-        for (LatexCommands cmd : cmds) {
-            if (!PsiCommandsKt.isDefinition(cmd) && !PsiCommandsKt.isEnvironmentDefinition(cmd)) {
-                continue;
+        val searchFiles = files.stream()
+                .map { obj: PsiFile -> obj.virtualFile }
+                .collect(Collectors.toSet())
+        searchFiles.add(file.virtualFile)
+        val scope = GlobalSearchScope.filesScope(project, searchFiles)
+        val cmds = LatexCommandsIndex.getItems(project, scope)
+        for (cmd in cmds) {
+            if (!cmd.isDefinition() && !cmd.isEnvironmentDefinition()) {
+                continue
             }
-
-            if (mode != LatexMode.MATH && "\\DeclareMathOperator".equals(cmd.getName())) {
-                continue;
+            if (mode !== LatexMode.MATH && "\\DeclareMathOperator" == cmd.name) {
+                continue
             }
-
-            String cmdName = getCommandName(cmd);
-            if (cmdName == null) {
-                continue;
-            }
+            val cmdName = getCommandName(cmd) ?: continue
 
             // Skip over 'private' commands containing @ symbol in normal tex source files.
-            if (!FilesKt.isClassFile(file) && !FilesKt.isStyleFile(file)) {
+            if (!file.isClassFile() && !file.isStyleFile()) {
                 if (cmdName.contains("@")) {
-                    continue;
+                    continue
                 }
             }
-
-            String tailText = getTailText(cmd);
-            String typeText = getTypeText(cmd);
-
-            int line = 1 + StringUtil.offsetToLineNumber(cmd.getContainingFile().getText(), cmd.getTextOffset());
-            typeText = typeText + " " + cmd.getContainingFile().getName() + ":" + line;
-
+            val tailText = getTailText(cmd)
+            var typeText = getTypeText(cmd)
+            val line = 1 + StringUtil.offsetToLineNumber(cmd.containingFile.text, cmd.textOffset)
+            typeText = typeText + " " + cmd.containingFile.name + ":" + line
             result.addElement(LookupElementBuilder.create(cmd, cmdName.substring(1))
                     .withPresentableText(cmdName)
                     .bold()
                     .withTailText(tailText, true)
                     .withTypeText(typeText, true)
-                    .withInsertHandler(new LatexCommandArgumentInsertHandler())
+                    .withInsertHandler(LatexCommandArgumentInsertHandler())
                     .withIcon(TexifyIcons.DOT_COMMAND)
-            );
+            )
         }
-        result.addLookupAdvertisement(Kindness.getKindWords());
+        result.addLookupAdvertisement(getKindWords())
     }
 
-    @NotNull
-    private String getTypeText(@NotNull LatexCommands commands) {
-        if ("\\newcommand".equals(commands.getCommandToken().getText())) {
-            return "";
+    private fun getTypeText(commands: LatexCommands): String {
+        if ("\\newcommand" == commands.commandToken.text) {
+            return ""
         }
-
-        LatexCommands firstNext = PsiCommandsKt.nextCommand(commands);
-        if (firstNext == null) {
-            return "";
-        }
-
-        LatexCommands secondNext = PsiCommandsKt.nextCommand(firstNext);
-        if (secondNext == null) {
-            return "";
-        }
-
-        String lookup = secondNext.getCommandToken().getText();
-        return lookup == null ? "" : lookup;
+        val firstNext = commands.nextCommand() ?: return ""
+        val secondNext = firstNext.nextCommand() ?: return ""
+        val lookup = secondNext.commandToken.text
+        return lookup ?: ""
     }
 
-    @NotNull
-    private String getTailText(@NotNull LatexCommands commands) {
-        if (!"\\newcommand".equals(commands.getCommandToken().getText())) {
-            return "";
+    private fun getTailText(commands: LatexCommands): String {
+        if ("\\newcommand" != commands.commandToken.text) {
+            return ""
         }
-
-        List<String> optional = new LinkedList<>(commands.getOptionalParameters().keySet());
-
-        int cmdParameterCount = 0;
-
-        if (!optional.isEmpty()) {
+        val optional: List<String> = LinkedList(commands.optionalParameters.keys)
+        var cmdParameterCount = 0
+        if (optional.isNotEmpty()) {
             try {
-                cmdParameterCount = Integer.parseInt(optional.get(0));
+                cmdParameterCount = optional[0].toInt()
             }
-            catch (NumberFormatException ignore) {
+            catch (ignore: NumberFormatException) {
             }
         }
-
-        String tailText = Strings.repeat("{param}", Math.min(4, cmdParameterCount));
+        var tailText = Strings.repeat("{param}", min(4, cmdParameterCount))
         if (cmdParameterCount > 4) {
-            tailText = tailText + "... (+" + (cmdParameterCount - 4) + " params)";
+            tailText = tailText + "... (+" + (cmdParameterCount - 4) + " params)"
         }
-
-        return tailText;
+        return tailText
     }
 
-    @Nullable
-    private String getCommandName(@NotNull LatexCommands commands) {
-        switch (commands.getName()) {
-            case "\\DeclareMathOperator":
-            case "\\newcommand":
-            case "\\newif":
-                return getNewCommandName(commands);
-            default:
-                return getDefinitionName(commands);
+    private fun getCommandName(commands: LatexCommands): String? {
+        return when (commands.name) {
+            "\\DeclareMathOperator", "\\newcommand", "\\newif" -> getNewCommandName(commands)
+            else -> getDefinitionName(commands)
         }
     }
 
-    @Nullable
-    private String getNewCommandName(@NotNull LatexCommands commands) {
-        LatexCommands cmd = PsiCommandsKt.forcedFirstRequiredParameterAsCommand(commands);
-        return cmd == null ? null : cmd.getName();
-    }
+    private fun getNewCommandName(commands: LatexCommands) = commands.forcedFirstRequiredParameterAsCommand()?.name
 
-    @Nullable
-    private String getDefinitionName(@NotNull LatexCommands commands) {
-        LatexCommands next = PsiCommandsKt.definitionCommand(commands);
-        if (next == null) {
-            return null;
-        }
-
-        return next.getCommandToken().getText();
-    }
+    private fun getDefinitionName(commands: LatexCommands) = commands.definitionCommand()?.commandToken?.text
 }
