f856ad81cc5dc08591257b41f61e0684631b9a1c
Abby Berkers <abbyberkers@gmail.com>
2020-03-23 17:37:39 +0100

    Add begin and end commands to include the backslash on selection

--- src/nl/hannahsten/texifyidea/editor/CommandSelectionFilter.kt
+++ src/nl/hannahsten/texifyidea/editor/CommandSelectionFilter.kt
@@ -2,7 +2,9 @@ package nl.hannahsten.texifyidea.editor
 
 import com.intellij.openapi.util.Condition
 import com.intellij.psi.PsiElement
+import nl.hannahsten.texifyidea.psi.LatexBeginCommand
 import nl.hannahsten.texifyidea.psi.LatexCommands
+import nl.hannahsten.texifyidea.psi.LatexEndCommand
 import nl.hannahsten.texifyidea.settings.TexifySettings
 import nl.hannahsten.texifyidea.util.firstParentOfType
 
@@ -10,5 +12,11 @@ import nl.hannahsten.texifyidea.util.firstParentOfType
  * Disable WordSelectioner (on by default) for LatexCommands, because we handle those in [LatexCommandSelectioner].
  */
 class CommandSelectionFilter : Condition<PsiElement> {
-    override fun value(t: PsiElement?) = !TexifySettings.getInstance().includeBackslashInSelection || t?.firstParentOfType(LatexCommands::class)?.text != t?.text
+    override fun value(t: PsiElement?) = !(
+            TexifySettings.getInstance().includeBackslashInSelection && (
+                    t?.firstParentOfType(LatexCommands::class)?.text == t?.text
+                    || t?.firstParentOfType(LatexBeginCommand::class)?.textOffset == t?.textOffset
+                    || t?.firstParentOfType(LatexEndCommand::class)?.textOffset == t?.textOffset
+                    )
+            )
 }
--- src/nl/hannahsten/texifyidea/editor/LatexCommandSelectioner.kt
+++ src/nl/hannahsten/texifyidea/editor/LatexCommandSelectioner.kt
@@ -5,16 +5,13 @@ import com.intellij.codeInsight.editorActions.SelectWordUtil
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import nl.hannahsten.texifyidea.psi.LatexCommands
-import nl.hannahsten.texifyidea.settings.TexifySettings
-import nl.hannahsten.texifyidea.util.firstParentOfType
 
 /**
  * Select all of the LatexCommands, so including the backslash.
  */
 class LatexCommandSelectioner : ExtendWordSelectionHandlerBase() {
     override fun canSelect(e: PsiElement): Boolean {
-        return TexifySettings.getInstance().includeBackslashInSelection && e.firstParentOfType(LatexCommands::class)?.text == e.text
+        return !CommandSelectionFilter().value(e)
     }
 
     override fun select(e: PsiElement, editorText: CharSequence, cursorOffset: Int, editor: Editor): MutableList<TextRange>? {
