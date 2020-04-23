92f4026d3141ae72b29f9ad351acb5102accd716
Thomas <hollandpirates@gmail.com>
2020-04-23 20:02:11 +0200

    Add test cases

--- test/nl/hannahsten/texifyidea/inspections/latex/UnescapedHashInspectionTest.kt
+++ test/nl/hannahsten/texifyidea/inspections/latex/UnescapedHashInspectionTest.kt
@@ -12,6 +12,26 @@ class UnescapedHashInspectionTest : TexifyInspectionTriggerTestBase(LatexEscapeH
     override val noTriggers: List<String> = listOf(
             """\newcommand{\a}[1]{#1}""",
             """\NewDocumentCommand{\b}{m}{#1}""",
-            """\newcommand{\c}[1]{\textbf{#1}}"""
+            """\newcommand{\c}[1]{\textbf{#1}}""",
+            """\newcommand*\eval[1]{\left.#1\right\rvert}""",
+            """
+                \newcommand\restr[2]{{
+                    \left.\kern-\nulldelimiterspace % automatically resize the bar with \right
+                    #1 % the function
+                    \vphantom{\big|} % pretend it's a little taller at normal size
+                    \right|_{#2} % this is the delimiter
+                }}
+        
+                \newcommand*\circled[1]{\tikz[baseline=(char.base)]{
+                    \node[shape=circle,draw,inner sep=2pt] (char) {#1};
+                }}
+        
+                \makeatletter
+                \renewcommand*\env@matrix[1][*\c@MaxMatrixCols c]{%
+                    \hskip -\arraycolsep
+                    \let\@ifnextchar\new@ifnextchar
+                    \array{#1}}
+                \makeatother
+            """.trimIndent()
     )
 }
