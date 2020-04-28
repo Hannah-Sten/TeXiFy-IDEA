402d626682449e9f7df1ff413e8218705fe55665
Thomas <hollandpirates@gmail.com>
2020-04-28 21:05:08 +0200

    Some sort of autocompletion handler

--- src/nl/hannahsten/texifyidea/util/Magic.kt
+++ src/nl/hannahsten/texifyidea/util/Magic.kt
@@ -179,6 +179,20 @@ object Magic {
                 "|", "|",
                 "\\|", "\\|"
         )
+
+        /**
+         * Algorithmicx pairs (also hardcoded in lexer).
+         */
+        val pseudoCodeBeginEndOpposites = mapOf(
+                "If" to "EndIf",
+                "For" to "EndFor",
+                "ForAll" to "EndFor",
+                "While" to "EndWhile",
+                "Repeat" to "Until",
+                "Loop" to "EndLoop",
+                "Function" to "EndFunction",
+                "Procedure" to "EndProcedure"
+        )
     }
 
     /**
