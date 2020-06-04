b9b47a5e5583d50b9dd13cdb133620fd5532f457
Thomas <hollandpirates@gmail.com>
2020-06-04 18:25:20 +0200

    Replace deprecated calls

--- src/nl/hannahsten/texifyidea/templates/LatexContext.kt
+++ src/nl/hannahsten/texifyidea/templates/LatexContext.kt
@@ -1,8 +1,8 @@
 package nl.hannahsten.texifyidea.templates
 
 import com.intellij.codeInsight.template.EverywhereContextType
+import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
-import com.intellij.psi.PsiFile
 import nl.hannahsten.texifyidea.file.LatexFile
 import nl.hannahsten.texifyidea.util.inMathContext
 
@@ -18,14 +18,14 @@ open class LatexContext(
         baseContextType: Class<out TemplateContextType>
 ) : TemplateContextType(id, name, baseContextType) {
 
-    override fun isInContext(file: PsiFile, offset: Int): Boolean =
-            file is LatexFile
+    override fun isInContext(context: TemplateActionContext): Boolean =
+            context.file is LatexFile
 
     class Generic : LatexContext("LATEX", "LaTeX", EverywhereContextType::class.java)
 
     open class LatexMathContext : LatexContext("LATEX_MATH", "Math", Generic::class.java) {
 
-        override fun isInContext(file: PsiFile, offset: Int): Boolean =
-                file is LatexFile && file.findElementAt(offset)?.inMathContext() == true
+        override fun isInContext(context: TemplateActionContext): Boolean =
+                context.file is LatexFile && context.file.findElementAt(context.startOffset)?.inMathContext() == true
     }
 }
