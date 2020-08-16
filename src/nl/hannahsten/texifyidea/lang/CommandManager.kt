921dd75d1f76b1c81de11f2253b19a0a17d4e217
Thomas Schouten <hollandpirates@gmail.com>
2020-08-16 12:51:06 +0200

    Synchronize CommandManager, fixes #1537 probably

--- src/nl/hannahsten/texifyidea/lang/CommandManager.kt
+++ src/nl/hannahsten/texifyidea/lang/CommandManager.kt
@@ -2,13 +2,12 @@ package nl.hannahsten.texifyidea.lang
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
+import kotlinx.coroutines.sync.Mutex
 import nl.hannahsten.texifyidea.index.LatexCommandsIndex
 import nl.hannahsten.texifyidea.psi.LatexCommands
 import nl.hannahsten.texifyidea.util.*
 import java.io.Serializable
-import java.util.Collections
-import java.util.HashSet
-import java.util.Spliterator
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.stream.Stream
 import java.util.stream.StreamSupport
@@ -135,7 +134,9 @@ object CommandManager : Iterable<String?>, Serializable {
         val aliasSet: MutableSet<String> = HashSet()
         aliasSet.add(command)
         original[command] = aliasSet
-        aliases[command] = aliasSet
+        synchronized(aliases) {
+            aliases[command] = aliasSet
+        }
     }
 
     /**
@@ -171,16 +172,18 @@ object CommandManager : Iterable<String?>, Serializable {
      */
     @Synchronized
     fun registerAlias(command: String, alias: String) {
-        val aliasSet = aliases[command] ?: mutableSetOf()
+        synchronized(aliases) {
+            val aliasSet = aliases[command] ?: mutableSetOf()
 
-        // If the alias is already assigned: unassign it.
-        if (isRegistered(alias)) {
-            val previousAliases = aliases[alias]
-            previousAliases?.remove(alias)
-            aliases.remove(alias)
+            // If the alias is already assigned: unassign it.
+            if (isRegistered(alias)) {
+                val previousAliases = aliases[alias]
+                previousAliases?.remove(alias)
+                aliases.remove(alias)
+            }
+            aliasSet.add(alias)
+            aliases[alias] = aliasSet
         }
-        aliasSet.add(alias)
-        aliases[alias] = aliasSet
     }
 
     /**
@@ -214,13 +217,16 @@ object CommandManager : Iterable<String?>, Serializable {
      */
     fun getAliases(command: String): Set<String> {
         if (!isRegistered(command)) return emptySet()
-        return aliases[command]?.toSet() ?: emptySet()
+        synchronized(aliases) {
+            return aliases[command]?.toSet() ?: emptySet()
+        }
     }
 
     /**
      * If needed (based on the number of indexed \newcommand-like commands) check for new aliases of the given alias set. This alias can be any alias of its alias set.
      * If the alias set is not yet registered, it will be registered as a new alias set.
      */
+    @Synchronized
     fun updateAliases(aliasSet: Set<String>, project: Project) {
         // Register if needed
         if (aliasSet.isEmpty()) return
@@ -246,9 +252,11 @@ object CommandManager : Iterable<String?>, Serializable {
             // Alternatively we could save a numberOfIndexedCommandDefinitions per alias set, and only update the
             // requested alias set (otherwise only the first alias set requesting an update will get it)
             // We have to deepcopy the set of alias sets before iterating over it, because we want to modify aliases
-            val deepCopy = aliases.values.map { it1 -> it1.map { it }.toSet() }.toSet()
-            for (copiedAliasSet in deepCopy) {
-                findAllAliases(copiedAliasSet, indexedCommandDefinitions)
+            synchronized(aliases) {
+                val deepCopy = aliases.values.map { it1 -> it1.map { it }.toSet() }.toSet()
+                for (copiedAliasSet in deepCopy) {
+                    findAllAliases(copiedAliasSet, indexedCommandDefinitions)
+                }
             }
 
             numberOfIndexedCommandDefinitions = indexedCommandDefinitions.count()
@@ -400,7 +408,9 @@ object CommandManager : Iterable<String?>, Serializable {
      * otherwise.
      */
     fun isRegistered(command: String): Boolean {
-        return aliases.containsKey(command)
+        synchronized(aliases) {
+            return aliases.containsKey(command)
+        }
     }
 
     /**
