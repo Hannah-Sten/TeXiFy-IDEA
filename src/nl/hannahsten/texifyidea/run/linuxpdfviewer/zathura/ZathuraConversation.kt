f4bf14e752070b2df0e7adbcd9134adb8ab40164
Abby Berkers <abbyberkers@gmail.com>
2020-08-20 19:13:45 +0200

    Add zathura inverse search

--- src/nl/hannahsten/texifyidea/run/linuxpdfviewer/zathura/ZathuraConversation.kt
+++ src/nl/hannahsten/texifyidea/run/linuxpdfviewer/zathura/ZathuraConversation.kt
@@ -2,6 +2,8 @@ package nl.hannahsten.texifyidea.run.linuxpdfviewer.zathura
 
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.Project
 import nl.hannahsten.texifyidea.run.linuxpdfviewer.ViewerConversation
 import nl.hannahsten.texifyidea.run.linuxpdfviewer.okular.OkularConversation
@@ -9,12 +11,13 @@ import nl.hannahsten.texifyidea.run.linuxpdfviewer.okular.OkularConversation
 object ZathuraConversation : ViewerConversation() {
     override fun forwardSearch(pdfPath: String?, sourceFilePath: String, line: Int, project: Project, focusAllowed: Boolean) {
         if (pdfPath != null) {
-            // This okular command opens the pdf file using the destination coming from the line in the tex file.
-            val command = """zathura --synctex-forward="$line:1:$sourceFilePath" $pdfPath"""
+            val path = PathManager.getBinPath()
+            val name = ApplicationNamesInfo.getInstance().scriptName
+            val command = """zathura --synctex-forward="$line:1:$sourceFilePath" --synctex-editor-command="$path/$name.sh --line %{line} $sourceFilePath" $pdfPath"""
             Runtime.getRuntime().exec(arrayOf("bash", "-c", command))
         }
         else {
-            Notification("OkularConversation", "Could not execute forward search", "Please make sure you have compiled the document first.", NotificationType.ERROR).notify(project)
+            Notification("ZathuraConversation", "Could not execute forward search", "Please make sure you have compiled the document first.", NotificationType.ERROR).notify(project)
         }
     }
 }
