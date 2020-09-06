a31b8dc9b6e62affc96edfbc13a13e434c70147c
Thomas <hollandpirates@gmail.com>
2020-09-06 18:48:04 +0200

    Linting

--- src/nl/hannahsten/texifyidea/util/files/VirtualFile.kt
+++ src/nl/hannahsten/texifyidea/util/files/VirtualFile.kt
@@ -1,6 +1,5 @@
 package nl.hannahsten.texifyidea.util.files
 
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.LocalFileSystem
