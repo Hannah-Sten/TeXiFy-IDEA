e142c0adf9404845fa68f10dfecf8b4e810ce834
Abby Berkers <abbyberkers@gmail.com>
2020-09-15 12:23:13 +0200

    Move arguments left and right

--- src/nl/hannahsten/texifyidea/editor/LatexMoveElementLeftRightHandler.kt
+++ src/nl/hannahsten/texifyidea/editor/LatexMoveElementLeftRightHandler.kt
@@ -2,10 +2,12 @@ package nl.hannahsten.texifyidea.editor
 
 import com.intellij.codeInsight.editorActions.moveLeftRight.MoveElementLeftRightHandler
 import com.intellij.psi.PsiElement
+import com.intellij.util.containers.toArray
+import nl.hannahsten.texifyidea.psi.LatexCommands
 
 class LatexMoveElementLeftRightHandler : MoveElementLeftRightHandler(){
 
     override fun getMovableSubElements(element: PsiElement): Array<PsiElement> {
-        return emptyArray()
+        return (element as? LatexCommands)?.parameterList?.map { it as PsiElement }?.toArray(emptyArray()) ?: emptyArray()
     }
 }
