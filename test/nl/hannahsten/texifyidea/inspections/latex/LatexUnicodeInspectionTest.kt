e70116e0cd372d938f6fd84aab4c9fe0b4f45d73
Abby Berkers <abbyberkers@gmail.com>
2020-09-18 11:09:58 +0200

    Add test for redundant escape inspection

--- test/nl/hannahsten/texifyidea/inspections/latex/LatexUnicodeInspectionTest.kt
+++ test/nl/hannahsten/texifyidea/inspections/latex/LatexUnicodeInspectionTest.kt
@@ -1,12 +1,7 @@
 package nl.hannahsten.texifyidea.inspections.latex
 
-import io.mockk.every
-import io.mockk.mockkObject
 import nl.hannahsten.texifyidea.file.LatexFileType
 import nl.hannahsten.texifyidea.inspections.TexifyInspectionTestBase
-import nl.hannahsten.texifyidea.run.compiler.LatexCompiler
-import nl.hannahsten.texifyidea.run.latex.LatexDistribution
-import nl.hannahsten.texifyidea.settings.TexifyProjectSettings
 
 class OutsideMathLatexUnicodeInspectionTest : LatexUnicodeInspectionTest() {
     fun `test illegal unicode character`() {
@@ -106,20 +101,5 @@ class LatexUnicodeInspectionQuickFix : LatexUnicodeInspectionTest() {
 }
 
 abstract class LatexUnicodeInspectionTest : TexifyInspectionTestBase(LatexUnicodeInspection()) {
-
-    /**
-     * Set the TeXiFy Project Settings and the Latex Distribution in a way to ensure either unicode support, or no unicode support.
-     */
-    fun setUnicodeSupport(enabled: Boolean = true) {
-        if (enabled) {
-            // Unicode is always supported in lualatex.
-            TexifyProjectSettings.getInstance(myFixture.project).compilerCompatibility = LatexCompiler.LUALATEX
-        }
-        else {
-            // Unicode is not supported on pdflatex on texlive <= 2017.
-            TexifyProjectSettings.getInstance(myFixture.project).compilerCompatibility = LatexCompiler.PDFLATEX
-            mockkObject(LatexDistribution)
-            every { LatexDistribution.texliveVersion } returns 2017
-        }
-    }
+    fun setUnicodeSupport(enabled: Boolean = true) = nl.hannahsten.texifyidea.testutils.setUnicodeSupport(myFixture.project, enabled)
 }
--- test/nl/hannahsten/texifyidea/testutils/Common.kt
+++ test/nl/hannahsten/texifyidea/testutils/Common.kt
@@ -2,6 +2,11 @@ package nl.hannahsten.texifyidea.testutils
 
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
+import io.mockk.every
+import io.mockk.mockkObject
+import nl.hannahsten.texifyidea.run.compiler.LatexCompiler
+import nl.hannahsten.texifyidea.run.latex.LatexDistribution
+import nl.hannahsten.texifyidea.settings.TexifyProjectSettings
 
 /**
  * Execute the given action as write command.
@@ -13,5 +18,21 @@ import com.intellij.openapi.project.Project
 fun <T> writeCommand(project: Project, action: () -> T) {
     WriteCommandAction.writeCommandAction(project).compute<T, Exception> {
         action.invoke()
+    }
+}
+
+/**
+ * Set the TeXiFy Project Settings and the Latex Distribution in a way to ensure either unicode support, or no unicode support.
+ */
+fun setUnicodeSupport(project: Project, enabled: Boolean = true) {
+    if (enabled) {
+        // Unicode is always supported in lualatex.
+        TexifyProjectSettings.getInstance(project).compilerCompatibility = LatexCompiler.LUALATEX
+    }
+    else {
+        // Unicode is not supported on pdflatex on texlive <= 2017.
+        TexifyProjectSettings.getInstance(project).compilerCompatibility = LatexCompiler.PDFLATEX
+        mockkObject(LatexDistribution)
+        every { LatexDistribution.texliveVersion } returns 2017
     }
 }
