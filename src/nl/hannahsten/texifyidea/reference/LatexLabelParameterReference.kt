a9db950d0f538691d6f3ea915e4cb643d3e8b621
Thomas <hollandpirates@gmail.com>
2020-09-29 09:07:21 +0200

    Fix label reference resolving with respect to custom commands

--- src/nl/hannahsten/texifyidea/reference/LatexLabelParameterReference.kt
+++ src/nl/hannahsten/texifyidea/reference/LatexLabelParameterReference.kt
@@ -3,9 +3,9 @@ package nl.hannahsten.texifyidea.reference
 import com.intellij.psi.*
 import com.intellij.util.containers.toArray
 import nl.hannahsten.texifyidea.psi.LatexParameterText
+import nl.hannahsten.texifyidea.util.extractLabelElement
 import nl.hannahsten.texifyidea.util.extractLabelName
 import nl.hannahsten.texifyidea.util.findLatexLabelPsiElementsInFileSetAsSequence
-import nl.hannahsten.texifyidea.util.firstChildOfType
 
 /**
  * The difference with [LatexLabelReference] is that this reference works on parameter text, i.e. the actual label parameters.
@@ -39,8 +39,7 @@ class LatexLabelParameterReference(element: LatexParameterText) : PsiReferenceBa
                     // We cannot just resolve to the label command itself, because for Find Usages IJ will get the name of the element
                     // under the cursor and use the words scanner to look for it (and then check if the elements found are references to the element under the cursor)
                     // but only the label text itself will have the correct name for that.
-                    val labelText = it.firstChildOfType(LatexParameterText::class) ?: return@mapNotNull null
-                    PsiElementResolveResult(labelText)
+                    PsiElementResolveResult(it.extractLabelElement() ?: return@mapNotNull null)
                 }
                 .toList()
                 .toArray(emptyArray())
