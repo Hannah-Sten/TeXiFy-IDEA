9a4e2814437f710a719c09419ac42e08c4d7b0d6
Abby Berkers <abbyberkers@gmail.com>
2020-09-29 08:39:55 +0200

    Add test for missing document environment inspection

--- src/nl/hannahsten/texifyidea/inspections/latex/LatexMissingDocumentEnvironmentInspection.kt
+++ src/nl/hannahsten/texifyidea/inspections/latex/LatexMissingDocumentEnvironmentInspection.kt
@@ -78,8 +78,6 @@ open class LatexMissingDocumentEnvironmentInspection : TexifyInspectionBase() {
                 |
                 |\begin{document}
                 |
-                |
-                |
                 |\end{document}""".trimMargin())
         }
     }
