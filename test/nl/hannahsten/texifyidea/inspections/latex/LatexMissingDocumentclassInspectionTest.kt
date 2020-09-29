c39a484e9ee8ef389d1faef603ab9a4a1517202d
Abby Berkers <abbyberkers@gmail.com>
2020-09-29 15:56:15 +0200

    Add tests for missing documentclass inspection

---
+++ test/nl/hannahsten/texifyidea/inspections/latex/LatexMissingDocumentclassInspectionTest.kt
@@ -0,0 +1,19 @@
+package nl.hannahsten.texifyidea.inspections.latex
+
+import nl.hannahsten.texifyidea.inspections.TexifyInspectionTestBase
+
+class LatexMissingDocumentclassInspectionTest : TexifyInspectionTestBase(LatexMissingDocumentclassInspection()) {
+    fun `test no warning`() = testHighlighting("""\documentclass{article}""")
+
+    fun `test warning`() = testHighlighting("""<error descr="Document doesn't contain a \documentclass command.">\usepackage{amsmath}</error>""")
+
+    fun `test quick fix`() = testQuickFix(
+            before = """
+                \usepackage{amsmath}
+            """.trimIndent(),
+            after = """
+                \documentclass{article}
+                \usepackage{amsmath}
+            """.trimIndent()
+    )
+}
