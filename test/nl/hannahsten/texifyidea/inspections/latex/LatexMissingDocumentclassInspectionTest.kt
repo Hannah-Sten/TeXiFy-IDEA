e9f88fbf6d5142990f05ac8a3aca3c0555f69592
Abby Berkers <abbyberkers@gmail.com>
2020-10-02 15:44:41 +0200

    Add forgotten test for missing documentclass inspection in sty file

--- test/nl/hannahsten/texifyidea/inspections/latex/LatexMissingDocumentclassInspectionTest.kt
+++ test/nl/hannahsten/texifyidea/inspections/latex/LatexMissingDocumentclassInspectionTest.kt
@@ -5,6 +5,11 @@ import nl.hannahsten.texifyidea.inspections.TexifyInspectionTestBase
 class LatexMissingDocumentclassInspectionTest : TexifyInspectionTestBase(LatexMissingDocumentclassInspection()) {
     fun `test no warning`() = testHighlighting("""\documentclass{article}""")
 
+    fun `test no warning in sty file`() {
+        myFixture.configureByText("package.sty", """\newcommand{\help}{help}""")
+        myFixture.checkHighlighting()
+    }
+
     fun `test warning`() = testHighlighting("""<error descr="Document doesn't contain a \documentclass command.">\usepackage{amsmath}</error>""")
 
     fun `test quick fix`() = testQuickFix(
