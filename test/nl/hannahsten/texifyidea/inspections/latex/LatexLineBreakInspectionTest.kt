02486b1b7b5d02882c5a0b0aec918eec2c02fe27
Abby Berkers <abbyberkers@gmail.com>
2020-10-07 12:27:39 +0200

    Add forgotten test for line break inspection in magic comments

--- test/nl/hannahsten/texifyidea/inspections/latex/LatexLineBreakInspectionTest.kt
+++ test/nl/hannahsten/texifyidea/inspections/latex/LatexLineBreakInspectionTest.kt
@@ -1,57 +1,35 @@
 package nl.hannahsten.texifyidea.inspections.latex
 
-import nl.hannahsten.texifyidea.file.LatexFileType
 import nl.hannahsten.texifyidea.inspections.TexifyInspectionTestBase
-import nl.hannahsten.texifyidea.testutils.writeCommand
 
 class LatexLineBreakInspectionTest : TexifyInspectionTestBase(LatexLineBreakInspection()) {
-    fun testWarning() {
-        myFixture.configureByText(LatexFileType, """
+    fun testWarning() = testHighlighting("""
             Not this, b<weak_warning descr="Sentence does not start on a new line">ut. This starts a new line.</weak_warning>
 This e<weak_warning descr="Sentence does not start on a new line">tc. is missing a normal space, but i.e. this etc.</weak_warning>\ is not.
             % not. in. comments
         """.trimIndent())
-        myFixture.checkHighlighting()
-    }
 
-    fun testNoWarning() {
-        myFixture.configureByText(LatexFileType, """
+    fun testNoWarning() = testHighlighting("""
             First sentence.
             Second sentence.
         """.trimIndent())
-        myFixture.checkHighlighting()
-    }
 
-    fun testNoWarningWithComment() {
-        myFixture.configureByText(LatexFileType, """
+    fun `test no warning in comment`() = testHighlighting("""
             This is an abbreviation (ABC). % commemt
             More text here.
         """.trimIndent())
-        myFixture.checkHighlighting()
-    }
 
-    fun testNoWarningInMathMode() {
-        myFixture.configureByText(LatexFileType, """
-            \[ Why. would. you. do. this. \]
-        """.trimIndent())
-        myFixture.checkHighlighting()
-    }
+    fun `test no warning in math mode`() = testHighlighting("""\[ Why. would. you. do. this. \]""")
 
-    fun testQuickfix() {
-        myFixture.configureByText(LatexFileType,
-        """
-            I end. a sentence.
-        """.trimIndent())
+    fun `test no warning in magic comment on own line`() = testHighlighting("""%! suppress = CiteBeforePeriod""")
 
-        val quickFixes = myFixture.getAllQuickFixes()
-        assertEquals(1, quickFixes.size)
-        writeCommand(myFixture.project) {
-            quickFixes.first().invoke(myFixture.project, myFixture.editor, myFixture.file)
-        }
+    fun `test no warning in magic comment on line with text`() = testHighlighting("""This is a sentence. %! suppress = CiteBeforePeriod""")
 
-        myFixture.checkResult("""
-            I end.
-            a sentence.
-        """.trimIndent())
-    }
+    fun `test quick fix`() = testQuickFix(
+            before = """I end. a sentence.""",
+            after = """
+                I end.
+                a sentence.
+            """.trimIndent()
+    )
 }
