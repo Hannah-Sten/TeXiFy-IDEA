34f1674f998085b3c4f4cf65c596d0eaf34d6d8f
Thomas <hollandpirates@gmail.com>
2020-11-25 10:07:40 +0100

    Autocompletion for suppress magic comment

--- src/nl/hannahsten/texifyidea/completion/LatexInspectionIdProvider.kt
+++ src/nl/hannahsten/texifyidea/completion/LatexInspectionIdProvider.kt
@@ -15,12 +15,26 @@ import nl.hannahsten.texifyidea.inspections.ALL_TEXIFY_INSPECTIONS
  */
 object LatexInspectionIdProvider : CompletionProvider<CompletionParameters>() {
 
-    override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
         val file = parameters.originalFile
         val inspectionIds = InsightGroup.byFileType(file.fileType)
             .flatMap { ALL_TEXIFY_INSPECTIONS[it] ?: emptyList() }
 
-        result.addAllElements(
+        // Because it was too difficult to parse elements of the magic comment separately,
+        // the default prefix matcher takes the complete comment as prefix (including %! suppress =)
+        // so we override the prefix to remove that manually.
+        val prefix = """suppress\s*=\s*(.*)IntellijIdeaRulezzz""".toRegex()
+            .find(parameters.position.text)?.groupValues?.getOrNull(1)
+        if (prefix != null) {
+            result.withPrefixMatcher(prefix)
+        }
+        else {
+            result
+        }.addAllElements(
             ContainerUtil.map2List(inspectionIds) {
                 LookupElementBuilder.create(it, it)
                     .withPresentableText(it)
