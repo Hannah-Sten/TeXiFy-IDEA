ed37f5f54fee343bc3690e8157118d7adfa41b68
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2021-01-09 11:37:44 +0100

    parameterText no longer counts parameters where it.command == null, like in \begin

--- src/nl/hannahsten/texifyidea/action/analysis/WordCountAction.kt
+++ src/nl/hannahsten/texifyidea/action/analysis/WordCountAction.kt
@@ -111,7 +111,10 @@ open class WordCountAction : AnAction(
             .filter { it.name.endsWith(".tex", ignoreCase = true) }
         val allNormalText = fileSet.flatMap { it.childrenOfType(LatexNormalText::class) }
         val parameterText = fileSet.flatMap { it.childrenOfType(LatexParameterText::class) }
-                .filter { it.command?.text !in IGNORE_COMMANDS }
+                .filter {
+                    val commandText = it.command?.text ?: return@filter false
+                    return@filter commandText !in IGNORE_COMMANDS
+                }
 
         val bibliographies = baseFile.childrenOfType(LatexEnvironment::class)
             .filter {
