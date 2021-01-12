8604e02049aa694175b637849f253b40b6f8cfa5
Hannah Schellekens <github@hannahschellekens.nl>
2021-01-12 13:37:35 +0100

    Added skeleton for symbol tool window.

---
+++ src/nl/hannahsten/texifyidea/ui/SymbolToolWindowFactory.kt
@@ -0,0 +1,35 @@
+package nl.hannahsten.texifyidea.ui
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.content.ContentFactory
+import nl.hannahsten.texifyidea.util.hasLatexModule
+import javax.swing.JPanel
+
+/**
+ * The Symbol tool window shows an overview of several symbols that can be inserted in the active latex document.
+ *
+ * @author Hannah Schellekens
+ */
+open class SymbolToolWindowFactory : ToolWindowFactory {
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val toolWindowPanel = SymbolToolWindow()
+        val content = ContentFactory.SERVICE.getInstance().createContent(toolWindowPanel, "", false)
+        toolWindow.contentManager.addContent(content)
+    }
+
+    override fun isApplicable(project: Project) = project.hasLatexModule()
+
+    /**
+     * The swing contents of the symbol tool window.
+     */
+    class SymbolToolWindow : JPanel() {
+
+        init {
+            add(JBLabel("A label"))
+        }
+    }
+}
