912a3ebee3ac9e285805d0176c0674e4f42bd566
Thomas <hollandpirates@gmail.com>
2021-02-10 19:12:01 +0100

    Take % alignment from previous line

--- src/nl/hannahsten/texifyidea/editor/typedhandlers/LatexEnterInCommentHandler.kt
+++ src/nl/hannahsten/texifyidea/editor/typedhandlers/LatexEnterInCommentHandler.kt
@@ -1,6 +1,5 @@
 package nl.hannahsten.texifyidea.editor.typedhandlers
 
-import com.intellij.application.options.CodeStyle
 import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegate
 import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegateAdapter
 import com.intellij.openapi.actionSystem.DataContext
@@ -11,6 +10,7 @@ import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import nl.hannahsten.texifyidea.util.files.document
+import nl.hannahsten.texifyidea.util.get
 
 class LatexEnterInCommentHandler : EnterHandlerDelegateAdapter() {
 
@@ -31,16 +31,30 @@ class LatexEnterInCommentHandler : EnterHandlerDelegateAdapter() {
 
     override fun postProcessEnter(file: PsiFile, editor: Editor, dataContext: DataContext): EnterHandlerDelegate.Result {
         if (isComment) {
-            // Not entirely correct, because the % sign on the previous column may have a different indent than the setting
-            // But looking at the setting is much easier for now
-            if (CodeStyle.getLanguageSettings(file).LINE_COMMENT_AT_FIRST_COLUMN) {
-                val startOfLine = editor.caretModel.currentCaret.visualLineStart
-                file.document()?.insertString(startOfLine, "%")
+            // Check location of the % on the previous line, especially the number of spaces before and after it
+            // because that's what we want to have on the current line as well
+            // (note the cursor is on the second line, after the enter has been processed)
+            val previousLineNumber = (editor.caretModel.currentCaret.logicalPosition.line - 1).coerceAtLeast(0)
+            val lineStart = editor.document.getLineStartOffset(previousLineNumber)
+            val lineEnd = editor.document.getLineEndOffset(previousLineNumber)
+            val previousLine = editor.document[IntRange(lineStart, lineEnd)]
 
+            // Since the enter handler already inserted the indent of the previous line, subtract it
+            val numberOfSpacesBeforePercent = previousLine.takeWhile { it == ' ' }.length
+            val numberOfSpacesAfterPercent = previousLine.dropWhile { it == ' ' }.dropWhile { it == '%' }.takeWhile { it == ' ' }.length
+
+            if (numberOfSpacesBeforePercent == 0) {
+                // It was right at the start of the line, so we only have to insert it there again
+                // and insert the spaces that were probably not part of the indent (which is already done by the enter handler)
+                val startOfLine = editor.caretModel.currentCaret.visualLineStart
+                file.document()?.insertString(startOfLine, "%" + " ".repeat(numberOfSpacesAfterPercent % 4))
             }
             else {
-                file.document()?.insertString(editor.caretModel.currentCaret.offset, "% ")
+                // There was indent before it, but that's handled by the enter handler already
+                // So we just need to insert any spaces after it
+                file.document()?.insertString(editor.caretModel.currentCaret.offset, "%" + " ".repeat(numberOfSpacesAfterPercent))
             }
+            // Alternatively, consider the setting CodeStyle.getLanguageSettings(file).LINE_COMMENT_AT_FIRST_COLUMN
         }
         PsiDocumentManager.getInstance(file.project).commitDocument(editor.document)
         return super.postProcessEnter(file, editor, dataContext)
