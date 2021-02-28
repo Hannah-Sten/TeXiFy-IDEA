34bac9ecf6ddd86325799c1423f07fece47869e9
Thomas Schouten <hollandpirates@gmail.com>
2021-02-28 18:26:21 +0100

    Add some tests

---
+++ test/nl/hannahsten/texifyidea/editor/typedhandlers/LatexEnterInCommentHandlerTest.kt
@@ -0,0 +1,41 @@
+package nl.hannahsten.texifyidea.editor.typedhandlers
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import nl.hannahsten.texifyidea.file.LatexFileType
+
+class LatexEnterInCommentHandlerTest : BasePlatformTestCase() {
+
+    fun testSimpleComment() {
+        myFixture.configureByText(LatexFileType, """
+            % These forests are to be indicated by the length <caret>according to their truthful measures.
+        """.trimIndent())
+        myFixture.type("\n") // It actually presses enter: see EditorTestFixture.java:91
+        myFixture.checkResult("""
+            % These forests are to be indicated by the length 
+            <caret>% according to their truthful measures.
+        """.trimIndent())
+    }
+
+    fun testCommentWithIndentation() {
+        // For example, when having the setting "line comment at first column" unselected
+        myFixture.configureByText(LatexFileType, """
+            % All beds diverged by the stated variants <caret>can be written by the algorithm.
+        """)
+        myFixture.type("\n")
+        myFixture.checkResult("""
+            % All beds diverged by the stated variants 
+            <caret>% can be written by the algorithm.
+        """)
+    }
+
+    fun testCommentAtFirstColumn() {
+        myFixture.configureByText(LatexFileType, """
+            %   And to state attentions to the purpose <caret>to sin the clear found by genes.
+        """.trimIndent())
+        myFixture.type("\n")
+        myFixture.checkResult("""
+            %   And to state attentions to the purpose 
+            <caret>%   to sin the clear found by genes.
+        """.trimIndent())
+    }
+}
