de8f0755104a26acfafd80de54865978105ca5fd
Thomas Schouten <hollandpirates@gmail.com>
2021-03-02 09:09:44 +0100

    Improve readability

--- src/nl/hannahsten/texifyidea/editor/typedhandlers/LatexEnterInCommentHandler.kt
+++ src/nl/hannahsten/texifyidea/editor/typedhandlers/LatexEnterInCommentHandler.kt
@@ -33,44 +33,52 @@ class LatexEnterInCommentHandler : EnterHandlerDelegateAdapter() {
 
     override fun postProcessEnter(file: PsiFile, editor: Editor, dataContext: DataContext): EnterHandlerDelegate.Result {
         if (isComment) {
-            // Check location of the % on the previous line, especially the number of spaces before and after it
-            // because that's what we want to have on the current line as well
-            // (note the cursor is on the second line, after the enter has been processed)
-            val previousLineNumber = (editor.caretModel.currentCaret.logicalPosition.line - 1).coerceAtLeast(0)
-            val lineStart = editor.document.getLineStartOffset(previousLineNumber)
-            val lineEnd = editor.document.getLineEndOffset(previousLineNumber)
-            val previousLine = editor.document[IntRange(lineStart, lineEnd)]
-
-            // If we pressed enter right before or after the %, then don't do anything, as we're not actually splitting a comment
-            // To clarify the second case: consider wanting to type a % at the end of a line
-            if (!previousLine.contains("%") || previousLine.endsWith("%")) {
-                return super.postProcessEnter(file, editor, dataContext)
-            }
-
-            // Since the enter handler already inserted the indent of the previous line, subtract it
-            val numberOfSpacesBeforePercent = previousLine.takeWhile { it == ' ' }.length
-            val numberOfSpacesAfterPercent = previousLine.dropWhile { it == ' ' }
-                // ! for magic comments
-                .dropWhile { it == '%' || it == '!' }
-                .takeWhile { it == ' ' }.length
-
-            // Include magic comments
-            val commentPrefix = """^\s*(?<percent>[%!]+)""".toRegex().find(previousLine)?.groups?.get("percent")?.value ?: return super.postProcessEnter(file, editor, dataContext)
-
-            if (numberOfSpacesBeforePercent == 0) {
-                // It was right at the start of the line, so we only have to insert it there again
-                // and insert the spaces that were probably not part of the indent (which is already done by the enter handler)
-                val startOfLine = editor.caretModel.currentCaret.visualLineStart
-                file.document()?.insertString(startOfLine, commentPrefix + " ".repeat(numberOfSpacesAfterPercent % 4))
-            }
-            else {
-                // There was indent before it, but that's handled by the enter handler already
-                // So we just need to insert any spaces after it
-                file.document()?.insertString(editor.caretModel.currentCaret.offset, commentPrefix + " ".repeat(numberOfSpacesAfterPercent))
-            }
-            // Alternatively, consider the setting CodeStyle.getLanguageSettings(file).LINE_COMMENT_AT_FIRST_COLUMN
+            handleEnterInComment(file, editor)
         }
-        PsiDocumentManager.getInstance(file.project).commitDocument(editor.document)
         return super.postProcessEnter(file, editor, dataContext)
+    }
+
+    /**
+     * Insert extra % sign when enter was pressed in a comment.
+     */
+    private fun handleEnterInComment(file: PsiFile, editor: Editor) {
+        // Check location of the % on the previous line, especially the number of spaces before and after it
+        // because that's what we want to have on the current line as well
+        // (note the cursor is on the second line, after the enter has been processed)
+        val previousLineNumber = (editor.caretModel.currentCaret.logicalPosition.line - 1).coerceAtLeast(0)
+        val lineStart = editor.document.getLineStartOffset(previousLineNumber)
+        val lineEnd = editor.document.getLineEndOffset(previousLineNumber)
+        val previousLine = editor.document[IntRange(lineStart, lineEnd)]
+
+        // If we pressed enter right before or after the %, then don't do anything, as we're not actually splitting a comment
+        // To clarify the second case: consider wanting to type a % at the end of a line
+        if (!previousLine.contains("%") || previousLine.endsWith("%")) {
+            return
+        }
+
+        // Since the enter handler already inserted the indent of the previous line, subtract it
+        val numberOfSpacesBeforePercent = previousLine.takeWhile { it == ' ' }.length
+        val numberOfSpacesAfterPercent = previousLine.dropWhile { it == ' ' }
+            // ! for magic comments
+            .dropWhile { it == '%' || it == '!' }
+            .takeWhile { it == ' ' }.length
+
+        // Include magic comments
+        val commentPrefix = """^\s*(?<percent>[%!]+)""".toRegex().find(previousLine)?.groups?.get("percent")?.value ?: return
+
+        if (numberOfSpacesBeforePercent == 0) {
+            // It was right at the start of the line, so we only have to insert it there again
+            // and insert the spaces that were probably not part of the indent (which is already done by the enter handler)
+            val startOfLine = editor.caretModel.currentCaret.visualLineStart
+            file.document()?.insertString(startOfLine, commentPrefix + " ".repeat(numberOfSpacesAfterPercent % 4))
+        }
+        else {
+            // There was indent before it, but that's handled by the enter handler already
+            // So we just need to insert any spaces after it
+            file.document()?.insertString(editor.caretModel.currentCaret.offset, commentPrefix + " ".repeat(numberOfSpacesAfterPercent))
+        }
+        // Alternatively, consider the setting CodeStyle.getLanguageSettings(file).LINE_COMMENT_AT_FIRST_COLUMN
+
+        PsiDocumentManager.getInstance(file.project).commitDocument(editor.document)
     }
 }
