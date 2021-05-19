60638a9de656057c69294373b16cb5931bf0d78c
Felix Berlakovich <felix@berlakovich.at>
2021-05-19 21:40:54 +0200

    add more default values

--- src/nl/hannahsten/texifyidea/settings/conventions/TexifyConventionsScheme.kt
+++ src/nl/hannahsten/texifyidea/settings/conventions/TexifyConventionsScheme.kt
@@ -1,11 +1,28 @@
 package nl.hannahsten.texifyidea.settings.conventions
 
+import nl.hannahsten.texifyidea.lang.DefaultEnvironment.*
+import nl.hannahsten.texifyidea.lang.commands.LatexGenericRegularCommand.*
+import nl.hannahsten.texifyidea.lang.commands.LatexListingCommand.LSTINPUTLISTING
+import nl.hannahsten.texifyidea.util.magic.env
+
 data class TexifyConventionsScheme(
     var myName: String = DEFAULT_SCHEME_NAME,
     var maxSectionSize: Int = 4000,
     var labelConventions: MutableList<LabelConvention> = mutableListOf(
-        LabelConvention(false, LabelConventionType.ENVIRONMENT, "lstlisting", "lst"),
-        LabelConvention(true, LabelConventionType.COMMAND, "section", "sec")
+        LabelConvention(true, LabelConventionType.COMMAND, CHAPTER.command, "ch"),
+        LabelConvention(true, LabelConventionType.COMMAND, SECTION.command, "sec"),
+        LabelConvention(true, LabelConventionType.COMMAND, SUBSECTION.command, "subsec"),
+        LabelConvention(false, LabelConventionType.COMMAND, SUBSUBSECTION.command, "subsubsec"),
+        LabelConvention(false, LabelConventionType.COMMAND, PARAGRAPH.command, "par"),
+        LabelConvention(false, LabelConventionType.COMMAND, SUBPARAGRAPH.command, "subpar"),
+        LabelConvention(false, LabelConventionType.COMMAND, ITEM.command, "itm"),
+        LabelConvention(true, LabelConventionType.COMMAND, LSTINPUTLISTING.command, "lst"),
+        LabelConvention(true, LabelConventionType.ENVIRONMENT, FIGURE.env, "fig"),
+        LabelConvention(true, LabelConventionType.ENVIRONMENT, TABLE.env, "tab"),
+        LabelConvention(true, LabelConventionType.ENVIRONMENT, EQUATION.env, "eq"),
+        LabelConvention(true, LabelConventionType.ENVIRONMENT, ALGORITHM.env, "alg"),
+        LabelConvention(true, LabelConventionType.ENVIRONMENT, LISTINGS.env, "lst"),
+        LabelConvention(true, LabelConventionType.ENVIRONMENT, VERBATIM_CAPITAL.env, "verb"),
     )
 ) : com.intellij.openapi.options.Scheme {
     val isProjectScheme: Boolean
