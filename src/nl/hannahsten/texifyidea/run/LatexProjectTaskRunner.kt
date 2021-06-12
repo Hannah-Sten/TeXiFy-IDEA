2184c32ef12901bb8fb73f378a10df3df63ca70f
Thomas Schouten <hollandpirates@gmail.com>
2021-06-12 20:48:40 +0200

    Formatting

--- src/nl/hannahsten/texifyidea/run/LatexProjectTaskRunner.kt
+++ src/nl/hannahsten/texifyidea/run/LatexProjectTaskRunner.kt
@@ -1,10 +1,8 @@
 package nl.hannahsten.texifyidea.run
 
-import com.intellij.execution.RunManager
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.impl.RunConfigurationBeforeRunProvider
 import com.intellij.execution.impl.RunManagerImpl
-import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
 import com.intellij.openapi.project.Project
 import com.intellij.task.*
@@ -19,6 +17,7 @@ import org.jetbrains.concurrency.Promise
  * the complete project, so I think the most sensible is to run all run configurations.
  */
 class LatexProjectTaskRunner : ProjectTaskRunner() {
+
     override fun canRun(projectTask: ProjectTask): Boolean {
         return projectTask is ModuleBuildTask && projectTask.module.moduleTypeName == LatexModuleType.ID
     }
@@ -26,10 +25,10 @@ class LatexProjectTaskRunner : ProjectTaskRunner() {
     override fun run(project: Project, context: ProjectTaskContext, vararg tasks: ProjectTask?): Promise<Result> {
         // Each task will probably be a module, but we don't use it
 
-
         project.getLatexRunConfigurations().forEach { runConfig ->
             val latexSettings = RunManagerImpl.getInstanceImpl(project).getSettings(runConfig) ?: return@forEach
-            val environment = ExecutionEnvironmentBuilder.createOrNull(DefaultRunExecutor.getRunExecutorInstance(), latexSettings)?.build() ?: return@forEach
+            val environment = ExecutionEnvironmentBuilder.createOrNull(DefaultRunExecutor.getRunExecutorInstance(), latexSettings)?.build()
+                ?: return@forEach
             RunConfigurationBeforeRunProvider.doExecuteTask(environment, latexSettings, null)
         }
 
