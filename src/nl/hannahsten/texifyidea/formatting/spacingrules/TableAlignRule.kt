0eb82cd7a9eea19cc0d69a35330838d9411047e2
Thomas Schouten <hollandpirates@gmail.com>
2021-10-07 19:37:50 +0200

    Improve support for escaped ampersands in table formatting

--- src/nl/hannahsten/texifyidea/formatting/spacingrules/TableAlignRule.kt
+++ src/nl/hannahsten/texifyidea/formatting/spacingrules/TableAlignRule.kt
@@ -92,7 +92,8 @@ fun getAmpersandOffsets(contentTextOffset: Int, indent: String, contentLines: Mu
     return contentLines.map { line ->
         val indices = mutableListOf<Int>()
         line.withIndex().forEach { (i, it) ->
-            if (it == '&') indices.add(currentOffset)
+            // Do not count escaped ampersands: \&
+            if ((it == '&') && (i == 0 || line[i - 1] != '\\')) indices.add(currentOffset)
             if (it == '\\' && if (i < line.length - 1) line[i + 1] == '\\' else false) indices.add(currentOffset)
             currentOffset++
         }
@@ -159,7 +160,8 @@ private fun removeExtraSpaces(contentLinesWithoutRules: MutableList<String>): Li
                 }
                 value in setOf(' ', '\n') -> {
                     if (i > 0 && i < line.length - 1) {
-                        val isAfterSpaceOrSeparator = line[i - 1] in setOf(' ', '&', '\n')
+                        // Spaces after an ignored ampersand are not removed
+                        val isAfterSpaceOrSeparator = (line[i - 1] in setOf(' ', '&', '\n') && (i < 2 || line[i - 2] != '\\'))
                         val isBeforeSpaceOrSeparator = line[i + 1] in setOf(' ', '&', '\n')
                         val isBeforeDoubleBackslash = i < line.length - 2 && line[i + 1] == '\\' && line[i + 2] == '\\'
                         if (isAfterSpaceOrSeparator || isBeforeSpaceOrSeparator || isBeforeDoubleBackslash) removedSpaces++
