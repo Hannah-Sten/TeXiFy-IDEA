f873a30fc04c4bb123e6242bddfef07734c2b522
Thomas Schouten <hollandpirates@gmail.com>
2022-03-23 08:49:44 +0100

    Add logging for executing system commands

--- src/nl/hannahsten/texifyidea/util/Log.kt
+++ src/nl/hannahsten/texifyidea/util/Log.kt
@@ -3,11 +3,12 @@ package nl.hannahsten.texifyidea.util
 import com.intellij.openapi.diagnostic.Logger
 
 /**
+ * Log messages to idea.log. During plugin development, it logs to build/idea-sandbox/system/log/idea.log.
  * @author Hannah Schellekens
  */
 object Log {
 
-    private val logger: Logger by lazy { Logger.getInstance(Log::class.java) }
+    val logger: Logger by lazy { Logger.getInstance(Log::class.java) }
 
     /**
      * Sends a formatted info message to the IntelliJ logger.
