c697e9b69fcc8d4a636be8a558d817fb5388053a
Thomas Schouten <hollandpirates@gmail.com>
2022-04-21 17:49:18 +0200

    Finish step after compilation is done

--- src/nl/hannahsten/texifyidea/run/step/OtherRunConfigurationStep.kt
+++ src/nl/hannahsten/texifyidea/run/step/OtherRunConfigurationStep.kt
@@ -8,10 +8,13 @@ import com.intellij.execution.compound.ConfigurationSelectionUtil
 import com.intellij.execution.compound.TypeNameTarget
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.impl.RunManagerImpl
+import com.intellij.execution.process.ProcessEvent
 import com.intellij.execution.process.ProcessHandler
+import com.intellij.execution.process.ProcessListener
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.wm.ToolWindowManager
 import nl.hannahsten.texifyidea.TeXception
 import nl.hannahsten.texifyidea.run.LatexRunConfiguration
@@ -91,33 +94,8 @@ class OtherRunConfigurationStep internal constructor(
     override fun execute(id: String, console: LatexExecutionConsole): ProcessHandler {
         // Don't silently skip the step when there is a problem
         val (settings, target) = mySettingsWithTarget ?: throw TeXception("Could not get settings from ${this.state.type} ${this.state.name}")
-        val environment = ExecutionEnvironmentBuilder.createOrNull(DefaultRunExecutor.getRunExecutorInstance(), settings)?.build()
-            ?: throw TeXception("Could not get run executor")
 
-        return object : ProcessHandler() {
-            override fun destroyProcessImpl() = notifyProcessTerminated(0)
-
-            override fun detachProcessImpl() = notifyProcessDetached()
-
-            override fun detachIsDefault(): Boolean = false
-
-            override fun getProcessInput(): OutputStream? = null
-
-            override fun startNotify() {
-                super.startNotify()
-                console.startStep(id, this@OtherRunConfigurationStep, this)
-                // Inspired by RunConfigurationBeforeRunProvider.doExecuteTask, but we cannot use it directly because the semaphore gets stuck if it's not the last step in the sequence
-                // Trigger other run config (will run later)
-                environment.runner.execute(environment)
-                // todo need to do the following _after_ execution, not directly after triggering it (execution will take back focus)
-                super.notifyProcessTerminated(0) // todo exit code?
-                console.finishStep(id, 0)
-                val executor = DefaultRunExecutor.getRunExecutorInstance()
-                val contentManager = ToolWindowManager.getInstance(configuration.project).getToolWindow(executor.toolWindowId)?.contentManager ?: return
-                contentManager.requestFocus(contentManager.getContent(0), true)
-
-            }
-        }
+        return OtherRunConfigurationProcessHandler(id, console, settings, this)
     }
 
     override fun clone(): Step {
@@ -133,5 +111,43 @@ class OtherRunConfigurationStep internal constructor(
 
     override fun loadState(state: TypeNameTarget) {
         this.state = state
+    }
+}
+
+class OtherRunConfigurationProcessHandler(val id: String, val console: LatexExecutionConsole, val settings: RunnerAndConfigurationSettings, val step: OtherRunConfigurationStep) : ProcessHandler() {
+    override fun destroyProcessImpl() = notifyProcessTerminated(0)
+
+    override fun detachProcessImpl() = notifyProcessDetached()
+
+    override fun detachIsDefault(): Boolean = false
+
+    override fun getProcessInput(): OutputStream? = null
+
+    override fun startNotify() {
+        super.startNotify()
+
+        val environment = ExecutionEnvironmentBuilder.createOrNull(DefaultRunExecutor.getRunExecutorInstance(), settings)?.build { descriptor ->
+            descriptor.processHandler?.addProcessListener(object : ProcessListener {
+                override fun startNotified(event: ProcessEvent) {}
+
+                override fun processTerminated(event: ProcessEvent) {
+                    val exitCode = event.exitCode
+                    this@OtherRunConfigurationProcessHandler.notifyProcessTerminated(exitCode)
+                    console.finishStep(id, exitCode)
+                    val executor = DefaultRunExecutor.getRunExecutorInstance()
+                    val contentManager = ToolWindowManager.getInstance(settings.configuration.project).getToolWindow(executor.toolWindowId)?.contentManager ?: return
+                    contentManager.requestFocus(contentManager.findContent("main"), true)
+                }
+
+                override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {}
+
+            })
+        }
+            ?: throw TeXception("Could not get run executor")
+
+        console.startStep(id, step, this)
+        // Inspired by RunConfigurationBeforeRunProvider.doExecuteTask, but we cannot use it directly because the semaphore gets stuck if it's not the last step in the sequence
+        // Trigger other run config (will run later)
+        environment.runner.execute(environment)
     }
 }
