bec6c31cb05ea2dc9fca9142dde1c3f5e27aea2b
Thomas Schouten <hollandpirates@gmail.com>
2022-04-29 13:32:41 +0200

    Change LatexPackageLocationCache.kt implementation to up-front caching

---
+++ src/nl/hannahsten/texifyidea/startup/LatexPackageLocationCacheInitializer.kt
@@ -0,0 +1,20 @@
+package nl.hannahsten.texifyidea.startup
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import nl.hannahsten.texifyidea.util.files.LatexPackageLocationCache
+
+/**
+ * Initialize package location cache, because filling it takes a long time, we do not want to do that only at the moment we need it (when resolving references).
+ */
+class LatexPackageLocationCacheInitializer : StartupActivity {
+    override fun runActivity(project: Project) {
+        // Not sure on which thread this is run, run in background to be sure
+        CoroutineScope(Dispatchers.Default).launch {
+            LatexPackageLocationCache.fillCacheWithKpsewhich(project)
+        }
+    }
+}
