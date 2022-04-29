547d6dc09b9b16838a096d0205af334e6a374574
Thomas Schouten <hollandpirates@gmail.com>
2022-04-29 14:35:05 +0200

    Fix in escaping paths

--- src/nl/hannahsten/texifyidea/startup/LatexPackageLocationCacheInitializer.kt
+++ src/nl/hannahsten/texifyidea/startup/LatexPackageLocationCacheInitializer.kt
@@ -11,6 +11,7 @@ import nl.hannahsten.texifyidea.util.files.LatexPackageLocationCache
  * Initialize package location cache, because filling it takes a long time, we do not want to do that only at the moment we need it (when resolving references).
  */
 class LatexPackageLocationCacheInitializer : StartupActivity {
+
     override fun runActivity(project: Project) {
         // Not sure on which thread this is run, run in background to be sure
         CoroutineScope(Dispatchers.Default).launch {
