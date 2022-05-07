498c65af3bfb844e0fbc7a7fd2b644ce7403d4c6
Thomas Schouten <hollandpirates@gmail.com>
2022-05-07 13:36:46 +0200

    Disable inline math completion in verbatim

---
+++ test/nl/hannahsten/texifyidea/editor/typedhandlers/LatexTypedHandlerTest.kt
@@ -0,0 +1,49 @@
+package nl.hannahsten.texifyidea.editor.typedhandlers
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import nl.hannahsten.texifyidea.file.LatexFileType
+
+class LatexTypedHandlerTest : BasePlatformTestCase() {
+
+    fun testInlineMath() {
+        myFixture.configureByText(LatexFileType, "")
+        myFixture.type("$")
+        myFixture.checkResult("$$")
+    }
+
+    fun testInlineMath2() {
+        myFixture.configureByText(LatexFileType, "$\\xi<caret>$")
+        myFixture.type("$")
+        myFixture.checkResult("$\\xi$<caret>")
+    }
+
+    fun testVerbatim() {
+        myFixture.configureByText(LatexFileType, """
+            \begin{verbatim}
+                <caret>
+            \end{verbatim}
+        """.trimIndent())
+        myFixture.type("$")
+        myFixture.checkResult("""
+            \begin{verbatim}
+                $<caret>
+            \end{verbatim}
+        """.trimIndent())
+    }
+
+    fun testDisplayMath() {
+        myFixture.configureByText(LatexFileType, "")
+        myFixture.type("\\[")
+        myFixture.checkResult("\\[\\]")
+    }
+
+    fun testDisplayMath2() {
+        myFixture.configureByText(LatexFileType, "\\[<caret>\\]")
+        myFixture.type("\n")
+        myFixture.checkResult("""
+            \[
+                <caret>
+            \]
+        """.trimIndent())
+    }
+}
