eeec1d61549ad7f150cac0819863f17861a8ca7c
Felix Berlakovich <felix@berlakovich.at>
2022-05-31 12:30:46 +0200

    comment and formatting

--- src/nl/hannahsten/texifyidea/index/LatexGlossaryEntryIndex.kt
+++ src/nl/hannahsten/texifyidea/index/LatexGlossaryEntryIndex.kt
@@ -4,6 +4,9 @@ import com.intellij.psi.stubs.StringStubIndexExtension
 import nl.hannahsten.texifyidea.LatexParserDefinition
 import nl.hannahsten.texifyidea.psi.LatexCommands
 
+/**
+ * Index all (supported) glossary entry commands.
+ */
 class LatexGlossaryEntryIndex : StringStubIndexExtension<LatexCommands>() {
 
     companion object : IndexUtilBase<LatexCommands>(LatexCommands::class.java, IndexKeys.GLOSSARY_ENTRIES_KEY)
