b91653ea454e3fb3fede43bbcbf55771405855b2
Greg Olmschenk <greg@olmschenk.com>
2022-06-17 22:08:23 -0400

    Remove PythonTeX environments and commands from latex inspections

--- src/nl/hannahsten/texifyidea/lang/commands/LatexListingCommand.kt
+++ src/nl/hannahsten/texifyidea/lang/commands/LatexListingCommand.kt
@@ -3,6 +3,7 @@ package nl.hannahsten.texifyidea.lang.commands
 import nl.hannahsten.texifyidea.lang.LatexPackage
 import nl.hannahsten.texifyidea.lang.LatexPackage.Companion.LISTINGS
 import nl.hannahsten.texifyidea.lang.LatexPackage.Companion.LUACODE
+import nl.hannahsten.texifyidea.lang.LatexPackage.Companion.PYTHONTEX
 
 /**
  * @author Hannah Schellekens
@@ -19,6 +20,11 @@ enum class LatexListingCommand(
     LSTINPUTLISTING("lstinputlisting", "options".asOptional(), RequiredFileArgument("filename", false, commaSeparatesArguments = false), dependency = LISTINGS),
     DIRECTLUA("directlua", dependency = LUACODE),
     LUAEXEC("luaexec", dependency = LUACODE),
+    PY("py", dependency = PYTHONTEX),
+    PYB("pyb", dependency = PYTHONTEX),
+    PYC("pyc", dependency = PYTHONTEX),
+    PYS("pys", dependency = PYTHONTEX),
+    PYV("pyv", dependency = PYTHONTEX),
     ;
 
     override val identifier: String
