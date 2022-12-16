f1fb7df581aef456992a5286440405f6a4b0849a
Thomas Schouten <hollandpirates@gmail.com>
2022-12-16 14:25:47 +0100

    Update qodana action

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 // Include the Gradle plugins which help building everything.
 // Supersedes the use of "buildscript" block and "apply plugin:"
 plugins {
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.10.1"
     kotlin("jvm") version ("1.7.22")
     kotlin("plugin.serialization") version ("1.7.22")
 
