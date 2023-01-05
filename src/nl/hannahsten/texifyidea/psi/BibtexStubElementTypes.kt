cf891b5815616d1ac3d48ca7dc3068b86650d382
Thomas Schouten <hollandpirates@gmail.com>
2023-01-05 09:15:33 +0100

    Use separate interfaces for stub element types

---
+++ src/nl/hannahsten/texifyidea/psi/BibtexStubElementTypes.kt
@@ -0,0 +1,9 @@
+package nl.hannahsten.texifyidea.psi
+
+import com.intellij.psi.tree.IElementType
+
+@Suppress("PropertyName")
+interface BibtexStubElementTypes {
+    val ENTRY: IElementType
+        get() = BibtexTypes.ENTRY
+}
