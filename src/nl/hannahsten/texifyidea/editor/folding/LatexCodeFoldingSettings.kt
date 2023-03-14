dd7b6d80c720fadef0cf53fdae4d457ff814b2bd
Thomas Schouten <hollandpirates@gmail.com>
2023-03-14 21:46:37 +0100

    Add setting to configure default folding of imports

---
+++ src/nl/hannahsten/texifyidea/editor/folding/LatexCodeFoldingSettings.kt
@@ -0,0 +1,27 @@
+package nl.hannahsten.texifyidea.editor.folding
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
+import com.intellij.util.xmlb.XmlSerializerUtil
+
+/**
+ * Settings for [LatexCodeFoldingOptionsProvider], inspired by RsCodeFoldingSettings from the Rust plugin.
+ * Other languages appear to mostly use editor.xml, so we use it as well.
+ * Note: in the debug instance, this file is at build/idea-sandbox/config/options/editor.xml.
+ */
+@State(name = "LatexCodeFoldingSettings", storages = [Storage("editor.xml")])
+class LatexCodeFoldingSettings : PersistentStateComponent<LatexCodeFoldingSettings> {
+
+    var collapseImports: Boolean = true
+
+    override fun getState() = this
+
+    override fun loadState(state: LatexCodeFoldingSettings) = XmlSerializerUtil.copyBean(state, this)
+
+    companion object {
+        fun getInstance(): LatexCodeFoldingSettings = service()
+    }
+
+}
