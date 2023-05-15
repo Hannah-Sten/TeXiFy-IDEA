87c72e32bcacff9df29d2c42937bc8b6b995e553
Abby Berkers <abbyberkers@gmail.com>
2023-05-15 21:44:22 +0200

    Add failing testcases

--- test/nl/hannahsten/texifyidea/inspections/latex/probablebugs/LatexSuspiciousSectionFormattingInspectionTest.kt
+++ test/nl/hannahsten/texifyidea/inspections/latex/probablebugs/LatexSuspiciousSectionFormattingInspectionTest.kt
@@ -14,11 +14,35 @@ internal class LatexSuspiciousSectionFormattingInspectionTest : TexifyInspection
         myFixture.checkHighlighting(true, false, true, false)
     }
 
+    fun `test backslash warning`() {
+        myFixture.configureByText(
+            LatexFileType,
+            "\\section{You should not use<warning descr=\"Suspicious formatting in \\section\">\\\\</warning>in the title of a section}"
+        )
+        myFixture.checkHighlighting(true, false, true, false)
+    }
+
+    fun `test multiple warnings in one section`() {
+        myFixture.configureByText(
+            LatexFileType,
+            "\\section{You should not use<warning descr=\"Suspicious formatting in \\section\">~</warning>in the title<warning descr=\"Suspicious formatting in \\section\">~</warning>of a section}"
+        )
+        myFixture.checkHighlighting(true, false, true, false)
+    }
+
     fun `test no warning when optional argument is present`() {
         myFixture.configureByText(
             LatexFileType,
             "\\section[Table of contents long title]{Title with explicit \\\\ formatting}"
         )
         myFixture.checkHighlighting(true, false, true, false)
+    }
+
+    fun `test simple quickfix`() {
+        myFixture.configureByText(
+            LatexFileType,
+            "\\section{You should not use~in the title}"
+        )
+        testQuickFix("\\section{You should not use~in the title}", "\\section[You should not use in the title]{You should not use~in the title}")
     }
 }
