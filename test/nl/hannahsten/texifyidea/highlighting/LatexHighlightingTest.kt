d293d2e7592a1efe4aa17f9ab923a93cd13fff78
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2023-06-17 16:30:57 -0700

    Add tests

--- test/nl/hannahsten/texifyidea/highlighting/LatexHighlightingTest.kt
+++ test/nl/hannahsten/texifyidea/highlighting/LatexHighlightingTest.kt
@@ -2,7 +2,6 @@ package nl.hannahsten.texifyidea.highlighting
 
 import com.intellij.testFramework.EditorTestUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import org.junit.Test
 
 class LatexHighlightingTest : BasePlatformTestCase() {
     override fun getTestDataPath(): String {
@@ -14,22 +13,8 @@ class LatexHighlightingTest : BasePlatformTestCase() {
         EditorTestUtil.testFileSyntaxHighlighting(testFile, "$testDataPath/CommandsAndCommentsOutput.tex", true)
     }
 
-    @Test
     fun testInlineMath() {
-        val testName = getTestName(false)
-        myFixture.configureByFile("$testName.tex")
-        val hls = myFixture.doHighlighting()
-        hls.forEach { println(it.text + ": " + it.problemGroup) }
-//        myFixture.checkHighlighting(false, true, false ,false)
-    }
-
-    @Test
-    fun testBracketMath() {
-        val testName = getTestName(false)
-        myFixture.configureByFile("$testName.tex")
-        val hls = myFixture.doHighlighting()
-        hls.forEach { println(it.text + ": " + it.problemGroup) }
-//        println(hls.joinToString("\n"))
-//        myFixture.checkHighlighting(false, true, false ,false)
+        val testFile = myFixture.configureByFile("InlineMath.tex")
+        EditorTestUtil.testFileSyntaxHighlighting(testFile, "$testDataPath/InlineMathOutput.tex", true)
     }
 }
