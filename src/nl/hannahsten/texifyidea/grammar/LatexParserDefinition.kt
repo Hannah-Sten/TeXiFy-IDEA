63b764ce463a83cea857d8a5ac8faafe99591cf5
Thomas Schouten <hollandpirates@gmail.com>
2023-08-14 09:04:21 +0200

    Release 0.9.0

--- src/nl/hannahsten/texifyidea/grammar/LatexParserDefinition.kt
+++ src/nl/hannahsten/texifyidea/grammar/LatexParserDefinition.kt
@@ -49,7 +49,7 @@ class LatexParserDefinition : ParserDefinition {
         val FILE: IStubFileElementType<*> = object : IStubFileElementType<LatexFileStub>(
             "LatexStubFileElementType", Language.findInstance(LatexLanguage::class.java)
         ) {
-            override fun getStubVersion(): Int = 58
+            override fun getStubVersion(): Int = 59
         }
     }
 
