657c22cc88d9cd50fc1966c280b95385f406bf82
Thomas Schouten <hollandpirates@gmail.com>
2023-12-03 11:12:17 +0100

    Disable SumatraPDF forward search

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,8 +78,9 @@ dependencies {
     // Local dependencies
     implementation(files("lib/pretty-tools-JDDE-2.1.0.jar"))
     // These lines can sometimes be problematic on Linux, but are required for SumatraPDF
-    implementation(files("lib/JavaDDE.dll"))
-    implementation(files("lib/JavaDDEx64.dll"))
+    // TODO Temporarily disabled to fix Linux builds
+//    implementation(files("lib/JavaDDE.dll"))
+//    implementation(files("lib/JavaDDEx64.dll"))
 
     // D-Bus Java bindings
     implementation("com.github.hypfvieh:dbus-java:3.3.2")
