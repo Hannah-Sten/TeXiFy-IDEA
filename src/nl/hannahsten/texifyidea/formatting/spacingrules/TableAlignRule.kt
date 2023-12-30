7e2809754f799e79724e3cb17c6113bd6a735513
Abby Berkers <abbyberkers@gmail.com>
2023-12-30 20:59:24 +0100

    Fix #3336: disable alignment of & in \url commands in table

--- src/nl/hannahsten/texifyidea/formatting/spacingrules/TableAlignRule.kt
+++ src/nl/hannahsten/texifyidea/formatting/spacingrules/TableAlignRule.kt
@@ -11,6 +11,7 @@ import nl.hannahsten.texifyidea.psi.getEnvironmentName
 import nl.hannahsten.texifyidea.util.getIndent
 import nl.hannahsten.texifyidea.util.magic.EnvironmentMagic
 import nl.hannahsten.texifyidea.util.parser.firstParentOfType
+import nl.hannahsten.texifyidea.util.parser.inUrl
 import kotlin.math.min
 
 /** At this length, we put table cells on their own line. */
@@ -20,9 +21,10 @@ const val LINE_LENGTH = 80
  * Align spaces to the right of &
  */
 fun rightTableSpaceAlign(latexCommonSettings: CommonCodeStyleSettings, parent: ASTBlock, left: ASTBlock): Spacing? {
-    // Only add spaces after &, unless escaped
+    // Only add spaces after &, unless escaped or inside url.
     if (left.node?.text?.endsWith("&") == false) return null
     if (left.node?.text?.endsWith("\\&") == true) return null
+    if (left.node?.psi?.inUrl() == true) return null
 
     if (parent.node?.psi?.firstParentOfType(LatexEnvironmentContent::class)
         ?.firstParentOfType(LatexEnvironment::class)?.getEnvironmentName() !in EnvironmentMagic.getAllTableEnvironments(
@@ -43,6 +45,8 @@ fun rightTableSpaceAlign(latexCommonSettings: CommonCodeStyleSettings, parent: A
  * Align spaces to the left of & or \\
  */
 fun leftTableSpaceAlign(latexCommonSettings: CommonCodeStyleSettings, parent: ASTBlock, right: ASTBlock): Spacing? {
+    if (right.node?.psi?.inUrl() == true) return null
+
     // Check if parent is in environment content of a table environment
     val contentElement = parent.node?.psi?.firstParentOfType(LatexEnvironmentContent::class)
     val project = parent.node?.psi?.project ?: ProjectManager.getInstance().defaultProject
