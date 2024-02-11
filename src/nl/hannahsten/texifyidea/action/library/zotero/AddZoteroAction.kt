5651a48d2ea0752eae62be3780f896049b14f188
Thomas Schouten <hollandpirates@gmail.com>
2024-02-11 17:07:45 +0100

    Support custom url for remote library connections

--- src/nl/hannahsten/texifyidea/action/library/zotero/AddZoteroAction.kt
+++ src/nl/hannahsten/texifyidea/action/library/zotero/AddZoteroAction.kt
@@ -25,15 +25,22 @@ class AddZoteroAction : AddLibraryAction<ZoteroLibrary, AddZoteroAction.AddZoter
     }
 
     override suspend fun createLibrary(dialogWrapper: AddZoteroDialogWrapper, project: Project): Either<RemoteLibraryRequestFailure, Pair<ZoteroLibrary, List<BibtexEntry>>?> = either {
-        val library = RemoteBibLibraryFactory.create<ZoteroLibrary>(ZoteroLibrary.NAME) ?: return@either null
+        val library = RemoteBibLibraryFactory.create<ZoteroLibrary>(dialogWrapper.name, dialogWrapper.url) ?: return@either null
         val credentials = Credentials(dialogWrapper.userID, dialogWrapper.userApiKey)
         PasswordSafe.instance.set(Zotero.userAttributes, credentials)
         val bibItems = library.getCollection().getOrElse { raise(it) }
-        RemoteLibraryManager.getInstance().updateLibrary(library, bibItems)
+        RemoteLibraryManager.getInstance().updateLibrary(library, bibItems, dialogWrapper.url)
         library to bibItems
     }
 
     class AddZoteroDialogWrapper(val project: Project) : AddLibDialogWrapper(ZoteroLibrary.NAME) {
+
+        var name: String = "Zotero"
+
+        // Other examples:
+        // https://api.zotero.org/groups/2608283/items?format=biblatex
+        // http://127.0.0.1:23119/better-bibtex/export/collection?/1/VXN7BVFN.biblatex
+        var url: String = ZoteroLibrary.DEFAULT_URL
 
         var userID: String = ""
 
@@ -45,6 +52,14 @@ class AddZoteroAction : AddLibraryAction<ZoteroLibrary, AddZoteroAction.AddZoter
 
         override fun createCenterPanel(): JComponent {
             return panel {
+                row("Name") {
+                    textField().bindText({ name }, { name = it })
+                    contextHelp("Name as shown in the TeXiFy Remote Library tool window")
+                }
+                row("URL") {
+                    textField().bindText({ url }, { url = it }).resizableColumn()
+                    contextHelp("Zotero API, see TeXiFy documentation for using groups or BBT")
+                }
                 row("User ID:") {
                     textField().bindText({ userID }, { userID = it }).focused()
                     contextHelp("You can find your user ID in Zotero Settings > Feeds/API.")
