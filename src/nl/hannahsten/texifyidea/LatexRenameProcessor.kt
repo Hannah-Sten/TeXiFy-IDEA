92a4b103de39db2256071db94cbbfa272d5b1259
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2024-05-16 00:19:34 -0600

    The simplest solution

---
+++ src/nl/hannahsten/texifyidea/LatexRenameProcessor.kt
@@ -0,0 +1,22 @@
+package nl.hannahsten.texifyidea
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.refactoring.rename.RenameDialog
+import com.intellij.refactoring.rename.RenamePsiElementProcessor
+import com.intellij.refactoring.rename.RenamePsiFileProcessor.PsiFileRenameDialog
+
+class LatexRenameProcessor: RenamePsiElementProcessor() {
+    override fun canProcessElement(element: PsiElement): Boolean {
+        return when (element) {
+            is PsiFile -> true
+            else -> true
+        }
+    }
+
+    override fun createRenameDialog(project: Project, element: PsiElement, nameSuggestionContext: PsiElement?, editor: Editor?): RenameDialog {
+        return PsiFileRenameDialog(project, element, nameSuggestionContext, null)
+    }
+}
