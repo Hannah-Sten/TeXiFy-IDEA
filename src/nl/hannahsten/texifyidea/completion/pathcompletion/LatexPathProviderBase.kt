94285f4499bba3cfa5cb2b662c12a146816a9c82
Thomas Schouten <hollandpirates@gmail.com>
2024-05-18 14:38:55 +0200

    Clean up LatexPathProviderBase and refresh file system before searching files

--- src/nl/hannahsten/texifyidea/completion/pathcompletion/LatexPathProviderBase.kt
+++ src/nl/hannahsten/texifyidea/completion/pathcompletion/LatexPathProviderBase.kt
@@ -122,7 +122,7 @@ abstract class LatexPathProviderBase : CompletionProvider<CompletionParameters>(
      * add completion entries for absolute path
      */
     private fun addAbsolutePathCompletion(baseDir: String) {
-        LocalFileSystem.getInstance().findFileByPath(baseDir)?.let { dirFile ->
+        LocalFileSystem.getInstance().refreshAndFindFileByPath(baseDir)?.let { dirFile ->
             if (searchFolders()) {
                 addFolderNavigations(baseDir)
                 getContents(dirFile, true).forEach {
@@ -272,18 +272,6 @@ abstract class LatexPathProviderBase : CompletionProvider<CompletionParameters>(
      * search in given path for subfiles or directories
      */
     private fun getContents(base: VirtualFile?, directory: Boolean): List<VirtualFile> {
-        val contents = java.util.ArrayList<VirtualFile>()
-
-        if (base == null) {
-            return contents
-        }
-
-        for (file in base.children) {
-            if (file.isDirectory == directory) {
-                contents.add(file)
-            }
-        }
-
-        return contents
+        return base?.children?.filter { it.isDirectory == directory } ?: mutableListOf()
     }
 }
