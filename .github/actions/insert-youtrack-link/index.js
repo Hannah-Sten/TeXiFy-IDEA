e4ed32a7ffb403ebe0632259ac99fbc8545d50d0
Thomas Schouten <hollandpirates@gmail.com>
2024-10-22 21:26:52 +0200

    Use module

--- .github/actions/insert-youtrack-link/index.js
+++ .github/actions/insert-youtrack-link/index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core';
+import github from '@actions/github';
 import {Octokit} from "@octokit/rest";
 
 function getText() {
