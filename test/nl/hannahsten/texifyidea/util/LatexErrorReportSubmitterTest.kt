9aa6bba4f6617a4d22769dd6819af5e19fee2826
Thomas Schouten <hollandpirates@gmail.com>
2024-11-20 12:07:38 +0100

    Fix test

--- test/nl/hannahsten/texifyidea/util/LatexErrorReportSubmitterTest.kt
+++ test/nl/hannahsten/texifyidea/util/LatexErrorReportSubmitterTest.kt
@@ -9,7 +9,7 @@ class LatexErrorReportSubmitterTest {
     @Test
     fun testLongStacktrace() {
         val stacktrace = """
-            com.intellij.diagnostic.PluginException: Cannot create extension (class=nl.hannahsten.texifyidea.index.file.LatexExternalEnvironmentIndex) [Plugin: nl.rubensten.texifyidea]
+        com.intellij.diagnostic.PluginException: Cannot create extension (class=nl.hannahsten.texifyidea.index.file.LatexExternalEnvironmentIndex) [Plugin: nl.rubensten.texifyidea]
                 at com.intellij.serviceContainer.ComponentManagerImpl.createError(ComponentManagerImpl.kt:980)
                 at com.intellij.openapi.extensions.impl.XmlExtensionAdapter.doCreateInstance(XmlExtensionAdapter.kt:73)
                 at com.intellij.openapi.extensions.impl.XmlExtensionAdapter.createInstance(XmlExtensionAdapter.kt:33)
@@ -27,7 +27,7 @@ class LatexErrorReportSubmitterTest {
                 at com.intellij.indexing.shared.platform.impl.SharedIndexChunkConfigurationImpl.openFileBasedIndexChunk(SharedIndexChunkConfigurationImpl.java:657)
                 at com.intellij.indexing.shared.platform.impl.SharedIndexChunkConfigurationImpl.registerChunk(SharedIndexChunkConfigurationImpl.java:618)
                 at com.intellij.indexing.shared.platform.impl.SharedIndexChunkConfigurationImpl.openChunkForProject(SharedIndexChunkConfigurationImpl.java:484)
-            Caused by: java.util.concurrent.ExecutionException: java.lang.InterruptedException
+        Caused by: java.util.concurrent.ExecutionException: java.lang.InterruptedException
                 at java.base/java.util.concurrent.CompletableFuture.reportGet(CompletableFuture.java:396)
                 at java.base/java.util.concurrent.CompletableFuture.get(CompletableFuture.java:2096)
                 at org.jetbrains.concurrency.AsyncPromise.get(AsyncPromise.kt:51)
@@ -37,7 +37,7 @@ class LatexErrorReportSubmitterTest {
         """.trimIndent()
         val trimmedStacktrace = LatexErrorReportSubmitter.Util.filterInterestingLines(stacktrace)
         val expected = """
-            com.intellij.diagnostic.PluginException: Cannot create extension (class=nl.hannahsten.texifyidea.index.file.LatexExternalEnvironmentIndex) [Plugin: nl.rubensten.texifyidea]
+        com.intellij.diagnostic.PluginException: Cannot create extension (class=nl.hannahsten.texifyidea.index.file.LatexExternalEnvironmentIndex) [Plugin: nl.rubensten.texifyidea]
                 at com.intellij.serviceContainer.ComponentManagerImpl.createError(ComponentManagerImpl.kt:980)
                 at com.intellij.openapi.extensions.impl.XmlExtensionAdapter.doCreateInstance(XmlExtensionAdapter.kt:73)
                 at com.intellij.openapi.extensions.impl.XmlExtensionAdapter.createInstance(XmlExtensionAdapter.kt:33)
@@ -55,7 +55,7 @@ class LatexErrorReportSubmitterTest {
                 at com.intellij.indexing.shared.platform.impl.SharedIndexChunkConfigurationImpl.openFileBasedIndexChunk(SharedIndexChunkConfigurationImpl.java:657)
                 (...)
                 at com.intellij.indexing.shared.platform.impl.SharedIndexChunkConfigurationImpl.openChunkForProject(SharedIndexChunkConfigurationImpl.java:484)
-            Caused by: java.util.concurrent.ExecutionException: java.lang.InterruptedException
+        Caused by: java.util.concurrent.ExecutionException: java.lang.InterruptedException
                 at java.base/java.util.concurrent.CompletableFuture.reportGet(CompletableFuture.java:396)
                 (...)
                 at org.jetbrains.concurrency.AsyncPromise.blockingGet(AsyncPromise.kt:130)
