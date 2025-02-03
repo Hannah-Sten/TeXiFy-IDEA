0d42acc45432f5af7a176845eb445a9ade23c224
Thomas Schouten <hollandpirates@gmail.com>
2025-02-03 21:24:15 +0100

    the rewrite

--- src/nl/hannahsten/texifyidea/editor/folding/LatexSectionFoldingBuilder.kt
+++ src/nl/hannahsten/texifyidea/editor/folding/LatexSectionFoldingBuilder.kt
@@ -37,9 +37,8 @@ open class LatexSectionFoldingBuilder : FoldingBuilderEx() {
             .sortedBy { it.textOffset }
         val comments = root.childrenOfType<LatexMagicComment>().filter { it.key() == DefaultMagicKeys.FAKE }
         // If the user has a custom folding region interleaving with sections, e.g. spanning multiple sections but ending inside a section, we give the user priority over the default section folding (so that the user does not need to put fake sections everywhere)
-        val customRegions = root.childrenOfType<LatexMagicComment>().filter { it.key() == CustomMagicKey("region") || it.key() == CustomMagicKey("endregion") }
+        val customRegions = root.childrenOfType<LatexMagicComment>().filter { it.isStartRegion() || it.isEndRegion() }
         val sectionElements: List<PsiElement> = (commands + comments).sortedBy { it.textOffset }
-        val sectionAndRegionElements: List<PsiElement> = (sectionElements + customRegions).sortedBy { it.textOffset }
 
         if (sectionElements.isEmpty()) {
             return descriptors.toTypedArray()
@@ -57,58 +56,53 @@ open class LatexSectionFoldingBuilder : FoldingBuilderEx() {
         }
 
         for (currentFoldingCommand in sectionElements) {
-            var foundHigherCommand = false
             val currentCommandRank = sectionCommandNames.indexOf(currentFoldingCommand.name() ?: continue)
-
-            // Count nested regions
-            var customRegionCounter = 0
 
             // Find the 'deepest' section under currentFoldingCommand,
             // so when we find something that is equal or lower in rank
             // (a section is ranked lower than a subsection) then we
             // get the block of text between it and the currentFoldingCommand
-            for (nextFoldingCommand in sectionAndRegionElements.filter { it.startOffset > currentFoldingCommand.startOffset }) {
-                // Keep track of custom folding regions within a section that we should skip (we are looking for an endregion that starts before this section, and are assuming that these are balanced)
-                // todo now do the same for custom regions that are starting in this section but not ending here
-                if (nextFoldingCommand is LatexMagicComment) {
-                    if (nextFoldingCommand.key() == CustomMagicKey("region")) {
-                        customRegionCounter++
-                        continue
+            val nextElements = sectionElements.filter { it.startOffset > currentFoldingCommand.startOffset }
+            // If we found a command which is ranked lower, save the block of text inbetween
+            // Note that a section is ranked lower than a subsection
+            val sectionEnd = nextElements.firstOrNull { sectionCommandNames.indexOf(it.name()) <= currentCommandRank }
+
+            // Keep track of custom folding regions within a section that we should skip
+            // We are looking for an endregion that starts before this section, or a start region that ends after this section,
+            // so effectively we are looking for the first unmatched (within this section) region command
+            val firstUnmatchedRegion = customRegions
+                .filter { it.startOffset > currentFoldingCommand.startOffset }
+                .filter { if (sectionEnd == null) true else it.startOffset < sectionEnd.startOffset }
+                .fold(listOf<LatexMagicComment>()) { unmatchedIndices, comment ->
+                    if (comment.isStartRegion()) {
+                        unmatchedIndices + comment
                     }
-                    else if (nextFoldingCommand.key() == CustomMagicKey("endregion")) {
-                        customRegionCounter--
+                    // If the last one is an end, it is already not matched - don't remove it
+                    else if (unmatchedIndices.lastOrNull()?.isStartRegion() == true) {
+                        unmatchedIndices.dropLast(1)
                     }
-                    if (customRegionCounter >= 0) continue
+                    else {
+                        unmatchedIndices
+                    }
                 }
+                .firstOrNull()
 
-                // Find the rank of the next command to compare with the current rank
-                val nextCommandRank = sectionCommandNames.indexOf(nextFoldingCommand.name())
+            if (sectionEnd != null) {
+                // Get the location of the next folding command
+                val end = (firstUnmatchedRegion ?: sectionEnd).parentOfType(LatexNoMathContent::class)?.previousSiblingIgnoreWhitespace()
+                    ?: break
 
-                // If we found a command which is ranked lower, save the block of text inbetween
-                // Note that a section is ranked lower than a subsection
-                if (nextCommandRank <= currentCommandRank) {
-                    // Get the location of the next folding command
-                    val end = nextFoldingCommand.parentOfType(LatexNoMathContent::class)?.previousSiblingIgnoreWhitespace()
-                        ?: break
+                // Get the text range between the current and the next folding command
+                if (end.textOffset + end.textLength - currentFoldingCommand.textOffset > 0) {
+                    val foldingRange = TextRange(currentFoldingCommand.textOffset, end.textOffset + end.textLength)
 
-                    // Get the text range between the current and the next folding command
-                    if (end.textOffset + end.textLength - currentFoldingCommand.textOffset > 0) {
-                        val foldingRange = TextRange(currentFoldingCommand.textOffset, end.textOffset + end.textLength)
-
-                        // Add it as a folding block
-                        descriptors.add(FoldingDescriptor(currentFoldingCommand, foldingRange))
-                    }
-
-                    foundHigherCommand = true
-                    break
+                    // Add it as a folding block
+                    descriptors.add(FoldingDescriptor(currentFoldingCommand, foldingRange))
                 }
             }
-
-            /*
-             * If this item is the topmost level of the section structure or the last section marker,
-             * use the end of all text as the end of the range.
-             */
-            if (!foundHigherCommand) {
+            // If this item is the topmost level of the section structure or the last section marker,
+            // use the end of all text as the end of the range.
+            else {
                 val foldingContent = sectionElements.last().parentOfType(LatexNoMathContent::class) ?: continue
                 var nextContent: LatexNoMathContent? = foldingContent
                 var previousContent: LatexNoMathContent = foldingContent
@@ -116,10 +110,11 @@ open class LatexSectionFoldingBuilder : FoldingBuilderEx() {
                     previousContent = nextContent
                     nextContent = nextContent.nextSiblingIgnoreWhitespace() as? LatexNoMathContent
                 }
-                if (previousContent.textOffset + previousContent.textLength - currentFoldingCommand.textOffset > 0) {
+                val sectionEnd = firstUnmatchedRegion ?: previousContent
+                if (sectionEnd.textOffset + sectionEnd.textLength - currentFoldingCommand.textOffset > 0) {
                     val foldingRange = TextRange(
                         currentFoldingCommand.textOffset,
-                        previousContent.textOffset + previousContent.textLength
+                        sectionEnd.textOffset + sectionEnd.textLength
                     )
                     descriptors.add(FoldingDescriptor(currentFoldingCommand, foldingRange))
                 }
@@ -127,4 +122,7 @@ open class LatexSectionFoldingBuilder : FoldingBuilderEx() {
         }
         return descriptors.toTypedArray()
     }
+
+    private fun LatexMagicComment.isStartRegion(): Boolean = key() == CustomMagicKey("region")
+    private fun LatexMagicComment.isEndRegion(): Boolean = key() == CustomMagicKey("endregion")
 }
