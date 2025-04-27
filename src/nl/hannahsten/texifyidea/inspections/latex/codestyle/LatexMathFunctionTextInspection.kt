979e56ca20e036f2e38d4f921b9a458359b7fc76
Abby Berkers <abbyberkers@gmail.com>
2025-04-27 16:29:18 +0200

    Refactor math function quick fix from text to psi replacement

--- src/nl/hannahsten/texifyidea/inspections/latex/codestyle/LatexMathFunctionTextInspection.kt
+++ src/nl/hannahsten/texifyidea/inspections/latex/codestyle/LatexMathFunctionTextInspection.kt
@@ -9,18 +9,16 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.intellij.psi.SmartPointerManager
 import com.intellij.psi.SmartPsiElementPointer
-import com.intellij.psi.util.endOffset
-import com.intellij.psi.util.startOffset
 import nl.hannahsten.texifyidea.file.LatexFileType
 import nl.hannahsten.texifyidea.inspections.InsightGroup
 import nl.hannahsten.texifyidea.inspections.TexifyInspectionBase
 import nl.hannahsten.texifyidea.psi.LatexCommands
+import nl.hannahsten.texifyidea.psi.LatexPsiHelper
 import nl.hannahsten.texifyidea.util.files.commandsInFile
 import nl.hannahsten.texifyidea.util.files.document
 import nl.hannahsten.texifyidea.util.magic.CommandMagic
 import nl.hannahsten.texifyidea.util.parser.inMathContext
 import nl.hannahsten.texifyidea.util.parser.requiredParameter
-import nl.hannahsten.texifyidea.util.parser.requiredParameters
 
 /**
  * @author Hannah Schellekens
@@ -63,19 +61,17 @@ open class LatexMathFunctionTextInspection : TexifyInspectionBase() {
 
         override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
             val textCommand = textCommandPointer.element ?: return
-            val document = textCommand.containingFile.document() ?: return
-            val mathFunction = replacementString(textCommand) ?: return
-            document.replaceString(textCommand.startOffset, textCommand.requiredParameters()[0].endOffset, mathFunction)
+            val mathFunction = extractFunction(textCommand) ?: return
+            textCommand.replace(LatexPsiHelper(project).createFromText(mathFunction).firstChild)
         }
 
         override fun generatePreview(project: Project, previewDescriptor: ProblemDescriptor): IntentionPreviewInfo {
             val textCommand = textCommandPointer.element ?: return IntentionPreviewInfo.EMPTY
-            return replacementString(textCommand)
-                ?.let { IntentionPreviewInfo.CustomDiff(LatexFileType, textCommand.text, it) }
-                ?: IntentionPreviewInfo.EMPTY
+            val functionText = extractFunction(textCommand) ?: return IntentionPreviewInfo.EMPTY
+            return IntentionPreviewInfo.CustomDiff(LatexFileType, textCommand.text, functionText)
         }
 
-        private fun replacementString(textCommandElement: LatexCommands): String? {
+        private fun extractFunction(textCommandElement: LatexCommands): String? {
             return textCommandElement.requiredParameter(0)?.trim()?.let { "\\$it" }
         }
     }
