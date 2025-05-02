25ded4cff76452fc4708b4f1615db47fa8ae08e6
Abby Berkers <abbyberkers@gmail.com>
2025-05-02 14:09:50 +0200

    Fix en dash inspection preview

--- src/nl/hannahsten/texifyidea/inspections/latex/typesetting/LatexEnDashInspection.kt
+++ src/nl/hannahsten/texifyidea/inspections/latex/typesetting/LatexEnDashInspection.kt
@@ -1,10 +1,6 @@
 package nl.hannahsten.texifyidea.inspections.latex.typesetting
 
-import com.intellij.codeInspection.ProblemDescriptor
-import com.intellij.psi.PsiFile
 import nl.hannahsten.texifyidea.inspections.TexifyRegexInspection
-import nl.hannahsten.texifyidea.util.files.document
-import nl.hannahsten.texifyidea.util.length
 import nl.hannahsten.texifyidea.util.magic.PatternMagic
 import nl.hannahsten.texifyidea.util.toTextRange
 import java.util.regex.Pattern
@@ -20,19 +16,7 @@ open class LatexEnDashInspection : TexifyRegexInspection(
     quickFixName = { "Convert to en dash" },
     cancelIf = { matcher, _ -> PatternMagic.correctEnDash.matcher(matcher.group(1)).matches() },
     replacementRange = { it.groupRange(1) },
+    replacement = { matcher, _ -> "${matcher.group(2)}--${matcher.group(3)}"},
     highlightRange = { it.groupRange(1).toTextRange() },
     groupFetcher = { listOf(it.group(2), it.group(3)) }
-) {
-
-    override fun applyFix(descriptor: ProblemDescriptor, replacementRange: IntRange, replacement: String, groups: List<String>): Int {
-        val file = descriptor.psiElement as PsiFile
-        val document = file.document() ?: return 0
-        val start = groups[0]
-        val end = groups[1]
-
-        val dashReplacement = "$start--$end"
-        document.replaceString(replacementRange.first, replacementRange.last, dashReplacement)
-
-        return dashReplacement.length - replacementRange.length
-    }
-}
+)
