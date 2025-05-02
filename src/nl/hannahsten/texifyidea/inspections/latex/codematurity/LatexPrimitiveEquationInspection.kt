2e34c26b8e670e6e8d9a638f4141f15e36575952
Abby Berkers <abbyberkers@gmail.com>
2025-05-02 14:39:56 +0200

    Fix primitive equation inspection preview

--- src/nl/hannahsten/texifyidea/inspections/latex/codematurity/LatexPrimitiveEquationInspection.kt
+++ src/nl/hannahsten/texifyidea/inspections/latex/codematurity/LatexPrimitiveEquationInspection.kt
@@ -1,12 +1,8 @@
 package nl.hannahsten.texifyidea.inspections.latex.codematurity
 
-import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
-import com.intellij.psi.PsiFile
 import nl.hannahsten.texifyidea.inspections.TexifyRegexInspection
-import nl.hannahsten.texifyidea.util.files.document
 import nl.hannahsten.texifyidea.util.toTextRange
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 /**
@@ -16,27 +12,11 @@ open class LatexPrimitiveEquationInspection : TexifyRegexInspection(
     inspectionDisplayName = "Discouraged use of primitive TeX display math",
     inspectionId = "PrimitiveEquation",
     errorMessage = { "Use '\\[..\\]' instead of primitive TeX display math." },
-    pattern = Pattern.compile("(\\\$\\\$)[^\$]*\\\$?[^\$]*(\\\$\\\$)"),
+    pattern = Pattern.compile("\\\$\\\$([^\$]*\\\$?[^\$]*)\\\$\\\$"),
     mathMode = false,
-    replacement = { _, _ -> "" },
-    replacementRange = Util::replaceRange,
+    replacement = { matcher, _ -> "\\[${matcher.group(1)}\\]" },
+    replacementRange = { it.groupRange(0) },
     highlight = ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-    highlightRange = { Util.replaceRange(it).toTextRange().grown(-1) },
+    highlightRange = { it.groupRange(0).toTextRange() },
     quickFixName = { "Replace with '\\[..\\]'" }
-) {
-
-    object Util {
-        fun replaceRange(it: Matcher) = (it.groupRange(1).first..it.groupRange(2).last)
-    }
-
-    override fun applyFix(descriptor: ProblemDescriptor, replacementRange: IntRange, replacement: String, groups: List<String>): Int {
-        val file = descriptor.psiElement as PsiFile
-        val document = file.document() ?: return 0
-
-        document.replaceString(replacementRange.first, replacementRange.first + 2, "\\[")
-        document.replaceString(replacementRange.last - 2, replacementRange.last, "\\]")
-
-        // $$ were replaced by \[ or \] so the length did not change
-        return 0
-    }
-}
+)
