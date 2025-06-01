16bb796e57108ff12da6228d35cd51d59438fcd2
Thomas Schouten <hollandpirates@gmail.com>
2025-06-01 14:59:00 +0200

    Use ConcurrentHashMap in runCommandNonBlocking

--- src/nl/hannahsten/texifyidea/util/CommandRunner.kt
+++ src/nl/hannahsten/texifyidea/util/CommandRunner.kt
@@ -41,10 +41,10 @@ suspend fun runCommandNonBlocking(
 
         // where/which commands occur often but do not change since the output depends on PATH, so can be cached
         val isExecutableLocationCommand = commands.size == 2 && listOf("where", "which").contains(commands[0])
-        if (isExecutableLocationCommand && SystemEnvironment.executableLocationCache[commands[1]] != null) {
-            val standardOutput = SystemEnvironment.executableLocationCache[commands[1]]
-            Log.debug("Retrieved output of $commands from cache: $standardOutput")
-            return@withContext CommandResult(0, standardOutput, null)
+        val cachedExecutableLocation = SystemEnvironment.executableLocationCache[commands[1]]
+        if (isExecutableLocationCommand && cachedExecutableLocation != null) {
+            Log.debug("Retrieved output of $commands from cache: $cachedExecutableLocation")
+            return@withContext CommandResult(0, cachedExecutableLocation, null)
         }
 
         val processBuilder = GeneralCommandLine(*commands)
@@ -123,28 +123,6 @@ catch (e: IOException) {
         e.message
     }
     else throw e
-}
-
-suspend fun sendCommandNonBlocking(
-    vararg commands: String,
-    workingDirectory: Path? = null,
-    timeout: Long = 3
-): Int {
-    val processBuilder = GeneralCommandLine(*commands).withWorkingDirectory(workingDirectory)
-        .toProcessBuilder()
-    processBuilder.redirectError(ProcessBuilder.Redirect.DISCARD)
-    processBuilder.redirectOutput(ProcessBuilder.Redirect.DISCARD)
-    val process = processBuilder.start()
-    val exitCode = withTimeoutOrNull(1_000 * timeout) {
-        withContext(Dispatchers.IO) {
-            process.awaitExit()
-        }
-    } ?: run {
-        process.destroy()
-        Log.debug("${commands.firstOrNull()} destroyed after timeout $timeout seconds")
-        -1
-    }
-    return exitCode
 }
 
 /**
