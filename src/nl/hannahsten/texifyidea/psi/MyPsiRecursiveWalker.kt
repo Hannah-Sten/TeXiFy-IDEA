3126e2c118caee14709e5691c2ccba0e5f2e049c
Li Ernest <li-ernest@qq.com>
2025-08-11 13:47:47 -0400

    recover latex recursive walker

--- src/nl/hannahsten/texifyidea/psi/MyPsiRecursiveWalker.kt
+++ src/nl/hannahsten/texifyidea/psi/MyPsiRecursiveWalker.kt
@@ -86,3 +86,13 @@ abstract class MyPsiRecursiveWalker(private var depth: Int) : PsiRecursiveElemen
 
     protected open fun elementEnd(e: PsiElement) = Unit
 }
+
+abstract class LatexPsiRecursiveWalker(depth: Int) : MyPsiRecursiveWalker(depth) {
+
+    override fun elementStart(e: PsiElement) {
+        if(e is LatexNormalText || e is PsiWhiteSpace) {
+            // Skip normal text and whitespace elements
+            goDown = false
+        }
+    }
+}
