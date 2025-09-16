43d1308ac60f2b372431467013564e58f355a69a
Li Ernest <li-ernest@qq.com>
2025-09-15 22:18:26 -0400

    Cleanup

--- src/nl/hannahsten/texifyidea/inspections/latex/codestyle/LatexGroupedSubSupScriptInspection.kt
+++ src/nl/hannahsten/texifyidea/inspections/latex/codestyle/LatexGroupedSubSupScriptInspection.kt
@@ -10,30 +10,6 @@ import nl.hannahsten.texifyidea.lang.LatexContexts
 /**
  * @author Hannah Schellekens
  */
-// open class LatexGroupedSubSupScriptInspection : TexifyRegexInspection(
-//    inspectionDisplayName = "Grouped superscript and subscript",
-//    inspectionId = "GroupedSubSupScript",
-//    errorMessage = {
-//        val subSup = if (it.group(1) == "_") "Sub" else "Super"
-//        "${subSup}script is not grouped"
-//    },
-//    pattern = Pattern.compile("((?<!(\\\\)|(\\\\string))[_^])([a-zA-Z0-9][a-zA-Z0-9]+)"),
-//    mathMode = true,
-//    highlightRange = { TextRange(it.start() - 1, it.end()) },
-//    replacement = { it, _ -> "{${it.group(4)}}" },
-//    replacementRange = { it.groupRange(4) },
-//    quickFixName = { "Insert curly braces" }
-// ) {
-//
-//    override fun checkContext(element: PsiElement): Boolean {
-//        val parent = element.parentOfType(LatexCommands::class)
-//        return when (parent?.name) {
-//            "\\label", "\\bibitem" -> false
-//            else -> super.checkContext(element)
-//        }
-//    }
-// }
-
 class LatexGroupedSubSupScriptInspection : AbstractTexifyRegexBasedInspection(
     inspectionId = "GroupedSubSupScript",
     regex = """(?<!(\\)|(\\string))(?<subsup>[_^])(?<content>[a-zA-Z0-9]{2,})""".toRegex(),
--- src/nl/hannahsten/texifyidea/inspections/latex/typesetting/spacing/LatexSentenceEndWithCapitalInspection.kt
+++ src/nl/hannahsten/texifyidea/inspections/latex/typesetting/spacing/LatexSentenceEndWithCapitalInspection.kt
@@ -36,22 +36,3 @@ class LatexSentenceEndWithCapitalInspection : AbstractTexifyRegexBasedInspection
         return nextWhiteSpace.textContains('\n')
     }
 }
-
-// open class LatexSentenceEndWithCapitalInspection : TexifyRegexInspection(
-//    inspectionDisplayName = "End-of-sentence space after sentences ending with capitals",
-//    inspectionId = "SentenceEndWithCapital",
-//    errorMessage = { "Sentences ending with a capital letter should end with an end-of-sentence space" },
-//    pattern = Pattern.compile("[A-ZÀ-Ý](\\.)[ \\t]*\\n"),
-//    replacement = { _, _ -> "\\@." },
-//    replacementRange = { it.start(1)..it.start(1) + 1 },
-//    quickFixName = { "Add an end-of-sentence space" },
-//    cancelIf = { matcher, file -> isInElement<LatexCommands>(matcher, file) }
-// ) {
-//
-//    override fun checkContext(matcher: Matcher, element: PsiElement): Boolean {
-//        if (element.inDirectEnvironment(setOf("verbatim", "verbatim*", "lstlisting"))) {
-//            return false
-//        }
-//        return super.checkContext(matcher, element)
-//    }
-// }
