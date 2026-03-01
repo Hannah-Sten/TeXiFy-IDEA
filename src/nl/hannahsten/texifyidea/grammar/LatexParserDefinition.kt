d6257c67fa9e584dec33fe35cf8425fe2511bd2b
Thomas Schouten <15669080+PHPirates@users.noreply.github.com>
2026-03-01 08:51:01 +0100

    Release 0.11.8

--- src/nl/hannahsten/texifyidea/grammar/LatexParserDefinition.kt
+++ src/nl/hannahsten/texifyidea/grammar/LatexParserDefinition.kt
@@ -52,7 +52,7 @@ object LatexStubFileElementType : IStubFileElementType<LatexFileStub>(
     // debugName is required to let IntelliJ distinguish between this FILE and BibtexParserDefinition.FILE
     "LatexStubFileElementType", Language.findInstance(LatexLanguage::class.java)
 ) {
-    override fun getStubVersion(): Int = 93
+    override fun getStubVersion(): Int = 94
 
     override fun getExternalId(): @NonNls String = "texify.latex.LatexStubFileElementType"
 
