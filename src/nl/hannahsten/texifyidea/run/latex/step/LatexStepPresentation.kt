d9d8fece019fd5292ed524e06d71831ae0bbea01
Li Ernest <li-ernest@qq.com>
2026-03-01 16:22:44 +0800

    I18n for new run config

--- src/nl/hannahsten/texifyidea/run/latex/step/LatexStepPresentation.kt
+++ src/nl/hannahsten/texifyidea/run/latex/step/LatexStepPresentation.kt
@@ -5,19 +5,18 @@ import nl.hannahsten.texifyidea.run.latex.LatexStepType
 
 internal object LatexStepPresentation {
 
-    private val descriptionKeys: Map<String, String> = mapOf(
-        LatexStepType.LATEX_COMPILE to "run.step.type.compile.latex",
-        LatexStepType.LATEXMK_COMPILE to "run.step.type.compile.latexmk",
-        LatexStepType.EXTERNAL_TOOL to "run.step.type.external.tool",
-        LatexStepType.MAKEINDEX to "run.step.type.makeindex",
-        LatexStepType.BIBTEX to "run.step.type.bibtex",
-        LatexStepType.PYTHONTEX to "run.step.type.pythontex",
-        LatexStepType.MAKEGLOSSARIES to "run.step.type.makeglossaries",
-        LatexStepType.XINDY to "run.step.type.xindy",
-        LatexStepType.PDF_VIEWER to "run.step.type.pdf.viewer",
-        LatexStepType.FILE_CLEANUP to "run.step.type.file.cleanup",
+    private val descriptions: Map<String, String> = mapOf(
+        LatexStepType.LATEX_COMPILE to TexifyBundle.message("run.step.type.compile.latex"),
+        LatexStepType.LATEXMK_COMPILE to TexifyBundle.message("run.step.type.compile.latexmk"),
+        LatexStepType.EXTERNAL_TOOL to TexifyBundle.message("run.step.type.external.tool"),
+        LatexStepType.MAKEINDEX to TexifyBundle.message("run.step.type.makeindex"),
+        LatexStepType.BIBTEX to TexifyBundle.message("run.step.type.bibtex"),
+        LatexStepType.PYTHONTEX to TexifyBundle.message("run.step.type.pythontex"),
+        LatexStepType.MAKEGLOSSARIES to TexifyBundle.message("run.step.type.makeglossaries"),
+        LatexStepType.XINDY to TexifyBundle.message("run.step.type.xindy"),
+        LatexStepType.PDF_VIEWER to TexifyBundle.message("run.step.type.pdf.viewer"),
+        LatexStepType.FILE_CLEANUP to TexifyBundle.message("run.step.type.file.cleanup"),
     )
 
-    fun displayName(type: String): String = descriptionKeys[type]?.let(TexifyBundle::message)
-        ?: TexifyBundle.message("run.step.type.unsupported", type)
+    fun displayName(type: String): String = descriptions[type] ?: TexifyBundle.message("run.step.type.unsupported", type)
 }
