64dd08128458844a68d6a94535d58f5a405f948d
Thomas Schouten <15669080+PHPirates@users.noreply.github.com>
2026-09-13 10:14:43 +0200

    Change gradle settings to match intellij template

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 // NOTE: when updating versions, also update Qodana in qodana.yaml
 plugins {
-    id("org.jetbrains.intellij.platform") version "2.18.1"
+    id("org.jetbrains.intellij.platform")
     kotlin("jvm") version ("2.4.0")
     kotlin("plugin.serialization") version ("2.4.0")
 
@@ -219,7 +219,6 @@ changelog {
 
 intellijPlatform {
     pluginConfiguration {
-        name = "TeXiFy-IDEA"
         // Get the latest available change notes from the changelog file
         changeNotes = (
             provider {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,27 @@
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 rootProject.name = "TeXiFy-IDEA"
 
-// https://github.com/JetBrains/intellij-platform-gradle-plugin/issues/1750#issuecomment-3695942245
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        maven("https://www.jetbrains.com/intellij-repository/releases")
+pluginManagement {
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "2.1.20"
+        id("org.jetbrains.changelog") version "2.5.0"
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.16.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    // Configure all projects' repositories
+    repositories {
+        mavenCentral()
+
+        // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+        intellijPlatform {
+            defaultRepositories()
+        }
+    }
 }
